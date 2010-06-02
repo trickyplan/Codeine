ee39ecbb38a26997fccf2c96a063937c25474f67
BreathLess <dmitry.bergstein@gmail.com>
2010-06-02 10:41:34 +0400

    New.sh reconf.

--- Scripts/New.sh
+++
@@ -1,8 +0,0 @@
-echo Создаём сайт $1
-svn export http://testplace.ru:8080/svn/skeleton/ $1
-cd $1
-chmod 777 -R Data
-echo Создаём MySQL базу $1
-mysql -u root --password=$2 --execute="CREATE DATABASE $1;
-USE $1;
-source Install.sql"
