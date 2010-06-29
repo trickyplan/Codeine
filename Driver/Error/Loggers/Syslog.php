675045bc0699130443579a31c47bd9022dce134c
BreathLess <dmitry.bergstein@gmail.com>
2010-06-29 19:13:36 +0400

    Syslogger, new fuser.

---
+++ Driver/Error/Loggers/Syslog.php
@@ -0,0 +1,27 @@
+<?php
+
+    function F_Syslog_Output($Messages)
+    {
+        openlog(_SERVER, LOG_PID | LOG_PERROR, LOG_LOCAL0);
+        
+        foreach($Messages as $Message)
+        {
+                switch($Message[1])
+                {
+                    case 0:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    case 1:   syslog(LOG_ERR,     $Message[2].':'.$Message[0]); break;
+                    case 2:   syslog(LOG_WARNING, $Message[2].':'.$Message[0]); break;
+                    case 3:   syslog(LOG_WARNING, $Message[2].':'.$Message[0]); break;
+                    case 4:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    case 5:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    case 6:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    case 7:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    case 8:   syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                    default:  syslog(LOG_INFO,    $Message[2].':'.$Message[0]); break;
+                }
+        }
+        
+        closelog();
+       
+        return null;
+    }
