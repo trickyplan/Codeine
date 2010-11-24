eb57b7a1cb11ae68f8510eb3d7cd940dd5a454c7
BreathLess <dmitry.bergstein@gmail.com>
2010-11-25 00:25:58 +0300

    closes #221

--- Driver/View/Render/XML.php
+++ Driver/View/Render/XML.php
@@ -11,3 +11,23 @@
      * @time 3:48
      */
 
+    self::Fn('Do', function ($Call)
+    {
+        header('Content-type: text/xml;charset=utf-8');
+        $Output = '<?xml version="1.0" encoding="UTF-8" ?>';
+
+        $Output.= '<root>';
+        foreach($Call['Input']['Items'] as $Item)
+        {
+            if ($Item['UI'] == 'Object')
+            {
+                $Output.= '<item>';
+                    foreach ($Item['Data'] as $Key => $Value)
+                        $Output.= '<'.$Key.'>'.$Value.'</'.$Key.'>';
+                $Output.= '</item>';
+            }
+        }
+        $Output.= '</root>';
+
+        return $Output;
+    });
