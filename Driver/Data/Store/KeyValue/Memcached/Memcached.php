86c3f5759f7ebda39c77fa8d1690d740bcdded43
BreathLess <dmitry.bergstein@gmail.com>
2010-11-24 22:27:13 +0300

    closes #133

---
+++ Driver/Data/Store/KeyValue/Memcached/Memcached.php
@@ -0,0 +1,65 @@
+<?php
+
+    /* OSWA Codeine
+     * @author BreathLess
+     * @type Codeine Driver
+     * @description: Memcache Driver
+     * @package Codeine
+     * @subpackage Drivers
+     * @version 0.1
+     * @date 24.11.10
+     * @time 21:12
+     */
+
+    self::Fn('Connect', function ($Call)
+    {
+        $Memcached = new Memcached();
+
+        if (!$Memcached->connect($Call['Host'], $Call['Port']))
+        {
+            Code::Hook(__CLASS__, 'errDataMemCachedCannotConnect', $Call);
+            return false;
+        }
+        else
+            return $Memcached;
+    });
+
+    self::Fn('Disconnect', function ($Call)
+    {
+        if (isset($Call['Store']))
+            return $Call['Store']->close();
+        else
+            Code::Hook(__CLASS__, 'errDataMemcachedStoreNotFoundAtDisconnect', $Call);
+    });
+
+    self::Fn('Read', function ($Call)
+    {
+        if ($Call['Store'])
+            return $Call['Store']->get($Call['Point']['Scope'].$Call['Data']['Where']['ID']);
+        else
+            Code::Hook(__CLASS__, 'errDataMemcachedStoreNotFoundAtRead', $Call);
+    });
+
+    self::Fn('Create', function ($Call)
+    {
+        if ($Call['Store'])
+            return $Call['Store']->add($Call['Point']['Scope'].$Call['Data']['Where']['ID'], $Call['Data']);
+        else
+            Code::Hook(__CLASS__, 'errDataMemcachedStoreNotFoundAtCreate', $Call);
+    });
+
+    self::Fn('Update', function ($Call)
+    {
+        if ($Call['Store'])
+            return $Call['Store']->set($Call['Point']['Scope'].$Call['Data']['Where']['ID'], $Call['Data']);
+        else
+            Code::Hook(__CLASS__, 'errDataMemcachedStoreNotFoundAtUpdate', $Call);
+    });
+
+    self::Fn('Delete', function ($Call)
+    {
+        if ($Call['Store'])
+            return $Call['Store']->delete($Call['Point']['Scope'].$Call['Data']['Where']['ID']);
+        else
+            Code::Hook(__CLASS__, 'errDataMemcachedStoreNotFoundAtDelete', $Call);
+    });
