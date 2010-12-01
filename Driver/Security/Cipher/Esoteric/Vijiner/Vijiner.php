17bf06db1c8faf19cd357ffefa3825428fdbeeca
BreathLess <dmitry.bergstein@gmail.com>
2010-12-02 01:53:50 +0300

    es

---
+++ Driver/Security/Cipher/Esoteric/Vijiner/Vijiner.php
@@ -0,0 +1,17 @@
+<?php
+
+    /* OSWA Codeine
+     * @author BreathLess
+     * @type Codeine Driver
+     * @description: Vijiner Cipher
+     * @package Codeine
+     * @subpackage Drivers
+     * @version 5.0
+     * @date 02.12.10
+     * @time 1:52
+     */
+
+    self::Fn('Encrypt', function ($Call)
+    {
+        // TODO
+    });
