77fdff446a6affd50ccee7b17d703056bf53cae2
BreathLess <dmitry.bergstein@gmail.com>
2011-02-25 18:38:20 +0300

    fxs

---
+++ Driver/Calculate/Base/Base.php
@@ -0,0 +1,37 @@
+<?php
+
+    /* OSWA Codeine
+     * @author BreathLess
+     * @type Codeine Driver
+     * @description: Default driver for standart calculations
+     * @package Calculate
+     * @subpackage Standart
+     * @version 5.0
+     * @date 28.10.10
+     * @time 1:43
+     */
+
+    self::Fn('Add', function ($Call)
+    {
+        return $Call['A']+$Call['B'];
+    });
+
+    self::Fn('Substract', function ($Call)
+    {
+        return $Call['A']-$Call['B'];
+    });
+
+    self::Fn('Multiply', function ($Call)
+    {
+        return $Call['A']*$Call['B'];
+    });
+    
+    self::Fn('Divide', function ($Call)
+    {
+        return $Call['A']/$Call['B'];
+    });
+
+    self::Fn('SquareRoot', function ($Call)
+    {
+        return sqrt($Call['Input']);
+    });
