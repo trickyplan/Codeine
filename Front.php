4782cea8955ec373fa36328e7927b2d7a7e5f8cb
BreathLess <dmitry.bergstein@gmail.com>
2011-03-10 06:17:05 +0300

    New Options Model

--- Front.php
+++ Front.php
@@ -13,11 +13,15 @@
         
     try
     {
+        $ST = microtime(true);
         defined('Root') || define('Root', __DIR__);
 
         include 'Library/Core/Core.php';
         
         Code::Run(array('N' => 'Code.Flow.Front', 'F'=>'Run'));
+
+        echo round((microtime(true)-$ST)*1000,2).'<br/>'.round(memory_get_usage(true)/1024);
+        
     }
     catch (Exception $e)
     {
