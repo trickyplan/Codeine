0a559f0f7eccefb9f35bcf2908524b45a37e8f0b
BreathLess <dmitry.bergstein@gmail.com>
2011-11-10 20:24:58 +0400

    New test

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -16,4 +16,50 @@
                )
             ));
         }
+
+        public function testMerge()
+        {
+            $First = array(
+                'A' => 'B',
+                'C',
+                'D' =>
+                    array('E' => 'F',
+                    'G')
+            );
+
+            $Second = array(
+                            'A' =>
+                                array('B' => 'C'),
+                            'E',
+                            'D' => array
+                            (
+                                'E' => 'G',
+                                'G' => 'H'
+                            )
+                        );
+
+            $Result = array(
+                'A' => array('B' => 'C'),
+                'E',
+                'D' => array(
+                    'G',
+                    'E' => 'G',
+                    'G' => 'H'
+                )
+            );
+
+            $this->assertEquals(F::Merge($First, $Second), $Result);
+        }
+
+
+        public function testMergeFlat()
+        {
+            $First = array('A', 'B', 'C');
+
+            $Second = array('D', 'E', 'F');
+
+            $Result = array('D', 'E', 'F');
+
+            $this->assertEquals(F::Merge($First, $Second), $Result);
+        }
     }
