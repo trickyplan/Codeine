ec844ee3dced077083d5526d75dfae7a136df166
BreathLess <dmitry.bergstein@gmail.com>
2011-11-15 10:32:33 +0400

    Warning!
    Drivers moved to Hydrocodone
    https://github.com/Breathless/Hydrocodone

--- src/Drivers/View/I18N/GetText.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    /* Codeine
-     * @author BreathLess
-     * @description: GetText Adapter
-     * @package Codeine
-     * @version 6.0
-     */
-
--- src/Drivers/View/Processors/JS/Minify.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    /* Codeine
-     * @author BreathLess
-     * @description: Default Minifier
-     * @package Codeine
-     * @version 6.0
-     */
-
