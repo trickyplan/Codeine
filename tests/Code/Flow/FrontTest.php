7da2f118fe5a545b5e91038afe26ef9d9b1a9b49
BreathLess <dmitry.bergstein@gmail.com>
2012-01-07 04:50:31 +0400

    unnecessary tests

--- tests/Code/Flow/FrontTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    class FrontTest extends PHPUnit_Framework_TestCase
-    {
-        public function testRoot()
-        {
-            $this->assertArrayHasKey('Output', F::Run('Code.Flow.Front', 'Run', array('Value' => '/')));
-        }
-    }
