0b511ac7278a978479573e447d1c7d378d29d04f
BreathLess <dmitry.bergstein@gmail.com>
2012-02-15 03:17:49 +0400

    Hotfix

--- src/Drivers/View/Processors/HTML/Front.php
+++ src/Drivers/View/Processors/HTML/Front.php
@@ -9,17 +9,20 @@
 
     self::setFn('Layouts', function ($Call)
     {
-        $Slices = explode('.', $Call['Service']);
-        $Slices[] = $Call['Method'];
+        if (F::isCall($Call))
+            {
+                $Slices = explode('.', $Call['Service']);
+                $Slices[] = $Call['Method'];
 
-        $sz = sizeof($Slices);
+                $sz = sizeof($Slices);
 
-        for ($ic = 0; $ic<$sz; $ic++)
-        {
-            $ID = implode('/', array_slice($Slices, 0, $ic));
+                for ($ic = 0; $ic<$sz; $ic++)
+                {
+                    $ID = implode('/', array_slice($Slices, 0, $ic));
 
-            if ($Sublayout = F::Run('Engine.Template', 'Load', array ('Scope' => 'Layout','ID'=> $ID)))
-                $Call['Layout'] = str_replace('<place>Content</place>', $Sublayout, $Call['Layout']);
+                    if ($Sublayout = F::Run('Engine.Template', 'Load', array ('Scope' => 'Layout','ID'=> $ID)))
+                        $Call['Layout'] = str_replace('<place>Content</place>', $Sublayout, $Call['Layout']);
+                }
         }
 
         return $Call;
