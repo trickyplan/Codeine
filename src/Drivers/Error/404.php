b6c8d917f33e52fd5fdc267a84abf0dd00c91cc6
BreathLess <dmitry.bergstein@gmail.com>
2012-02-18 19:12:24 +0400

    404 Support

--- src/Drivers/Error/404.php
+++ src/Drivers/Error/404.php
@@ -9,20 +9,18 @@
 
     self::setFn ('Page', function ($Call)
     {
+
         $Call['Headers']['HTTP/1.0'] = '404 Not Found';
-        $Call['Value'] = array();
+        $Call['Output'] = array();
 
         $Call['Title'] = '404';
         $Call['Description'] = 'TODO';
         $Call['Keywords'] = array ('TODO');
 
+        $Call['Output']['Content'] = array (array (
+                                                'Type'  => 'Static',
+                                                'Value' => 'Errors/404'
+                                            ));
 
-        $Call['Value'][] =
-            array(
-                'Place' => 'Content',
-                'Type'  => 'Static',
-                'Value' => 'Error/404'
-            );
-
-         return $Call;
+        return $Call;
      });
