6dc543d50d9c40635143e717d1c2759e923b7ce9
BreathLess <dmitry.bergstein@gmail.com>
2012-02-18 23:54:22 +0400

    Sitemaps support.

---
+++ src/Drivers/SEO/Generate/Sitemap.php
@@ -0,0 +1,59 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.0
+     */
+
+    self::setFn('Generate', function ($Call)
+    {
+        $Links = array();
+        $Call['Headers']['Content-type:'] = 'text/xml; charset=utf-8';
+
+        foreach($Call['Handlers'] as $Handler)
+        {
+            $SubLinks = F::Live($Handler['Source']);
+
+            foreach ($SubLinks as $Sublink)
+                $Links[] = array ('url' => array (
+                                      'loc'         => htmlspecialchars($Sublink),
+                                      'changefreq'  => $Handler['Frequency'],
+                                      'priority'    => $Handler['Priority']
+                                  ));
+
+        }
+
+        $Call['Renderer'] = 'View.Render.XML';
+        $Call['Namespace'] = 'http://www.sitemaps.org/schemas/sitemap/0.9';
+        $Call['Attributes'] =
+            array(
+                array(
+                    'Prefix' => 'xmlns',
+                    'Key' => 'xsi',
+                    'Value' => 'http://www.w3.org/2001/XMLSchema-instance'
+                ),
+                array(
+                    'Prefix' => 'xsi',
+                    'Key' => 'schemaLocation',
+                    'Value' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'
+                )
+            );
+
+        $Call['Output'] =  array('Root' => 'urlset', 'Content' => $Links);
+
+        return $Call;
+    });
+
+    self::setFn('Ping', function ($Call)
+    {
+        foreach($Call['SearchEngines'] as $Name=> $URL)
+            $Call['Responses'][$Name] = F::Run('Engine.IO', 'Read',
+                array(
+                     'Storage' => 'HTTP',
+                     'ID' => $URL.'/ping?sitemap='.urlencode($_SERVER['HTTP_HOST'].'/sitemap.xml')
+                ));
+
+        return $Call;
+    });
