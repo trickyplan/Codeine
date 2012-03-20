59f043279c3baa2a360662874018ebc2f06dd92d
BreathLess <dmitry.bergstein@gmail.com>
2012-03-20 10:26:32 +0400

    No more caching

--- src/Drivers/View/Pipeline.php
+++ src/Drivers/View/Pipeline.php
@@ -16,17 +16,9 @@
                 if (is_array($Call['Output']))
                     foreach ($Call['Output'] as $Place => $Widgets)
                         foreach ($Widgets as $Key => $Widget)
-                            $Call['Output'][$Place][$Key] = F::Run(
-                                'Code.Run.Cached', 'Run',
-                                array(
-                                     'Run' =>
-                                         array(
-                                             'Service' => $Call['Renderer'] . '.Element.' . $Widget['Type'],
-                                             'Method'  => 'Make',
-                                             'Call'    => $Widget
-                                         )
-                                )
-                            );
+                            $Call['Output'][$Place][$Key] = F::Run($Call['Renderer'] . '.Element.' . $Widget['Type'], 'Make', $Widget);
+
+                // TODO Normal caching
             }
             else
                 $Call['Output']['Content'] = array ('No output'); // FIXME Add Hook
