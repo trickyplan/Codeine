b4d02ee6ed4af1f8eda1828666b1c0f19aef9e9f
BreathLess <dmitry.bergstein@gmail.com>
2012-05-24 15:55:17 +0400

    Fixes.

---
+++ src/Drivers/View/HTML/Element/Form/Radiogroup.php
@@ -0,0 +1,22 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.2
+     */
+
+    self::setFn('Make', function ($Call)
+    {
+        $Output = '';
+
+        foreach($Call['Value'] as $Value)
+            $Output.= F::Run('View', 'LoadParsed',
+                array('Scope' => 'Default',
+                      'ID' => 'UI/HTML/Form/Radio',
+                      'Data' => F::Merge($Call, array('Value' => $Value))));
+
+        return F::Run('View', 'LoadParsed', array('Scope' => 'Default', 'ID' => 'UI/HTML/Form/Radiogroup', 'Data' => F::Merge($Call, array(
+            'Radios' => $Output))));
+     });
