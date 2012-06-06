976918c4c58431cc118418104b7a7cbd5a628079
BreathLess <dmitry.bergstein@gmail.com>
2012-06-06 04:50:50 +0400

    Hotfix

--- src/Drivers/View/Parser/Layout.php
+++ src/Drivers/View/Parser/Layout.php
@@ -13,8 +13,8 @@
         {
             foreach ($Call['Parsed'][1] as $Ix => $Match)
             {
-
                 list($Asset, $ID) = F::Run('View', 'Asset.Route', array('Value' => $Match));
+
                 $Call['Value'] = str_replace ($Call['Parsed'][0][$Ix],
                     F::Run ('View', 'Load', array('Scope' => $Asset, 'ID' => $ID)), $Call['Value']);
             }
