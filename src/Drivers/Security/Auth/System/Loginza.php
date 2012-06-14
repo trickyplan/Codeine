6e4c170012f59f87b49b4b28414a3dfdf7dec097
BreathLess <dmitry.bergstein@gmail.com>
2012-06-14 19:33:24 +0400

    Логинзы коммит

--- src/Drivers/Security/Auth/System/Loginza.php
+++ src/Drivers/Security/Auth/System/Loginza.php
@@ -14,6 +14,12 @@
 
     self::setFn('Authenticate', function ($Call)
     {
-        d(__FILE__, __LINE__, $Call['token']);
+        d(__FILE__, __LINE__, F::Run('IO', 'Read',
+         array(
+             'Storage' => 'Web',
+             'Where' => 'http://loginza.ru/api/authinfo?token='.$Call['token'].'&id=24623&sig='.md5($Call['token'].'1dc118bc34d783e2117a7b826fccbe2a')
+         )));;
+
+die();
         return $Call;
     });
