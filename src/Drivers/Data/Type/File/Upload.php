0e813c124b94ae622242e979808270a251135bbd
BreathLess <dmitry.bergstein@gmail.com>
2012-06-15 14:41:29 +0400

    Hotfix

--- src/Drivers/Data/Type/File/Upload.php
+++ src/Drivers/Data/Type/File/Upload.php
@@ -9,6 +9,9 @@
 
     self::setFn('Write', function ($Call)
     {
+        if (is_scalar($Call['Value']))
+            return $Call['Value'];
+        
         // FIXME Scope Support
         if ($Call['Value']['error'] == 0)
             return F::Run('IO', 'Write', array ('Storage' =>  'Upload', 'Value' => $Call['Value']));
