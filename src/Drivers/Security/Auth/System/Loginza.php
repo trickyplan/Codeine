30bbc82b2887de0a17c8807b350e9421bcb149d2
BreathLess <dmitry.bergstein@gmail.com>
2012-06-15 14:53:21 +0400

    Cleanup code

--- src/Drivers/Security/Auth/System/Loginza.php
+++ src/Drivers/Security/Auth/System/Loginza.php
@@ -17,7 +17,7 @@
         $Response = json_decode(F::Run('IO', 'Read',
          array(
              'Storage' => 'Web',
-             'Where' => 'http://loginza.ru/api/authinfo?token='.$Call['Request']['token'].'&id=24623&sig='.md5($Call['Request']['token'].'1dc118bc34d783e2117a7b826fccbe2a')
+             'Where' => 'http://loginza.ru/api/authinfo?token='.$Call['Request']['token'].'&id='.$Call['Loginza']['ID'].'&sig='.md5($Call['Request']['token'].$Call['Loginza']['Key'])
          ))[0], true);
 
         if (isset($Response['identity']))
@@ -32,26 +32,25 @@
                     )
                 ))[0];
 
+            $UserData = array(
+                        'Login' => $Response['identity'],
+                        'Status' => 1,
+                        'Fullname' => isset($Response['name']['full_name'])
+                                ? $Response['name']['full_name']
+                                : $Response['name']['first_name'].' '.$Response['name']['last_name']);
+            // FIXME
+
+            foreach ($Call['Loginza']['Map'] as $Own => $Provider)
+                $UserData[$Own] = F::Dot($Response, $Provider);
+
             // Если нет, зарегистрировать
             if (empty($Call['User']))
-            {
                 $Call['User'] = F::Run('Entity','Create',
                     array(
                         'Entity' => 'User',
-                        'Data'  => array(
-                            'Login' => $Response['identity'],
-                            'Status' => 1,
-                            'Phone' => $Response['phone']['mobile'],
-                            'Photo' => $Response['photo'],
-                            'Fullname' => isset($Response['name']['full_name'])
-                                ? $Response['name']['full_name']
-                                : $Response['name']['first_name'].' '.$Response['name']['last_name'],
-                            'EMail' => $Response['email']
-                        )
+                        'Data'  => $UserData
                     ));
-            }
             else
-            {
                 $Call['User'] = F::Run('Entity','Update',
                     array(
                         'Entity' => 'User',
@@ -59,16 +58,8 @@
                         array(
                             'Login' => $Response['identity']
                         ),
-                        'Data'  => array(
-                            'Photo' => $Response['photo'],
-                            'Phone' => $Response['phone']['mobile'],
-                            'Fullname' => isset($Response['name']['full_name'])
-                                ? $Response['name']['full_name']
-                                : $Response['name']['first_name'].' '.$Response['name']['last_name'],
-                            'EMail' => $Response['email']
-                        )
+                        'Data'  => $UserData
                     ));
-            }
         }
 
         return $Call;
