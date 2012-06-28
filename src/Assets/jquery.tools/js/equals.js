a6041ac12015c15cba6af97e33d71e3c2803237b
BreathLess <dmitry.bergstein@gmail.com>
2012-06-28 09:47:55 +0400

    Unique

--- src/Assets/jquery.tools/js/equals.js
+++ src/Assets/jquery.tools/js/equals.js
@@ -1,4 +1,4 @@
-$.tools.validator.fn("[data-equals]", "Value not equal with the $1 field", function(input) {
+$.tools.validator.fn("[data-equals]", "Значения не совпадают", function(input) {
     var name = input.attr("data-equals"),
     field = this.getInputs().filter("[name=" + name + "]");
     return input.val() == field.val() ? true : [name];
---
+++ src/Assets/jquery.tools/js/unique.js
@@ -0,0 +1,21 @@
+$.tools.validator.fn("[data-unique]", "Занято", function(input) {
+    var result;
+    $.ajax(
+            {
+                url: '/check',
+                type: 'post',
+                async: false,
+                data:
+                {
+                    Key: input.attr('data-unique'),
+                    Value: input.attr('value')
+                },
+                success: function (data)
+                {
+                    result = data ? true : ['data-unique'];
+                }
+            }
+        );
+
+    return result;
+});
