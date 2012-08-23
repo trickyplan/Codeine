466f8d8bcd73f8cf3d38085a2c71ab4fe4acd995
BreathLess <dmitry.bergstein@gmail.com>
2012-08-23 04:11:30 +0400

    Security.UID API & 3 new drivers

---
+++ src/Drivers/Security/UID/Chars.php
@@ -0,0 +1,35 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description: Random integer
+     * @package Codeine
+     * @version 7.6.2
+     * @date 04.12.10
+     * @time 14:56
+     */
+
+    self::setFn('Get', function ($Call)
+    {
+        $Alphabet = 'abcdefghijklmnopqrstuvxwyzABCDEFGHIJKLMNOPQRSTUVXYZ';
+        $Output = '';
+
+        $Min = 0;
+        $Max = strlen($Alphabet);
+
+        switch ($Call['Case'])
+        {
+            case 'Lower':
+                $Max = strlen($Alphabet)/2;
+            break;
+
+            case 'Upper':
+                $Min = strlen($Alphabet)/2;
+            break;
+        }
+
+        for($IC = 0; $IC<$Call['Size']; $IC++)
+            $Output.= $Alphabet[rand($Min, $Max)];
+
+        return $Output;
+    });
---
+++ src/Drivers/Security/UID/Paranoid.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description: GUID
+     * @package Codeine
+     * @version 7.6.2
+     * @date 04.12.10
+     * @time 14:56
+     */
+
+    self::setFn('Get', function ($Call)
+    {
+        $Output = '';
+
+        for ($ic = 0; $ic < 64; $ic++)
+            $Output.=dechex(rand(0,15));
+
+        return $Output;
+    });
