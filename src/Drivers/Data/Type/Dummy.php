09dcb91c9bf4043513f300b77c2561bf8e2692bc
BreathLess <dmitry.bergstein@gmail.com>
2012-08-23 04:14:18 +0400

    New datatype — dummy.

---
+++ src/Drivers/Data/Type/Dummy.php
@@ -0,0 +1,18 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.6.2
+     */
+
+    self::setFn('Write', function ($Call)
+    {
+        return $Call['Value'];
+    });
+
+    self::setFn('Read', function ($Call)
+    {
+        return $Call['Value'];
+    });
