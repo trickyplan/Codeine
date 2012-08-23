293c5ed567f3c5b848939713003916e6bb7f7462
BreathLess <dmitry.bergstein@gmail.com>
2012-08-23 04:14:34 +0400

    Fixed List.Enum behaviour

--- src/Drivers/Data/Type/List/Enum.php
+++ src/Drivers/Data/Type/List/Enum.php
@@ -9,16 +9,22 @@
 
     self::setFn ('Write', function ($Call)
     {
+        $Call['Node']['Options'] = F::Live($Call['Node']['Options']);
+
+        if (isset($Call['Node']['StoreKey']))
+            return isset($Call['Node']['Options'][$Call['Value']])? $Call['Value']
+                : array_search($Call['Value'], $Call['Node']['Options']);
+
         return $Call['Value'];
     });
 
     self::setFn('Read', function ($Call)
     {
-        if ($Call['Purpose'] != 'Where')
+        if (!isset($Call['Purpose']) || $Call['Purpose'] != 'Where')
         {
             $Call['Node']['Options'] = F::Live($Call['Node']['Options']);
 
-            return $Call['Node']['Options'][(int) $Call['Value']];
+            return isset($Call['Node']['Options'][$Call['Value']])? $Call['Node']['Options'][$Call['Value']]: $Call['Value'];
         }
         else
             return $Call['Value'];
