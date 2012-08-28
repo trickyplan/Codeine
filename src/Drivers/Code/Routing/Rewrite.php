a6c0e95b1e82acb263a5543bf7b96b7a14965454
BreathLess <dmitry.bergstein@gmail.com>
2012-08-29 01:06:35 +0400

    New router — tough Rewrite

---
+++ src/Drivers/Code/Routing/Rewrite.php
@@ -0,0 +1,36 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description: 
+     * @package Codeine
+     * @version 7.x
+     * @date 31.08.11
+     * @time 6:17
+     */
+
+    self::setFn('Route', function ($Call)
+    {
+        if (strpos($Call['Run'], '?'))
+            list($Call['Run']) = explode('?', $Call['Run']);
+
+        if (isset($Call['Rewrite']))
+        {
+            if (is_string($Call['Run']) && isset($Call['Rewrite'][$Call['Run']]))
+            {
+                $Call['Run'] =
+                    [
+                        'Service' => 'System.Interface.Web',
+                        'Method' => 'Redirect',
+                        'Call' =>
+                            [
+                                'Location' => $Call['Rewrite'][$Call['Run']]
+                            ]
+                    ];
+            }
+        }
+        else
+            die('Rewrite routes table corrupted'); // FIXME
+
+        return $Call;
+    });
