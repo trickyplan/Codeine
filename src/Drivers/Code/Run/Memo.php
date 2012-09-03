b78b699e13d07f051338c89d51559583509f29db
BreathLess <dmitry.bergstein@gmail.com>
2012-09-03 13:48:49 +0600

    Поддержка прозрачной мемоизации (альфа).

---
+++ src/Drivers/Code/Run/Memo.php
@@ -0,0 +1,31 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description Memoize call
+     * @package Codeine
+     * @version 7.x
+     */
+
+    self::setFn('Run', function ($Call)
+    {
+        $Hash = [$Call['Run']['Service'], $Call['Run']['Method']];
+
+        foreach ($Call['Run']['Call']['Contract'][($Call['Run']['Method'])]['Call'] as $Key => $Argument)
+            if (isset($Call['Run']['Call'][$Key]))
+                $Hash[] = $Call['Run']['Call'][$Key];
+
+        $Hash = serialize($Hash);
+
+        $Call['Run']['Call']['Memo'] = true;
+
+        $Result = F::Get($Hash);
+
+        if ($Result === null)
+        {
+            $Result = F::Execute($Call['Run']['Service'], $Call['Run']['Method'], $Call['Run']['Call']);
+            F::Set($Hash, $Result);
+        }
+
+        return $Result;
+    });
