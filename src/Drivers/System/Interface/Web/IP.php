7ace3598cceb2a1201ee48657dfafca76ab27d94
BreathLess <dmitry.bergstein@gmail.com>
2012-09-04 17:11:57 +0600

    Better IP Detection

--- src/Drivers/System/Interface/Web/IP.php
+++ src/Drivers/System/Interface/Web/IP.php
@@ -9,5 +9,11 @@
 
     self::setFn('Get', function ($Call)
     {
-        return isset($Call['Substitute'][$_SERVER['REMOTE_ADDR']])? $Call['Substitute'][$_SERVER['REMOTE_ADDR']]: $_SERVER['REMOTE_ADDR'];
+        if (isset($_SERVER['HTTP_X_REAL_IP']))
+            $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
+
+        if ($_SERVER['REMOTE_ADDR'] == '127.0.0.1' && isset($Call['Pingback']))
+            return file_get_contents($Call['Pingback']);
+        else
+            return isset($Call['Substitute'][$_SERVER['REMOTE_ADDR']])? $Call['Substitute'][$_SERVER['REMOTE_ADDR']]: $_SERVER['REMOTE_ADDR'];
     });
