0b90744f0dce9dd12905b3cd6d55cbeb11b25247
BreathLess <dmitry.bergstein@gmail.com>
2012-09-05 18:16:44 +0600

    Memcached Support

---
+++ src/Drivers/IO/Storage/Memcached.php
@@ -0,0 +1,77 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description XCache Data Driver
+     * @package Codeine
+     * @version 7.x
+     */
+
+    self::setFn ('Open', function ($Call)
+    {
+        $Memcached = new Memcached();
+        $Memcached->addServer ($Call['Server'], $Call['Port']);
+
+        return $Memcached;
+    });
+
+    self::setFn ('Read', function ($Call)
+    {
+        if (is_array($Call['Where']['ID']))
+        {
+            foreach ($Call['Where']['ID'] as &$ID)
+                $ID = $Call['Scope'].$ID;
+
+            return $Call['Link']->getMulti($Call['Where']['ID']);
+        }
+        else
+            if (($Result = $Call['Link']->get($Call['Scope'].$Call['Where']['ID'])) !== false)
+                return array($Result);
+            else
+                return null;
+    });
+
+    self::setFn ('Write', function ($Call)
+    {
+        if (isset($Call['Where']))
+        {
+            if (null === $Call['Data'])
+                $Call['Link']->delete($Call['Scope'].$Call['Where']['ID']);
+            else
+            {
+                $Call['Data'] = F::Merge(F::Run(null, 'Read', $Call)[0], $Call['Data']);
+                $Call['Link']->set($Call['Scope'].$Call['Where']['ID'], $Call['Data'], $Call['TTL']);
+            }
+        }
+        else
+        {
+            $Call['Link']->set($Call['Scope'].$Call['Data']['ID'], $Call['Data'], $Call['TTL']);
+            F::Log($Call['Data']);
+        }
+
+        return $Call['Data'];
+    });
+
+    self::setFn ('Close', function ($Call)
+    {
+        return true;
+    });
+
+    self::setFn ('Execute', function ($Call)
+    {
+        return true;
+    });
+
+    self::setFn ('Exist', function ($Call)
+    {
+        return $Call['Link']->get($Call['Scope'].$Call['Where']['ID']);
+    });
+
+    self::setFn('Status', function ($Call)
+    {
+       $Info = $Call['Link']->getStats();
+       foreach ($Info as $Key => &$Value)
+           $Value = array($Key, $Value);
+
+       return $Info;
+    });
