5b3f1a5fe13ea2973a5d14e85ddb3cdc72c793b4
BreathLess <dmitry.bergstein@gmail.com>
2012-09-06 13:26:19 +0600

    Metaphone - Ru

---
+++ src/Drivers/Text/Index/Metaphone/Russian.php
@@ -0,0 +1,15 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+
+    self::setFn('Get', function ($Call)
+    {
+        $Call['Value'] = strtr(mb_strtolower(preg_replace('/(.)\\1+/','', $Call['Value'])), $Call['Replace']);
+        return preg_replace('/(.)\\1+/SsUu', '\1', $Call['Value']);
+    });
