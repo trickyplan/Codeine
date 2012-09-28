865a98d186b9b6163f289ec9f176a42efd028688
BreathLess <dmitry.bergstein@gmail.com>
2012-09-28 17:11:01 +0600

    KISSMetrics support

---
+++ src/Drivers/View/HTML/Element/Analytics/KISSMetrics.php
@@ -0,0 +1,34 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    self::setFn('Make', function ($Call)
+    {
+        // FIXME Templatize
+        // FIXME Options
+
+        if (isset($Call['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
+            $Code = '<!-- Do Not Track enabled. Kissmetrics supressed. -->';
+        else
+            $Code = "<script type=\"text/javascript\">
+  var _kmq = _kmq || [];
+  var _kmk = _kmk || '".$Call['ID']."';
+  function _kms(u){
+    setTimeout(function(){
+      var d = document, f = d.getElementsByTagName('script')[0],
+      s = d.createElement('script');
+      s.type = 'text/javascript'; s.async = true; s.src = u;
+      f.parentNode.insertBefore(s, f);
+    }, 1);
+  }
+  _kms('//i.kissmetrics.com/i.js');
+  _kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+</script>";
+
+        return $Code;
+     });
