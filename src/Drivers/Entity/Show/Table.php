7ac829e7c124613fdd8ac8b38819f6947e90a4f7
BreathLess <dmitry.bergstein@gmail.com>
2012-10-09 16:31:32 +0400

    Show Table

---
+++ src/Drivers/Entity/Show/Table.php
@@ -0,0 +1,29 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    self::setFn('Do', function ($Call)
+    {
+        $Element = F::Run('Entity', 'Read', $Call)[0];
+
+        $Data = [];
+        F::Map($Element, function ($Key, $Value, $D, $Fullkey) use ($Call, &$Data)
+        {
+            if (!is_array($Value))
+                $Data[] = ['<l>'.$Call['Entity'].'.Entity:'.substr($Fullkey, 1).'</l>', $Value];
+        });
+
+        $Call['Output']['Content'][]
+        =
+            [
+                'Type' => 'Table',
+                'Value' => $Data
+            ];
+
+        return $Call;
+    });
