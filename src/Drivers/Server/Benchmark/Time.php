cf5c86d90d85c1fecd15549f71b56169a67ef203
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-10-27 19:52:54 +0400

    Server Benchmark Proto

---
+++ src/Drivers/Server/Benchmark/Time.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    self::setFn('Test', function ($Call)
+    {
+        $Cycles = 1000000-2;
+
+        $Start = microtime(true);
+            for ($a = 1; $a<$Cycles; $a++)
+                microtime(true);
+
+        $Stop = microtime(true);
+        return round(1000/($Stop-$Start));
+    });
