7335c7acf87e550f642be3cf8fe80a929be09bc4
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-10-29 23:05:08 +0400

    Track PageSpeed

--- src/Drivers/View/HTML/Element/Analytics/Google.php
+++ src/Drivers/View/HTML/Element/Analytics/Google.php
@@ -20,6 +20,7 @@
   _gaq.push(['_setAccount', 'UA-".$Call['ID']."']);
   _gaq.push(['_trackPageview']);
   _gaq.push(['_setDomainName', 'none']);
+  _gaq.push(['_trackPageLoadTime']);
   (function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
