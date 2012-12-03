a5aa801158eb02249cf1ca10d7a30f296558eee2
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-12-03 20:30:43 +0400

    bugfixes

--- src/Drivers/View/HTML/Parslets/Io.php
+++ src/Drivers/View/HTML/Parslets/Io.php
@@ -15,17 +15,17 @@
             {
                 list(,$Storage, $Scope, $Where) = $Slices;
 
-                    $Element = F::Run('IO', 'Read',
-                        [
-                            'Storage' => $Storage,
-                            'Scope' => $Scope,
-                            'Where'  => $Where
-                        ]);
+                $Element = F::Run('IO', 'Read',
+                    [
+                        'Storage' => $Storage,
+                        'Scope' => $Scope,
+                        'Where'  => $Where
+                    ]);
 
-                    if (!empty($Element))
-                        $Call['Output'] = str_replace($Call['Parsed'][0][$Ix], $Element[0],$Call['Output']);
-                    else
-                        $Call['Output'] = str_replace($Call['Parsed'][0][$Ix], '', $Call['Output']);
+                if (!empty($Element))
+                    $Call['Output'] = str_replace($Call['Parsed'][0][$Ix], $Element[0],$Call['Output']);
+                else
+                    $Call['Output'] = str_replace($Call['Parsed'][0][$Ix], '', $Call['Output']);
             }
             else
                 $Call['Output'] = str_replace($Call['Parsed'][0][$Ix], '' . $Match, $Call['Output']);
--- src/Drivers/View/HTML/Parslets/Number.php
+++ src/Drivers/View/HTML/Parslets/Number.php
@@ -15,6 +15,7 @@
 
             $Format = isset($Root->attributes()->format)? (string) $Root->attributes()->format: 'French';
             $Digits = isset($Root->attributes()->digits)? (int) $Root->attributes()->digits: 0;
+
             switch($Format)
             {
                 case 'French':
