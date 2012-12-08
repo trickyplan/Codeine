f01f228eefc6e88ffeae22f8e3b1d4a2c39340eb
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-12-08 23:35:41 +0400

    Monitor fixes

--- src/Drivers/System/Monitor.php
+++ src/Drivers/System/Monitor.php
@@ -9,6 +9,8 @@
 
     setFn('Do', function ($Call)
     {
+        if (isset($Call['Sensor']))
+            $Sensors[$Call['Sensor']] = F::Run(null, $Call['Sensor'], $Call);
         foreach ($Call['Sensors'] as $Sensor)
             $Sensors[$Sensor] = F::Run(null, $Sensor, $Call);
 
