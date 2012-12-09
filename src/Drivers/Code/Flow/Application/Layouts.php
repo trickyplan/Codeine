9f933ee34f44c363314adb4f2d318bb9efbb84b1
Дмитрий Бергштейн <bergstein@one2team.ru>
2012-12-09 19:46:08 +0400

    godfixes )

---
+++ src/Drivers/Code/Flow/Application/Layouts.php
@@ -0,0 +1,33 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    setFn('Do', function ($Call)
+    {
+        if (!isset($Call['Layouts']))
+            $Call['Layouts'] = [];
+
+        if (F::isCall($Call['Run']))
+        {
+            $Slices = explode('.', $Call['Run']['Service']);
+
+            $sz = sizeof($Slices);
+
+            $Asset = $Slices[0];
+
+            for ($ic = 1; $ic < $sz; $ic++)
+                $IDs[$ic] = implode('/', array_slice($Slices, 1, $ic));
+
+            $IDs[] = 'Main';
+
+            foreach ($IDs as $ID)
+                array_unshift($Call['Layouts'], array ('Scope' => $Asset, 'ID'    => $ID, 'Context' => $Call['Context']));
+        }
+
+        return $Call;
+     });
