7102a0c37c4f0167050881456fbe44176342cabd
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-01-25 14:25:51 +0600

    Validity fix

--- src/Drivers/Entity/Validate/Required.php
+++ src/Drivers/Entity/Validate/Required.php
@@ -10,7 +10,8 @@
     setFn('Process', function ($Call)
     {
         if (isset($Call['Node']['Required']) && $Call['Node']['Required'])
-            if (!isset($Call['Data'][$Call['Name']]) or (empty($Call['Data'][$Call['Name']]) && !isset($Call['Node']['Nullable'])))
+            if (!isset($Call['Data'][$Call['Name']]) or (empty($Call['Data'][$Call['Name']]) && !isset($Call['Node']['Nullable'])) or !isset
+            ($Call['Current'][$Call['Name']]))
                 return 'Required';
 
         return true;
