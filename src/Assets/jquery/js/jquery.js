0adbeb22133da4aa853b408c7718e1e1c068dbb6
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-02-01 22:30:50 +0600

    jQuery 1.9

--- src/Assets/jquery/js/jquery.js
+++ src/Assets/jquery/js/jquery.js
@@ -1,16 +1,3 @@
-/*!
- * jQuery JavaScript Library v1.9.0
- * http://jquery.com/
- *
- * Includes Sizzle.js
- * http://sizzlejs.com/
- *
- * Copyright 2005, 2012 jQuery Foundation, Inc. and other contributors
- * Released under the MIT license
- * http://jquery.org/license
- *
- * Date: 2013-1-14
- */
 (function( window, undefined ) {
 "use strict";
 var
