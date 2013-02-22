48536d335289326c3e453549c6f02c7d6919af46
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-02-22 12:59:09 +0600

    Location fix

--- src/Drivers/Location/URL.php
+++ src/Drivers/Location/URL.php
@@ -15,13 +15,14 @@
         if (isset($Call['Call']['Where']['Location']))
             $Call['Location'] = F::Live($Call['Call']['Where']['Location']);
 
-        if (isset($Call['Location']))
+        if (isset($Call['Location']) && !empty($Call['Location']))
         {
             $Call['Session']['Location'] = F::Live($Call['Location']);
             $Call['Session']['LocationURL'] = '/'.F::Run('Entity', 'Far', ['Entity' => 'Location', 'Where' => F::Live($Call['Location']), 'Key' => 'Slug']);
         }
         else
         {
+
             $Call['Session']['Location'] = 0;
             $Call['Session']['LocationURL'] = '';
         }
