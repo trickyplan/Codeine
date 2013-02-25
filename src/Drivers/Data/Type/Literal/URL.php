700a4546109d519a2f662d8ddc579946d430b134
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-02-25 13:49:30 +0600

    Empty URL fix

--- src/Drivers/Data/Type/Literal/URL.php
+++ src/Drivers/Data/Type/Literal/URL.php
@@ -9,21 +9,25 @@
 
     setFn('Write', function ($Call)
     {
-        $Parts = parse_url($Call['Value']);
-
-        if (!isset($Parts['host']))
+        if (isset($Call['Value']) && !empty($Call['Value']))
         {
-            $Parts['host'] = $Parts['path'];
-            unset($Parts['path']);
+            $Parts = parse_url($Call['Value']);
+
+            if (!isset($Parts['host']))
+            {
+                $Parts['host'] = $Parts['path'];
+                unset($Parts['path']);
+            }
+
+            $Parts['scheme'] = isset($Parts['scheme'])? $Parts['scheme']: 'http';
+            $Parts['path'] = isset($Parts['path'])? $Parts['path']:'';
+            $Parts['query'] = isset($Parts['query'])? $Parts['query']:'';
+            $Parts['fragment'] = isset($Parts['fragment'])? $Parts['fragment']:'';
+
+
+            return $Parts['scheme'].'://'.$Parts['host'].$Parts['path'].$Parts['query'].$Parts['fragment'];
         }
-
-        $Parts['scheme'] = isset($Parts['scheme'])? $Parts['scheme']: 'http';
-        $Parts['path'] = isset($Parts['path'])? $Parts['path']:'';
-        $Parts['query'] = isset($Parts['query'])? $Parts['query']:'';
-        $Parts['fragment'] = isset($Parts['fragment'])? $Parts['fragment']:'';
-
-
-        return $Parts['scheme'].'://'.$Parts['host'].$Parts['path'].$Parts['query'].$Parts['fragment'];
+        else return null;
     });
 
     setFn('Read', function ($Call)
