2dc0d2e2eb4b8f551facb616ffc3fdc064444515
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-02-26 06:18:54 +0600

    Fixes

--- src/Drivers/View/HTML/Element/Form/Multifield.php
+++ src/Drivers/View/HTML/Element/Form/Multifield.php
@@ -9,5 +9,14 @@
 
     setFn('Make', function ($Call)
     {
+        $Data = [];
+
+        if (count($Call['Value']) > 0)
+            foreach ($Call['Value'] as $K => $V)
+                if ($K > 0)
+                    $Data[] = [$Call['Name'] => $V];
+
+        $Call['Data'] = json_encode($Data);
+
         return F::Run('View', 'LoadParsed', array('Scope' => 'Default', 'ID' => 'UI/Form/Multifield', 'Data' => $Call));
      });
