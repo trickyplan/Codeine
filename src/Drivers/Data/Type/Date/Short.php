7e5ae5cc18cb6fde7f3acac8b52b378e88f5d662
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-03-02 03:16:18 +0600

    Fixes

--- src/Drivers/Data/Type/Date/Short.php
+++ src/Drivers/Data/Type/Date/Short.php
@@ -11,8 +11,13 @@
     {
         if (isset($Call['Value']) && !empty($Call['Value']) && $Call['Value']>0)
         {
-            $Date = strptime($Call['Value'],'%d.%m.%Y');
-            return mktime(0,0,0, 1+$Date['tm_mon'], $Date['tm_mday'], 1900+$Date['tm_year']);
+            if (!is_numeric($Call['Value']))
+            {
+                $Date = strptime($Call['Value'],'%d.%m.%Y');
+                return mktime(0,0,0, 1+$Date['tm_mon'], $Date['tm_mday'], $Date['tm_year']);
+            }
+            else
+                return $Call['Value'];
         }
         else
             return null;
