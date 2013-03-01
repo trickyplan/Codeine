71105f500e2fe7371f1a6fca5ffb52f3ae37d506
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-03-02 02:45:17 +0600

    Log fixes

--- src/Drivers/IO/Log.php
+++ src/Drivers/IO/Log.php
@@ -12,19 +12,22 @@
     {
         $Logs = F::Logs();
 
-        foreach ($Logs as &$Log)
-            $Log = implode ("\t", $Log);
+        if (!empty($Logs))
+        {
+            foreach ($Logs as &$Log)
+                $Log = implode ("\t", $Log);
 
-        F::Run(
-            'IO', 'Write',
-            [
-                'Renderer' => $Call['Renderer'],
-                'Storage' => 'Developer',
-                'Data' => implode(PHP_EOL, $Logs)
-            ]
-        );
+            F::Run(
+                'IO', 'Write',
+                [
+                    'Renderer' => $Call['Renderer'],
+                    'Storage' => 'Developer',
+                    'Data' => implode(PHP_EOL, $Logs)
+                ]
+            );
 
-        F::Run('IO', 'Close', array('Storage' => 'Developer'));
+            F::Run('IO', 'Close', array('Storage' => 'Developer'));
+        }
 
         return $Call;
     });
