da894e297b3a334d7cd4ad1fd99eeff7e7afdfb9
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-03-21 17:19:02 +0400

    Entity Checks

--- src/Drivers/Entity/Check.php
+++ src/Drivers/Entity/Check.php
@@ -11,10 +11,14 @@
     {
         $Call = F::Run('Entity', 'Load', $Call);
 
+        $Call['Data'] = F::Run('Entity', 'Read', $Call);
+
         $Call = F::Hook('beforeCheck', $Call);
 
+        foreach ($Call['Data'] as $Element)
             F::Run('Entity', 'Update', $Call,
             [
+                'Where' => $Element['ID'],
                 'One' => true,
                 'Data' =>
                 [
