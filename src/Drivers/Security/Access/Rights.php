e35adf848897a4ede96e478aece2fc3154786f09
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-03-22 07:04:17 +0400

    Many fixes

--- src/Drivers/Security/Access/Rights.php
+++ src/Drivers/Security/Access/Rights.php
@@ -10,19 +10,18 @@
     setFn('Check', function ($Call)
     {
         if (isset($Call['Session']['User']['Rights']))
-        {
             $UserRights = (array) explode(',', $Call['Session']['User']['Rights']);
+        else
+            $UserRights = [];
 
-            foreach ($Call['Access']['Rights'] as $Name => $Rule)
-                if (($Diff = F::Diff($Rule, $Call['Run'])) === null)
-                {
-                    F::Log('Rule applied: '.$Name, LOG_INFO);
-                    $Call['Decision'] = in_array($Name, $UserRights);
-                }
-        }
+        foreach ($Call['Access']['Rights'] as $Name => $Rule)
+            if (($Diff = F::Diff($Rule, $Call['Run'])) === null)
+            {
+                F::Log('Right applied: '.$Name, LOG_INFO);
+                $Call['Decision'] = in_array($Name, $UserRights);
+            }
 
         F::Log('Decision:'. ($Call['Decision']? 'Allow': 'Deny'), LOG_INFO);
 
-        unset($Call['Access']);
         return $Call;
     });
