af8b3646676eac5a5cefb9cac8f5c8517ee4b5d8
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-03-24 07:28:44 +0400

    Smallfix.

--- src/Drivers/Location/Filter.php
+++ src/Drivers/Location/Filter.php
@@ -9,7 +9,7 @@
 
     setFn('Do', function ($Call)
     {
-        if (isset($Call['Session']['Location']) && $Call['Session']['Location'] != 0 && is_array($Call['Where']))
+        if ((isset($Call['Session']['Location']) && $Call['Session']['Location'] != 0) && isset($Call['Where']) && is_array($Call['Where']))
             $Call['Where']['Location'] = $Call['Session']['Location'];
 
         return $Call;
