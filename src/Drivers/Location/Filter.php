b7590634069eae39a380bc13be125e51cc0297d6
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-04-14 13:21:41 +0400

    Hotfixes

--- src/Drivers/Location/Filter.php
+++ src/Drivers/Location/Filter.php
@@ -9,11 +9,8 @@
 
     setFn('Do', function ($Call)
     {
-        if (!isset($Call['Where']))
-        {
-            if ((isset($Call['Session']['Location']) && $Call['Session']['Location'] != 0) && isset($Call['Where']) && is_array($Call['Where']))
-                $Call['Where']['Location'] = $Call['Session']['Location'];
-        }
+        if ((isset($Call['Session']['Location']) && $Call['Session']['Location'] != 0) && isset($Call['Where']) && is_array($Call['Where']))
+            $Call['Where']['Location'] = $Call['Session']['Location'];
 
         return $Call;
     });
