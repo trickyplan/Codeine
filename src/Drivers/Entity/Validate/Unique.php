2e19d495012064c85f0dc513c7687005bc257ab7
Дмитрий Бергштейн <bergstein@one2team.ru>
2013-04-30 16:26:17 +0400

    #1172

--- src/Drivers/Entity/Validate/Unique.php
+++ src/Drivers/Entity/Validate/Unique.php
@@ -11,14 +11,18 @@
     {
         if (isset($Call['Node']['Unique']) && $Call['Node']['Unique'] && isset($Call['Data'][$Call['Name']]))
         {
+            $Where = [
+                      $Call['Name'] => $Call['Data'][$Call['Name']],
+                      'ID' => ['$ne' => $Call['Data']['ID']]
+                   ];
+
+            if (isset($Call['Node']['Unique By Location']))
+                $Where['Location'] = $Call['Data']['Location'];
+
             if (F::Run('Entity', 'Count',
                           [
                                'Entity' => $Call['Entity'],
-                               'Where' =>
-                                   [
-                                      $Call['Name'] => $Call['Data'][$Call['Name']],
-                                      'ID' => ['$ne' => $Call['Data']['ID']]
-                                   ]
+                               'Where' => $Where
                           ]) > 0)
                 return 'Unique';
         }
