dca6fde7dde3b84142fcac75de018bf7b8f5aa89
Dmitry Bergstein <bergstein@one2team.ru>
2013-07-14 08:55:21 +0400

    Universal Analytics Support

--- src/Drivers/View/HTML/Element/Analytics/Google.php
+++ src/Drivers/View/HTML/Element/Analytics/Google.php
@@ -30,3 +30,23 @@
 
         return $Code;
      });
+
+    setFn('Universal', function ($Call)
+    {
+        // FIXME Templatize
+        // GA FIXME Options
+
+        if (isset($Call['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
+            $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
+        else
+            $Code = "<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-".$Call['ID']."', '".$Call['Host']."');
+  ga('send', 'pageview');
+</script>";
+
+        return $Code;
+    });
