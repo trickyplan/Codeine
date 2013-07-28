3c3c58ddfd48b5813021ed0383ec8c41f725454a
Dmitry Bergstein <bergstein@one2team.ru>
2013-07-29 03:18:55 +0400

    Возрастные рейтинги. closes #1252

---
+++ src/Drivers/User/Entity.php
@@ -0,0 +1,16 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    setFn('Age', function ($Call)
+    {
+        if (isset($Call['Data']['DOB']))
+            return floor(time()-$Call['Data']['DOB'])/(86400*365);
+        else
+            return 0;
+    });
