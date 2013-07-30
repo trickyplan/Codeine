da6ce9875a64c970ef6b94098b1bc66609d3d1b7
Dmitry Bergstein <bergstein@one2team.ru>
2013-07-30 23:27:09 +0400

    Hotfix.

---
+++ src/Drivers/View/HTML/Widget/Form.php
@@ -0,0 +1,14 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+
+    setFn('Make', function ($Call)
+    {
+        return $Call;
+    });
