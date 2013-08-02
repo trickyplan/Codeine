459e4356fac002173f99566643bf08b0db0c2a7e
Dmitry Bergstein <bergstein@one2team.ru>
2013-08-02 06:09:28 +0400

    View Call Enable.

--- src/Drivers/View/Hooks/Call.php
+++ src/Drivers/View/Hooks/Call.php
@@ -9,10 +9,8 @@
 
     setFn('Parse', function ($Call)
     {
-        if (preg_match_all('@<c>(.*)</c>@SsUu', $Call['Value'], $Pockets))
+        if (preg_match_all('@<call>(.*)</call>@SsUu', $Call['Value'], $Pockets))
         {
-            $Pockets[1] = array_unique($Pockets[1]);
-
             foreach ($Pockets[1] as $IX => $Match)
             {
                 if (($Matched = F::Dot($Call, $Match)) !== null)
@@ -27,8 +25,6 @@
 
                     $Call['Value'] = str_replace($Pockets[0][$IX], F::Live($Matched), $Call['Value']);
                 }
-                else
-                    $Call['Value'] = str_replace($Pockets[0][$IX], '', $Call['Value']);
             }
         }
 
