5beb4587bbf71dec2cc93a9de9495f607a17288c
Dmitry Bergstein <bergstein@one2team.ru>
2013-08-05 21:20:42 +0400

    Active link fixed.

--- src/Assets/Default/js/activelink.js
+++ src/Assets/Default/js/activelink.js
@@ -2,7 +2,7 @@ $(document).ready(function ()
 {
     var str = location.href.toLowerCase();
 
-    $("li a, td a, a").each(function ()
+    $("li a, td a").each(function ()
     {
         if (str.indexOf(this.href.toLowerCase())>-1)
             $(this).parent().addClass("active");
