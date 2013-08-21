253cd23c15fcee9dc84e376c1fe087da9d58c6ea
Dmitry Bergstein <bergstein@one2team.ru>
2013-08-21 17:25:57 +0400

    Meta fixes.

--- src/Drivers/View/RSS/Meta/Scan.php
+++ src/Drivers/View/RSS/Meta/Scan.php
@@ -9,36 +9,27 @@
 
     setFn('Title', function ($Call)
     {
-        if (preg_match_all('@<subtitle>(.*)<\/subtitle>@SsUu', $Call['Output'], $Pockets))
+        if (preg_match_all('@<subtitle>(.*)<\/subtitle>@SsUu', $Call['Layout'], $Pockets))
             foreach ($Pockets[1] as $IX => $Match)
-            {
                 $Call['Title'][] = $Match;
-                $Call['Output'] = str_replace($Pockets[0][$IX], '', $Call['Output']);
-            }
 
         return $Call;
     });
 
     setFn('Keywords', function ($Call)
     {
-        if (preg_match_all('@<keyword>(.*)<\/keyword>@SsUu', $Call['Output'], $Pockets))
+        if (preg_match_all('@<keyword>(.*)<\/keyword>@SsUu', $Call['Layout'], $Pockets))
             foreach ($Pockets[1] as $IX => $Match)
-            {
                 $Call['Keywords'][] = $Match;
-                $Call['Output'] = str_replace($Pockets[0][$IX], '', $Call['Output']);
-            }
 
         return $Call;
     });
 
     setFn('Description', function ($Call)
     {
-        if (preg_match_all('@<description>(.*)<\/description>@SsUu', $Call['Output'], $Pockets))
+        if (preg_match_all('@<description>(.*)<\/description>@SsUu', $Call['Layout'], $Pockets))
             foreach ($Pockets[1] as $IX => $Match)
-            {
                 $Call['Description'] = $Match;
-                $Call['Output'] = str_replace($Pockets[0][$IX], '', $Call['Output']);
-            }
 
         return $Call;
     });
