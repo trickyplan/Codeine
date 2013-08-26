a14ed4ff14b43c1ed643976d1a50b4bf32579dbc
Dmitry Bergstein <bergstein@one2team.ru>
2013-08-26 06:18:19 +0400

    Breadcrumbs unnecessary sorting.

--- src/Drivers/View/HTML/Breadcrumbs.php
+++ src/Drivers/View/HTML/Breadcrumbs.php
@@ -23,7 +23,6 @@
                 $Call['Output'] = str_replace($Pockets[0][$IX], '', $Call['Output']);
             }
 
-            ksort($Call['Breadcrumbs']);
             $Last = array_pop($Call['Breadcrumbs']);
 
             foreach ($Call['Breadcrumbs'] as $Breadcrumb)
