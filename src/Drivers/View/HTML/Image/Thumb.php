1bc31d6d2e8fb217be96ea04a56da5a3550ccbb1
Dmitry Bergstein <bergstein@one2team.ru>
2013-10-30 16:00:26 +0400

    Fix

--- src/Drivers/View/HTML/Image/Thumb.php
+++ src/Drivers/View/HTML/Image/Thumb.php
@@ -17,7 +17,11 @@
 
             if (!isset($Call['Current Image']['Height']))
                 $Call['Current Image']['Height'] =
-                    ($Call['Current Image']['Width']/$GImage->getimagewidth())*$GImage->getimageheight();
+                    round(($Call['Current Image']['Width']/$GImage->getimagewidth())*$GImage->getimageheight());
+
+            if (!isset($Call['Current Image']['Width']))
+                $Call['Current Image']['Width'] =
+                    round(($Call['Current Image']['Height']/$GImage->getimageheight())*$GImage->getimagewidth());
 
             $GImage->cropthumbnailimage($Call['Current Image']['Width'], $Call['Current Image']['Height']);
 
