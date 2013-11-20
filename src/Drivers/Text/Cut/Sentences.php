a335d9d68ee90eab30e4de097bca420b5bdb5509
Dmitry Bergstein <bergstein@one2team.ru>
2013-11-20 07:48:41 +0400

    7.40

--- src/Drivers/Text/Cut/Sentences.php
+++ src/Drivers/Text/Cut/Sentences.php
@@ -9,9 +9,9 @@
 
     setFn('Do', function ($Call)
     {
-        $Sentences = preg_split('/[.!?;]+/', $Call['Value']);
+        $Sentences = preg_split('/[.!?;]+/', strip_tags($Call['Value']));
 
-        if (count($Sentences) > $Call['Sentences'])
+        if (count($Sentences) > $Call['Sentences'] && !empty($Sentences[$Call['Sentences']]))
             $Cutted = mb_substr($Call['Value'], 0, mb_strpos($Call['Value'], $Sentences[$Call['Sentences']])-1);
         else
             $Cutted = $Call['Value'];
