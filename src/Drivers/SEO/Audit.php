3b1b639972f79aa6a4859aa28048eb6658e89e0d
Dmitry Bergstein <bergstein@one2team.ru>
2014-01-26 16:06:23 +0400

    Skip SEO Audit for control

--- src/Drivers/SEO/Audit.php
+++ src/Drivers/SEO/Audit.php
@@ -9,9 +9,12 @@
 
     setFn('Do', function ($Call)
     {
-        if (isset($Call['View']['HTML']) && !isset($Call['Headers']['Location:']))
-            foreach($Call['Auditors'] as $Auditor)
-                $Call = F::Live($Auditor, $Call);
+        if (in_array($Call['Run']['Service'], $Call['No SEO Audit']))
+            F::Log('SEO Audit skipped', LOG_INFO, 'Marketing');
+        else
+            if (isset($Call['View']['HTML']) && !isset($Call['Headers']['Location:']))
+                foreach($Call['Auditors'] as $Auditor)
+                    $Call = F::Live($Auditor, $Call);
 
         return $Call;
     });
