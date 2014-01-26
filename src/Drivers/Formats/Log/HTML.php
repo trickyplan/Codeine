c53a5c57e220304d67fbddb471aa378de2293905
Dmitry Bergstein <bergstein@one2team.ru>
2014-01-26 18:07:38 +0400

    HTML Log Improvements.

--- src/Drivers/Formats/Log/HTML.php
+++ src/Drivers/Formats/Log/HTML.php
@@ -9,16 +9,22 @@
 
     setFn('Write', function ($Call)
     {
-        $Output = '<table class="console"><tr class="'.$Call['Levels'][11].'"><td colspan="3">'.$Call['Channel'].' Channel ('.count($Call['Value']).')</td></tr><tr><td colspan="3">'.date(DATE_RSS, round(Started)).' *'.$Call['UA'].'* from *'.$Call['IP'].'*</td></tr>';
+        $Header = $Call['Channel'].' Channel ('.count($Call['Value']).')</td></tr><tr><td colspan="3">'.date(DATE_RSS, round(Started)).' *'.$Call['UA'].'* from *'.$Call['IP'].'*';
 
-        foreach ($Call['Value'] as $IX => $Row)
-            $Output .= '<tr class="'.$Call['Levels'][$Row[0]].'">
-                    <td>'.$Row[1].'</td>
-                    <td>'.($Row[3] == (isset($Call['Value'][$IX-1][3])? $Call['Value'][$IX-1][3]: false)? '': $Row[3]).'</td>
-                    <td>'.stripslashes(htmlentities($Row[2])).'</td>
-                    </tr>';
+        if (is_array($Call['Value']))
+        {
+            $Output = '<table class="console"><tr class="'.$Call['Levels'][11].'"><td colspan="3">'.$Header.'</td></tr>';
 
-        $Output .= '</table>';
+            foreach ($Call['Value'] as $IX => $Row)
+                $Output .= '<tr class="'.$Call['Levels'][$Row[0]].'">
+                        <td>'.$Row[1].'</td>
+                        <td>'.($Row[3] == (isset($Call['Value'][$IX-1][3])? $Call['Value'][$IX-1][3]: false)? '': $Row[3]).'</td>
+                        <td>'.stripslashes(htmlentities($Row[2])).'</td>
+                        </tr>';
+            $Output .= '</table>';
+        }
+        else
+            $Output = $Header.PHP_EOL.$Call['Value'];
 
         if ($Call['Asterisk Support'])
             $Output = preg_replace('/\*(.*)\*/SsUu', '<strong>$1</strong>', $Output);
