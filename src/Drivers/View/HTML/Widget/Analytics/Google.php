5c95a5e8d0610330118eaa31ec5b9d16c86df5ee
Dmitry Bergstein <bergstein@one2team.ru>
2014-02-04 17:44:02 +0400

    GA Link Attribution

--- src/Drivers/View/HTML/Widget/Analytics/Google.php
+++ src/Drivers/View/HTML/Widget/Analytics/Google.php
@@ -53,6 +53,7 @@
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
       ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
+      ga('require', 'linkid', 'linkid.js');
       ga('send', 'pageview');
     </script>";
         }
