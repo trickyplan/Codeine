181d831c1717c7cbecc31f5ee51f69ba3e3aea63
Dmitry Bergstein <bergstein@one2team.ru>
2014-02-04 17:32:33 +0400

    GA Options.

--- src/Drivers/View/HTML/Widget/Analytics/Google.php
+++ src/Drivers/View/HTML/Widget/Analytics/Google.php
@@ -12,11 +12,11 @@
         // FIXME Templatize
         // GA FIXME Options
 
-        if (isset($Call['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
+        if (isset($Call['Analytics']['Google']['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
             $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
         else
         {
-            if (isset($Call['Demography']) && $Call['Demography'])
+            if (isset($Call['Analytics']['Google']['Demography']) && $Call['Analytics']['Google']['Demography'])
                 $Source = "ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'";
             else
                 $Source = "ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'";
@@ -40,17 +40,22 @@
     {
         // FIXME Templatize
         // GA FIXME Options
-        if (isset($Call['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
+        if (isset($Call['Analytics']['Google']['DNT Support']) && F::Run('System.Interface.Web.DNT', 'Detect', $Call))
             $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
         else
-            $Code = "<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
-  ga('send', 'pageview');
-</script>";
+        {
+            if (in_array($Call['HTTP']['URL'], $Call['Analytics']['Google']['URLs']['Disabled']))
+                $Code = '';
+            else
+                $Code = "<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
+      ga('send', 'pageview');
+    </script>";
+        }
 
         return $Code;
     });
