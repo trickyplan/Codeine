49a95f274a5253cecef3a1505a1395979143a44c
Dmitry Bergstein <bergstein@one2team.ru>
2014-03-26 13:10:51 +0400

    Log 404's referer

--- src/Drivers/Error/404.php
+++ src/Drivers/Error/404.php
@@ -11,7 +11,10 @@
     {
         $Call['HTTP']['Headers']['HTTP/1.1'] = '404 Not Found';
 
-        F::Log('Not found: '.$Call['HTTP']['URI'], $Call['404 Error Level'], 'Developer');
+        F::Log('Page not found: '.$Call['HTTP']['URI'], $Call['404 Error Level'], 'Developer');
+
+        if (isset($Call['HTTP']['Referer']))
+            F::Log('Referrer: '.$Call['HTTP']['Referer'], $Call['404 Error Level'], 'Developer');
 
         $Call['Layouts'] = [
             [
