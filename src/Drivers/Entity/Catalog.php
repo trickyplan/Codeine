cec121167b122397db43f9fdfb1bebde703d46a8
Dmitry Bergstein <bergstein@one2team.ru>
2014-04-07 06:08:35 +0400

    Performance fix.

--- src/Drivers/Entity/Catalog.php
+++ src/Drivers/Entity/Catalog.php
@@ -22,8 +22,9 @@
         if (count($Elements) > 0)
         {
             foreach ($Elements as $Element)
-                $Values[$Element[$Call['Key']]] = F::Run('Entity', 'Count', $Call,
+                $Values[$Element[$Call['Key']]] = F::Run('Entity', 'Count',
                     [
+                        'Entity' => $Call['Entity'],
                         'Where' =>
                         [
                             $Call['Key'] => $Element[$Call['Key']]
