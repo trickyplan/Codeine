4c389951e78670daeb317d8e54681b3876139df8
Dmitry Bergstein <bergstein@one2team.ru>
2014-04-07 21:21:45 +0400

    Fixes.

--- src/Drivers/Text/Cut/Words.php
+++ src/Drivers/Text/Cut/Words.php
@@ -12,7 +12,12 @@
         $Words = preg_split('/[\s,]+/', $Call['Value']);
 
         if (count($Words) > $Call['Words'])
-            $Cutted = mb_substr($Call['Value'], 0, mb_strpos($Call['Value'], $Words[$Call['Words']])-1);
+        {
+            if (empty($Call['Value']))
+                $Cutted = $Call['Value'];
+            else
+                $Cutted = mb_substr($Call['Value'], 0, mb_strpos($Call['Value'], $Words[$Call['Words']])-1);
+        }
         else
             $Cutted = $Call['Value'];
 
