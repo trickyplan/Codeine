fa4ae624723e4c6ef841da2f273f21baf495d022
Dmitry Bergstein <bergstein@trickyplan.ru>
2014-04-23 00:06:34 +0400

    Fix.

--- src/Drivers/Text/Cut/Words.php
+++ src/Drivers/Text/Cut/Words.php
@@ -13,7 +13,7 @@
 
         if (count($Words) > $Call['Words'])
         {
-            if (empty($Call['Value']))
+            if (empty($Words[$Call['Words']]))
                 $Cutted = $Call['Value'];
             else
                 $Cutted = mb_substr($Call['Value'], 0, mb_strpos($Call['Value'], $Words[$Call['Words']])-1);
