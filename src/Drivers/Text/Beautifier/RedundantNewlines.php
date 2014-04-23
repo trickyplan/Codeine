5412754a92fd2accd1c715b37b293c04e9a882c6
Dmitry Bergstein <bergstein@trickyplan.ru>
2014-04-23 14:09:27 +0400

    Beautifier fixes.

--- src/Drivers/Text/Beautifier/RedundantNewlines.php
+++ src/Drivers/Text/Beautifier/RedundantNewlines.php
@@ -14,7 +14,6 @@
         if ($Length > 0)
         {
             $Ratio = preg_match_all('/(<br\/?>)/m', $Call['Value'])/$Length;
-
             if ($Ratio > $Call['Redundant Newlines']['Max Ratio'])
                 $Call['Value'] =  preg_replace('/(<br\/?>)/m', '', $Call['Value']);
 
