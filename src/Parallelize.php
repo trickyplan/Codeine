a2964a0e3feaf5e25fc3272bee10af00432cacb5
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-05-06 19:51:40 +0400

    EMail credentials.

---
+++ src/Parallelize.php
@@ -0,0 +1,41 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    include 'Core.php';
+
+    $Values = [];
+
+    for ($Z = 1; $Z<200000; $Z++)
+        $Values[] = rand(0, 100000);
+
+    $Values = array_chunk($Values, count($Values)/8);
+
+    foreach ($Values as $Value)
+    {
+        $PID = pcntl_fork();
+
+        if ($PID == -1)
+        {
+            F::Log('Daemon: Fork failed', LOG_CRIT);
+            return -1;
+        } //TODO: ошибка - не смогли создать процесс
+        elseif ($PID)
+        {
+            F::Log('Child forked '.$PID, LOG_INFO);
+            $Ungrateful[] = $PID;
+        }
+        else
+        {
+            unset ($Values);
+            sleep(5);
+            break;
+        }
+    }
+
+    pcntl_wait($Status);
