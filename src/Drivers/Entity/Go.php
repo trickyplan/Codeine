63d4af4838a72ecf926fe654bdd6c8b99cf955ad
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-06-26 15:02:10 +0400

    Entity.Go Action.

---
+++ src/Drivers/Entity/Go.php
@@ -0,0 +1,22 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    setFn('Do', function ($Call)
+    {
+        $Call = F::Run('Entity', 'Load', $Call);
+
+        $Call = F::Hook('beforeEntityGo', $Call);
+
+            $Call['Data'] = F::Run('Entity', 'Read', $Call);
+            $Call = F::Run('System.Interface.HTTP', 'Remote Redirect', $Call, ['Location' => $Call['Data']['URL']]);
+
+        $Call = F::Hook('afterEntityGo', $Call);
+
+        return $Call;
+    });
