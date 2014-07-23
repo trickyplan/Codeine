3709c0ebc38e005eefad865d2f269f585e73da64
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-07-24 01:13:45 +0400

    No counters on development and staging.

--- src/Drivers/View/HTML/Widget/Analytics/Google.php
+++ src/Drivers/View/HTML/Widget/Analytics/Google.php
@@ -40,23 +40,28 @@
     {
         // FIXME Templatize
         // GA FIXME Options
+        $Code = '';
+
         if (isset($Call['Analytics']['Google']['DNT Support']) && F::Run('System.Interface.HTTP.DNT', 'Detect', $Call))
             $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
         else
         {
             if (in_array($Call['HTTP']['URL'], $Call['Analytics']['Google']['URLs']['Disabled']))
-                $Code = '';
+                ;
             else
-                $Code = "<script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
-      ga('require', 'linkid', 'linkid.js');
-      ga('require', 'displayfeatures');
-      ga('send', 'pageview');
-    </script>";
+            {
+                if (F::Environment() == 'Production')
+                    $Code = "<script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
+          ga('require', 'linkid', 'linkid.js');
+          ga('require', 'displayfeatures');
+          ga('send', 'pageview');
+        </script>";
+            }
         }
 
         return $Code;
--- src/Drivers/View/HTML/Widget/Analytics/Yandex.php
+++ src/Drivers/View/HTML/Widget/Analytics/Yandex.php
@@ -12,35 +12,20 @@
         // FIXME Templatize
 
         $Call['Options']['id'] = $Call['ID'];
+        $Code = '';
 
         if (isset($Call['DNT Support']) && F::Run('System.Interface.HTTP.DNT', 'Detect', $Call))
             $Code = '<!-- Do Not Track enabled. Yandex Metrics supressed. -->';
         else
-            $Code = '
-            <!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-(function (d, w, c) {
-    (w[c] = w[c] || []).push(function() {
-        try {
-            w.yaCounter13852573 = new Ya.Metrika('.json_encode($Call['Options']).');
-        } catch(e) {}
-    });
-
-    var n = d.getElementsByTagName("script")[0],
-        s = d.createElement("script"),
-        f = function () { n.parentNode.insertBefore(s, n); };
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-    if (w.opera == "[object Opera]") {
-        d.addEventListener("DOMContentLoaded", f);
-    } else { f(); }
-})(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="//mc.yandex.ru/watch/13852573" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-    ';
+        {
+            if ((F::Environment() == 'Production'))
+            {
+                $Code = '
+<!-- Yandex.Metrika counter -->
+(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter'.$Call['Options']['id'].' = new Ya.Metrika({id:'.json_encode($Call['Options']).', webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
+<!-- /Yandex.Metrika counter -->';
+            }
+        }
 
         return $Code;
      });
