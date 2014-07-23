3904e7ec11ec19c7a96eda67ec7b26337c8a77eb
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-07-24 01:40:05 +0400

    Hotfix.

--- src/Drivers/View/HTML/Widget/Analytics/Yandex.php
+++ src/Drivers/View/HTML/Widget/Analytics/Yandex.php
@@ -20,10 +20,11 @@
         {
             if ((F::Environment() == 'Production'))
             {
-                $Code = '<script>
-<!-- Yandex.Metrika counter -->
+                $Code = '<!-- Yandex.Metrika counter -->
+<script>
 (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter'.$Call['Options']['id'].' = new Ya.Metrika({id:'.json_encode($Call['Options']).', webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
-<!-- /Yandex.Metrika counter --></script>';
+</script>
+<!-- /Yandex.Metrika counter -->';
             }
         }
 
