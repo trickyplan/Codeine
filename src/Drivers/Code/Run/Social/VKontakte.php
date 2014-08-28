2d72d5103832425d1c044154900e2f192e866cb8
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-08-28 23:27:07 +0400

    Access Token, fixes.

--- src/Drivers/Code/Run/Social/VKontakte.php
+++ src/Drivers/Code/Run/Social/VKontakte.php
@@ -9,28 +9,59 @@
 
     setFn('Run', function ($Call)
     {
-        $Call['Call']['param_v'] = $Call['VKontakte']['Version'];
+        $Result = null;
 
-        $Query = '?'.http_build_query($Call['Call']);
-        $Result = F::Run('IO', 'Read',
-               [
-                   'Storage' => 'Web',
-                   'Format'  => 'Formats.JSON',
-                   'Where'   => $Call['VKontakte']['Entry Point'].'/'.$Call['Service'].'.'.$Call['Method'].$Query
-               ]);
+        $Call = F::Hook('beforeVKontakteRun', $Call);
 
-        $Result = array_pop($Result);
+            $Call['Call']['param_v'] = $Call['VKontakte']['Version'];
 
-        if (isset($Result['response']))
-        {
-            if (isset($Call['Return Key']) && (F::Dot($Result['response'], $Call['Return Key']) !== null))
-                return F::Dot($Result['response'], $Call['Return Key']);
+            $Query = '?'.http_build_query($Call['Call']);
+            $Result = F::Run('IO', 'Read',
+                   [
+                       'Storage' => 'Web',
+                       'Format'  => 'Formats.JSON',
+                       'Where'   => $Call['VKontakte']['Entry Point'].'/'.$Call['Service'].'.'.$Call['Method'].$Query
+                   ]);
+
+            $Result = array_pop($Result);
+
+            if (isset($Result['response']))
+            {
+                if (isset($Call['Return Key']) && (F::Dot($Result['response'], $Call['Return Key']) !== null))
+                    $Result = F::Dot($Result['response'], $Call['Return Key']);
+                else
+                    $Result = $Result['response'];
+            }
             else
-                return $Result['response'];
-        }
+                F::Log($Result['error']['error_msg'], LOG_ERR);
+
+        F::Hook('afterVKontakteRun', $Call);
+
+        return $Result;
+    });
+
+    setFn('Access Token', function ($Call)
+    {
+        if (isset($Call['Session']['User']['VKontakte']['Auth']))
+            $Token = $Call['Session']['User']['VKontakte']['Auth'];
         else
-        {
-            F::Log($Result['error'], LOG_ERR);
-            return null;
-        }
+            $Token =
+                    F::Run ('Entity', 'Read',
+                        [
+                            'Entity' => 'User',
+                            'Where'  =>
+                            [
+                                'VKontakte.Auth' =>
+                                [
+                                    '$exists' => true
+                                ]
+                            ],
+                            'Sort' =>
+                            [
+                                'Modified' => true
+                            ],
+                            'One' => true
+                        ])['VKontakte']['Auth'];
+
+        return $Token;
     });
