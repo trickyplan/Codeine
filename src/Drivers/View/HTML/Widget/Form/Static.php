06e944fbab515bc0f268a3addf4239a2dc509b60
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-09-28 23:17:15 +0400

    Fixes.

---
+++ src/Drivers/View/HTML/Widget/Form/Static.php
@@ -0,0 +1,13 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description HTML Textfield Driver 
+     * @package Codeine
+     * @version 7.x
+     */
+
+     setFn('Make', function ($Call)
+     {
+         return $Call;
+     });
