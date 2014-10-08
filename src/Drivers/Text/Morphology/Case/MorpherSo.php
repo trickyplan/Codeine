03e7ea1a622a594d68ec3a7ba68422f0cb921f3a
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-10-08 18:27:07 +0400

    Fixes.

---
+++ src/Drivers/Text/Morphology/Case/MorpherSo.php
@@ -0,0 +1,13 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description  
+     * @package Codeine
+     * @version 7.x
+     */
+
+    setFn('Convert', function ($Call)
+    {
+        return morpher_inflect($Call['Value'], $Call['Morpher']['Cases map'][$Call['Case']]);
+    });
