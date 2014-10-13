4d234239e170d9533e630470812659d243dd3090
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-10-13 13:29:06 +0400

    Fixes.

--- src/Assets/Default/js/activelink.js
+++ src/Assets/Default/js/activelink.js
@@ -1,10 +1,18 @@
-$(document).ready(function ()
-{
-    var str = location.href.toLowerCase();
+$(function () {
+    // show current menu object highlighted
+    var url = window.location.pathname;
+    var checkString;
 
-    $("li a, td a").each(function ()
-    {
-        if (this.href.toLowerCase() == str)
-            $(this).parent().addClass("active");
+    // now grab every link from the navigation
+    $('a').each(function () {
+        // and test its href against the url pathname
+        checkString = url.match($(this).attr('href'));
+        if ((checkString != null && $(this).attr('href') != '/') || (url == $(this).attr('href'))) {
+            {
+                $(this).addClass('active');
+                $(this).parent().addClass('active');
+            }
+        }
     });
+
 });
