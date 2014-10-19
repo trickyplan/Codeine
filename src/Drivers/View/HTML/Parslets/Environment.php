ca270ce865df2daa7897f0dd37da84689c57b198
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-10-19 21:38:47 +0400

    Fixes.

--- src/Drivers/View/HTML/Parslets/Environment.php
+++ src/Drivers/View/HTML/Parslets/Environment.php
@@ -13,12 +13,22 @@
         {
             $Root = simplexml_load_string('<root '.$Call['Parsed'][1][$IX].'></root>');
 
-            $Environment = isset($Root->attributes()->eq)? (string) $Root->attributes()->eq: 'Production';
+            $Environment = isset($Root->attributes()->eq)? (string) $Root->attributes()->eq: null;
 
             if ($Environment == F::Environment())
                 $Outer = $Match;
             else
                 $Outer = '';
+
+            $NotEnvironment = isset($Root->attributes()->neq)? (string) $Root->attributes()->neq: null;
+
+            if ($NotEnvironment !== null)
+            {
+                if ($NotEnvironment == F::Environment())
+                    $Outer = '';
+                else
+                    $Outer = $Match;
+            }
 
             $Call['Output'] = str_replace ($Call['Parsed'][0][$IX], $Outer, $Call['Output']);
         }
