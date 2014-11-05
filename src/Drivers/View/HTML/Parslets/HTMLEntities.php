ceb116da99262eb7f7aca814465eeda0af1d0016
Дмитрий Бергштейн <bergstein@trickyplan.ru>
2014-11-05 06:54:06 +0300

    New Parslet — HTML Entities. URLEncode renamed.

---
+++ src/Drivers/View/HTML/Parslets/HTMLEntities.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* Codeine
+     * @author BreathLess
+     * @description Exec Parslet 
+     * @package Codeine
+     * @version 6.0
+     */
+
+     setFn('Parse', function ($Call)
+     {
+         foreach ($Call['Parsed'][2] as $Ix => $Match)
+         {
+             $Call['Output'] = str_replace ($Call['Parsed'][0][$Ix],
+                 htmlentities($Match)
+                 ,$Call['Output']);
+         }
+
+        return $Call;
+     });
