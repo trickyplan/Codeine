9253d762b80d37cf39b153e16580019b22a2ba86
alex <alex@captchabot.com>
2014-11-18 11:08:24 +0200

    issue 1795

--- src/Drivers/Entity/Touch.php
+++ src/Drivers/Entity/Touch.php
@@ -13,7 +13,35 @@
 
         $Call = F::Hook('beforeTouch', $Call);
 
-            $Results = F::Run('Entity', 'Update', $Call, ['One' => false]);
+        $Results = F::Run('Entity', 'Update', $Call, ['One' => false]);
+
+        $Call['Output']['Content'][] =
+            [
+                'Type' => 'Block',
+                'Value' => count($Results).' touched'
+            ];
+
+     //   $Call = F::Hook('afterTouch', $Call);
+
+        return $Call;
+    });
+
+    setFn('All', function ($Call)
+    {
+        $Call   = F::Apply('Entity', 'Load', $Call);
+        $Total  = F::Run('Entity', 'Count', $Call);
+        $Amount = ceil($Total/$Call['All']['Limit']);
+
+        for ($i = 0; $i < $Amount; $i++)
+        {
+            $Call = F::Hook('beforeTouch', $Call);
+            $Results = F::Run('Entity', 'Update', $Call, 
+                        [
+                        'One' => false,
+                        'Limit' => ['From' => $i*$Call['All']['Limit'],
+                                    'To'   => ($i+1)*$Call['All']['Limit']
+                                    ]
+                        ]);
 
             $Call['Output']['Content'][] =
                 [
@@ -22,6 +50,11 @@
                 ];
 
      //   $Call = F::Hook('afterTouch', $Call);
-
+        }
         return $Call;
+    });
+
+    setFn('Test', function ($Call)
+    {
+        F::Run(null, "All", $Call, ['Entity' => 'User', 'Where' => ['ID'=> ['$gt'=>3]],'Live Fields' => ['VKontakte.DOB']]);
     });
