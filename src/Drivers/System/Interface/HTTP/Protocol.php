fb85a92ffcb6997b940d86232c760160419009e3
Dmitry Bergstein <bergstein@trickyplan.ru>
2014-11-29 02:08:17 +0300

    Fixes.

--- src/Drivers/System/Interface/HTTP/Protocol.php
+++ src/Drivers/System/Interface/HTTP/Protocol.php
@@ -12,18 +12,19 @@
         if (
                (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']))
                 or (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
-                or (isset($_SERVER['HTTP_X_HTTPS']) &&$_SERVER['HTTP_X_HTTPS']))
-            {
-                $Call['HTTP']['Proto'] = 'https://';
-            }
+                or (isset($_SERVER['HTTP_X_HTTPS']) &&$_SERVER['HTTP_X_HTTPS'])
+            )
+            $Call['HTTP']['Proto'] = 'https://';
         else
             $Call['HTTP']['Proto'] = 'http://';
 
         if (isset($Call['HTTP']['Force SSL']) && $Call['HTTP']['Force SSL'])
         {
             if ($Call['HTTP']['Proto'] !== 'https://')
-                $Call = F::Run(null, 'Redirect', $Call, ['Location' => 'https://'.$Call['HTTP']['Host'].$Call['HTTP']['URI']]);
-            elseif (isset($Call['HTTP']['HSTS']['Enabled']) && $Call['HTTP']['HSTS']['Enabled'])
+                $Call = F::Run(null, 'Redirect', $Call,
+                    ['Location' => 'https://'.$Call['HTTP']['Host'].$Call['HTTP']['URI']]);
+
+            if (isset($Call['HTTP']['HSTS']['Enabled']) && $Call['HTTP']['HSTS']['Enabled'])
             {
                 $Header = 'max-age='.$Call['HTTP']['HSTS']['Expire'];
 
@@ -31,9 +32,8 @@
                     $Header.= '; includeSubdomains';
 
                 $Call['HTTP']['Headers']['Strict-Transport-Security:'] = $Header;
-            }
-            else
                 $Call['HTTP']['Proto'] = 'https://';
+            }
         }
 
         if (empty($Call['HTTP']['Proto']))
