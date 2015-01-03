c78bc29b7371ce346efd40e9e515007346b3232b
Dmitry Bergstein <bergstein@trickyplan.com>
2015-01-03 13:55:31 +0300

    Off Arrow

--- src/Assets/Default/js/arrow-navigation.js
+++ src/Assets/Default/js/arrow-navigation.js
@@ -1,2 +1,3 @@
+/*
 $(document).bind('keydown', 'Ctrl+left', function() { window.location.replace($('a[rel="prev"]').attr('href'));} );
-$(document).bind('keydown', 'Ctrl+right', function() { window.location.replace($('a[rel="next"]').attr('href'));} );
+$(document).bind('keydown', 'Ctrl+right', function() { window.location.replace($('a[rel="next"]').attr('href'));} );*/
