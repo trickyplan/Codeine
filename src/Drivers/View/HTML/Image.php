58634c296596395395c96c22b554b4c6c854b3a1
Dmitry Bergstein <bergstein@trickyplan.com>
2015-01-09 11:50:21 +0300

    Fix

--- src/Drivers/View/HTML/Image.php
+++ src/Drivers/View/HTML/Image.php
@@ -67,7 +67,7 @@
                             (isset($Call['Current Image']['Width'])? $Call['Current Image']['Width']: 0),
                             (isset($Call['Current Image']['Height'])? $Call['Current Image']['Height']: 0),
                             $Call['Current Image']['Source']['Scope'],
-                            strtr(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_PATH), '/', '.')
+                            strtr(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_PATH).parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_QUERY), '/', '.')
             ]);
 
         return $Call;
