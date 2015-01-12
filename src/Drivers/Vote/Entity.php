51ae8165b6b4cd8d48d3e56457278d4c228e6428
Dmitry Bergstein <bergstein@trickyplan.com>
2015-01-12 12:58:28 +0300

    Fix

--- src/Drivers/Vote/Entity.php
+++ src/Drivers/Vote/Entity.php
@@ -26,9 +26,20 @@
 
     setFn('Ratio', function ($Call)
     {
+        if (isset($Call['Data']['VoteFor']))
+            ;
+        else
+            $Call['Data']['VoteFor'] = 0;
+
+        if (isset($Call['Data']['VoteAgainst']))
+            ;
+        else
+            $Call['Data']['VoteAgainst'] = 0;
+
         $Sum = $Call['Data']['VoteFor']+$Call['Data']['VoteAgainst'];
+
         if ($Sum == 0)
-            return 100;
+            return 1;
         else
             return ($Call['Data']['VoteFor'] / $Sum);
     });
