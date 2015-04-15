745a098bcc2253e8ed78e121ec72678cf781b947
Dmitry Bergstein <bergstein@trickyplan.com>
2015-04-15 19:17:03 +0300

    Fix.

--- src/Drivers/View/HTML/Image.php
+++ src/Drivers/View/HTML/Image.php
@@ -179,7 +179,7 @@
         if (null === $Call['Current Image']['Source']['Where'] ||
             !F::Run('IO', 'Execute', ['Execute' => 'Exist'], $Call['Current Image']['Source']))
         {
-            F::Log('Image not found:'.$Call['Current Image']['Source']['Where']['ID'], LOG_BAD);
+            F::Log('Image not found:'.$Call['Current Image']['Source']['Where']['ID'], LOG_INFO);
             return null;
         }
 
