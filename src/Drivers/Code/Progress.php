173de0ac17f183dd1f78d2fa4e51c3af7e950300
Dmitry Bergstein <bergstein@trickyplan.com>
2015-05-16 03:05:42 +0300

    Fix.

--- src/Drivers/Code/Progress.php
+++ src/Drivers/Code/Progress.php
@@ -19,14 +19,18 @@
     {
         $Call['Progress']['Ticks']++;
 
-        // if ($Call['Progress']['Ticks'] % $Call['Progress']['Ticks per log'] == 0)
+        if ($Call['Progress']['Ticks'] % $Call['Progress']['Ticks per log'] == 0)
         {
             $Call['Progress']['Percent'] = $Call['Progress']['Now']/($Call['Progress']['Max']-$Call['Progress']['Min']);
             $Call['Progress']['Elapsed'] = microtime(true) - $Call['Progress']['Started'];
             $Call['Progress']['Estimated'] = ((1/$Call['Progress']['Percent']) * $Call['Progress']['Elapsed']) - $Call['Progress']['Elapsed'];
-            F::Log('Progress: '.(round($Call['Progress']['Percent'], $Call['Progress']['Precision'])*100).'% '.($Call['Progress']['Now'].'/'.($Call['Progress']['Max']-$Call['Progress']['Min'])), LOG_NOTICE, 'Developer');
-            F::Log('Elapsed: '.round($Call['Progress']['Elapsed'], $Call['Progress']['Precision']).' sec.', LOG_NOTICE, 'Developer');
-            F::Log('Estimated: '.round($Call['Progress']['Estimated'], $Call['Progress']['Precision']).' sec.', LOG_NOTICE, 'Developer');
+
+            if (PHP_SAPI == 'cli')
+            {
+                F::Log('Progress: '.(round($Call['Progress']['Percent'], $Call['Progress']['Precision'])*100).'% '.($Call['Progress']['Now'].'/'.($Call['Progress']['Max']-$Call['Progress']['Min'])), LOG_NOTICE, 'Developer');
+                F::Log('Elapsed: '.round($Call['Progress']['Elapsed'], $Call['Progress']['Precision']).' sec.', LOG_NOTICE, 'Developer');
+                F::Log('Estimated: '.round($Call['Progress']['Estimated'], $Call['Progress']['Precision']).' sec.', LOG_NOTICE, 'Developer');
+            }
         }
 
         return $Call;
@@ -35,7 +39,8 @@
     setFn('Finish', function ($Call)
     {
         $Call['Progress']['Elapsed'] = microtime(true) - $Call['Progress']['Started'];
-        F::Log('Elapsed: '.round($Call['Progress']['Elapsed'], $Call['Progress']['Precision']), LOG_NOTICE, 'Developer');
+        if (PHP_SAPI == 'cli')
+            F::Log('Elapsed: '.round($Call['Progress']['Elapsed'], $Call['Progress']['Precision']), LOG_NOTICE, 'Developer');
         unset($Call['Progress']);
         return $Call;
     });
