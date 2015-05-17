839b2419b5fe6dd1154837a858e5582b3ef0f206
Dmitry Bergstein <bergstein@trickyplan.com>
2015-05-17 04:33:25 +0300

    Fix.

--- src/Drivers/Entity/Hooks/Live.php
+++ src/Drivers/Entity/Hooks/Live.php
@@ -21,7 +21,8 @@
                 if (isset($Call['Live Fields']) && !in_array($Name, $Call['Live Fields']))
                     continue;
 
-                F::Log('Processing node '.$Name.' for *'.$Call['Entity'].':'.$Call['Data']['ID'].'*', LOG_DEBUG);
+                if (isset($Call['Data']['ID']))
+                    F::Log('Processing node '.$Name.' for *'.$Call['Entity'].':'.$Call['Data']['ID'].'*', LOG_DEBUG);
 
                 // Если у ноды определён нужный хук
                 if (isset($Node['Hooks']) && isset($Node['Hooks'][$Call['On']]))
