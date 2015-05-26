5cb58f1d3db4e3db9d84bd498101c61e0619555f
Dmitry Bergstein <bergstein@trickyplan.com>
2015-05-26 09:41:54 +0300

    Fixes.

--- src/Drivers/Data/Type/Literal/HTML.php
+++ src/Drivers/Data/Type/Literal/HTML.php
@@ -11,7 +11,11 @@
     {
         $Call['Value'] = strip_tags($Call['Value'], $Call['Allowed Tags']);
 
-        return htmlspecialchars_decode($Call['Value'], ENT_HTML5 | ENT_QUOTES);
+        // Empty Check
+        if ('' === trim(preg_replace('/\<[\S]+?\>/Ssu', '', $Call['Value'])))
+            return null;
+        else
+            return htmlspecialchars_decode($Call['Value'], ENT_HTML5 | ENT_QUOTES);
     });
 
     setFn(['Read', 'Where'], function ($Call)
