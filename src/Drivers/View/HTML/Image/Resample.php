a3df60e5644561fb2fd09547e685e7e547684d55
Dmitry Bergstein <bergstein@trickyplan.com>
2015-06-05 16:06:28 +0300

    Resampler

--- src/Drivers/View/HTML/Image/Resample.php
+++ src/Drivers/View/HTML/Image/Resample.php
@@ -9,24 +9,26 @@
 
     setFn('Do', function ($Call)
     {
-        if (isset($Call['Current Image']['Thumb']) && !empty($Call['Current Image']['Data']))
+        if (isset($Call['Current Image']['Resample']) && !empty($Call['Current Image']['Data']))
         {
             try
             {
                 $GImage = new Gmagick();
                 $GImage->readimageblob($Call['Current Image']['Data']);
-    /*            $GImage->setCompressionQuality($Call['Image']['Quality']);*/
 
-
-                if (!isset($Call['Current Image']['Height']))
+                if (isset($Call['Current Image']['Height']))
+                    ;
+                else
                     $Call['Current Image']['Height'] =
                         ceil(($Call['Current Image']['Width']/$GImage->getimagewidth())*$GImage->getimageheight());
 
-                if (!isset($Call['Current Image']['Width']))
+                if (isset($Call['Current Image']['Width']))
+                    ;
+                else
                     $Call['Current Image']['Width'] =
                         ceil(($Call['Current Image']['Height']/$GImage->getimageheight())*$GImage->getimagewidth());
 
-                $GImage->cropthumbnailimage($Call['Current Image']['Width'], $Call['Current Image']['Height']);
+                $GImage->resizeImage($Call['Current Image']['Width'], $Call['Current Image']['Height'], Gmagick::FILTER_LANCZOS, 1);
 
                 $Call['Current Image']['Data'] = $GImage->getImageBlob();
             }
