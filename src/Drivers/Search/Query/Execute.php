d77ce1d95e4de6e29a34a4b60e8e50e89cdf38dd
Dmitry Bergstein <bergstein@trickyplan.com>
2015-06-21 07:05:51 +0300

    Fixes.

--- src/Drivers/Search/Query/Execute.php
+++ src/Drivers/Search/Query/Execute.php
@@ -9,23 +9,37 @@
      
      setFn('Do', function ($Call)
      {
+         $Call['IX'] = 0;
+
          F::Log('Search Queries: '.F::Run('Entity', 'Count', $Call,
             [
                 'Entity' => 'Search.Query'
             ]), LOG_NOTICE);
 
         $Queries = F::Run('Entity', 'Read', $Call,
+        [
+            'Entity' => 'Search.Query',
+            'Limit'     =>
             [
-                'Entity' => 'Search.Query'
-            ]);
+                'From'  => $Call['IX'] * $Call['Execute']['EPP'],
+                'To'    => $Call['Execute']['EPP']
+            ]
+        ]);
 
-        foreach ($Queries as $Query)
+        while (count($Queries) > 0)
         {
-            if (empty($Query['Query']))
-                ;
-            else
-                F::Run('Search', 'Query', [
-                    'Query' => $Query['Query']
-                ]);
+            foreach ($Queries as $Query)
+            {
+                if (empty($Query['Query']))
+                    ;
+                else
+                    F::Run('Search', 'Query', [
+                        'Query' => $Query['Query']
+                    ]);
+            }
+
+            $Call['IX']++;
         }
+
+        return $Call;
      });
