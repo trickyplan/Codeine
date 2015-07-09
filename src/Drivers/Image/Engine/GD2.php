d442be0cc85d3682a846563e4d2975d8cd9da9e6
Dmitry Bergstein <bergstein@trickyplan.com>
2015-07-09 12:42:49 +0300

    Fixes.

--- src/Drivers/Image/Engine/GD2.php
+++ src/Drivers/Image/Engine/GD2.php
@@ -23,88 +23,93 @@
 
     setFn('Load', function ($Call)
     {
-        list ($Call['Image']['Width'], $Call['Image']['Height'], $Call['Image']['Type']) = getimagesize($Call['ID']);
-
-        switch ($Call['Image']['Type'])
+        if (F::file_exists($Call['ID']))
         {
-            case IMAGETYPE_GIF:
-                $Call['Image']['Object'] = imagecreatefromgif($Call['ID']);
-                $Call['Image']['Type'] = 'GIF';
-            break;
+            list ($Call['Image']['Width'], $Call['Image']['Height'], $Call['Image']['Type']) = getimagesize($Call['ID']);
 
-        	case IMAGETYPE_JPEG:
-                $Call['Image']['Object'] = imagecreatefromjpeg($Call['ID']);
-                $Call['Image']['Type'] = 'JPEG';
-            break;
+            switch ($Call['Image']['Type'])
+            {
+                case IMAGETYPE_GIF:
+                    $Call['Image']['Object'] = imagecreatefromgif($Call['ID']);
+                    $Call['Image']['Type'] = 'GIF';
+                break;
 
-        	case IMAGETYPE_PNG:
-                $Call['Image']['Object'] = imagecreatefrompng($Call['ID']);
-                $Call['Image']['Type'] = 'PNG';
-            break;
+                case IMAGETYPE_JPEG:
+                    $Call['Image']['Object'] = imagecreatefromjpeg($Call['ID']);
+                    $Call['Image']['Type'] = 'JPEG';
+                break;
 
-        	case IMAGETYPE_SWF:
-                // TODO SWF Support
-            break;
+                case IMAGETYPE_PNG:
+                    $Call['Image']['Object'] = imagecreatefrompng($Call['ID']);
+                    $Call['Image']['Type'] = 'PNG';
+                break;
 
-        	case IMAGETYPE_PSD:
-                // TODO PSD Support
-            break;
+                case IMAGETYPE_SWF:
+                    // TODO SWF Support
+                break;
 
-        	case IMAGETYPE_BMP:
-                // TODO BMP Support
-            break;
+                case IMAGETYPE_PSD:
+                    // TODO PSD Support
+                break;
 
-        	case IMAGETYPE_TIFF_II:
-                // TODO TIFF Support
-            break;
+                case IMAGETYPE_BMP:
+                    // TODO BMP Support
+                break;
 
-        	case IMAGETYPE_TIFF_MM:
+                case IMAGETYPE_TIFF_II:
+                    // TODO TIFF Support
+                break;
 
-            break;
+                case IMAGETYPE_TIFF_MM:
 
-        	case IMAGETYPE_JPC:
-                // TODO JPC Support
-            break;
+                break;
 
-        	case IMAGETYPE_JP2:
-                // TODO JP2 Support
-            break;
+                case IMAGETYPE_JPC:
+                    // TODO JPC Support
+                break;
 
-        	case IMAGETYPE_JPX:
-                // TODO JPX Support
-            break;
+                case IMAGETYPE_JP2:
+                    // TODO JP2 Support
+                break;
 
-        	case IMAGETYPE_JB2:
-                // TODO JB2 Support
-            break;
+                case IMAGETYPE_JPX:
+                    // TODO JPX Support
+                break;
 
-        	case IMAGETYPE_SWC:
-                // TODO SWC Support
-            break;
+                case IMAGETYPE_JB2:
+                    // TODO JB2 Support
+                break;
 
-        	case IMAGETYPE_IFF:
-                // TODO IFF Support
-            break;
+                case IMAGETYPE_SWC:
+                    // TODO SWC Support
+                break;
 
-        	case IMAGETYPE_WBMP:
-                $Call['Image']['Object'] = imagecreatefromwbmp($Call['ID']);
-                $Call['Image']['Type'] = 'WBMP';
-            break;
+                case IMAGETYPE_IFF:
+                    // TODO IFF Support
+                break;
 
-        	case IMAGETYPE_XBM:
-                $Call['Image']['Object'] = imagecreatefromxbm($Call['ID']);
-                $Call['Image']['Type'] = 'XBM';
-            break;
+                case IMAGETYPE_WBMP:
+                    $Call['Image']['Object'] = imagecreatefromwbmp($Call['ID']);
+                    $Call['Image']['Type'] = 'WBMP';
+                break;
 
-            case IMAGETYPE_ICO:
-                // TODO ICO Support
-            break;
+                case IMAGETYPE_XBM:
+                    $Call['Image']['Object'] = imagecreatefromxbm($Call['ID']);
+                    $Call['Image']['Type'] = 'XBM';
+                break;
+
+                case IMAGETYPE_ICO:
+                    // TODO ICO Support
+                break;
+            }
+
+            // TODO WEBP Support
+            // TODO TGA Support
+
+            return $Call['Image'];
         }
-
-        // TODO WEBP Support
-        // TODO TGA Support
-
-        return $Call['Image'];
+        else
+            return null;
     });
 
     setFn('Save', function ($Call)
