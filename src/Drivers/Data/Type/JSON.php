75a782a221b3de063c3fd39148791c62d1e4e8dd
Dmitry Bergstein <bergstein@trickyplan.com>
2015-07-25 16:04:38 +0300

    Fix.

---
+++ src/Drivers/Data/Type/JSON.php
@@ -0,0 +1,18 @@
+<?php
+    
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description  
+     * @package Codeine
+     * @version 8.x
+     */
+
+    setFn('Write', function ($Call)
+    {
+        return jd($Call['Value']);
+    });
+
+    setFn('Read', function ($Call)
+    {
+        return j($Call['Value']);
+    });
