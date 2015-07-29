23f38cc7d13f018711c4e3dc6ff7dd64d122521a
Dmitry Bergstein <bergstein@trickyplan.com>
2015-07-29 11:13:55 +0300

    Fix.

--- src/Drivers/View/HTML/Image.php
+++ src/Drivers/View/HTML/Image.php
@@ -72,7 +72,7 @@
                             (isset($Call['Current Image']['Width'])? $Call['Current Image']['Width']: 0),
                             (isset($Call['Current Image']['Height'])? $Call['Current Image']['Height']: 0),
                             isset($Call['Current Image']['Source']['Scope'])? $Call['Current Image']['Source']['Scope']: '',
-                            sha1(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_QUERY)).strtr(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_PATH), '/', '.')
+                            sha1(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_QUERY)).'.'.sha1(strtr(parse_url($Call['Current Image']['Source']['Where']['ID'], PHP_URL_PATH), '/', '.'))
             ]);
 
         return $Call;
