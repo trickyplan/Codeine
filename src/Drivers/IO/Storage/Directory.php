c946b6ad366c3cd119bae05f520336c43b8d367c
Dmitry Bergstein <bergstein@trickyplan.com>
2015-08-02 22:37:22 +0300

    Fix.

--- src/Drivers/IO/Storage/Directory.php
+++ src/Drivers/IO/Storage/Directory.php
@@ -119,15 +119,29 @@
 
         $DirName = dirname($Filename);
 
-        if (!file_exists($DirName) || !is_dir($DirName))
+        $MakeDirectory = false;
+
+        if (file_exists($DirName))
+        {
+            if (is_dir($DirName))
+                F::Log('Directory '.$DirName.' already exists', LOG_DEBUG, 'Administrator');
+            else
+            {
+                F::Log('File '.$DirName.' already exists, removing', LOG_DEBUG, 'Administrator');
+                unlink($DirName);
+                $MakeDirectory = true;
+            }
+        }
+        else
+            $MakeDirectory = true;
+
+        if ($MakeDirectory)
         {
             if (mkdir($DirName, 0777, true)) // Fuck PHP
                 F::Log('Directory '.$DirName.' created with mode '.$Call['IO']['Directory']['Create Mode'], LOG_INFO, 'Administrator');
             else
                 F::Log('Directory '.$DirName.' cannot created', LOG_ERR, 'Administrator');
         }
-        else
-            F::Log('Directory '.$DirName.' already exists', LOG_DEBUG, 'Administrator');
 
         if (isset($Call['Data']) && ($Call['Data'] != 'null') && ($Call['Data'] != null))
         {
