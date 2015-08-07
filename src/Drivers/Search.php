a91c9ba0c35ff4f9ce93c8c09da9a222004c80e8
Dmitry Bergstein <bergstein@trickyplan.com>
2015-08-07 13:55:00 +0300

    Fix.

--- src/Drivers/Search.php
+++ src/Drivers/Search.php
@@ -29,7 +29,7 @@
             $Call['Query'] = $Call['Request']['Query']; // FIXME
 
         // FIXME
-        $Call['Query'] = preg_replace('/[^\W]+/', '', $Call['Query']);
+        $Call['Query'] = preg_replace('/[^\W\d]+/', '', $Call['Query']);
 
         $Call = F::Hook('beforeQuery', $Call);
 
