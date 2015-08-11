22866431801928aedb7ec268dd5d5645f705e71b
Dmitry Bergstein <bergstein@trickyplan.com>
2015-08-11 15:02:05 +0300

    Fix.

--- src/Drivers/View/HTML/Widget/Analytics/Google.php
+++ src/Drivers/View/HTML/Widget/Analytics/Google.php
@@ -15,17 +15,21 @@
         $Dimension = '';
 
         if (isset($Call['Analytics']['Google']['DNT Support']) && F::Run('System.Interface.HTTP.DNT', 'Detect', $Call))
+        {
             $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
+            F::Log('GA Suppressed by DNT: '.$Call['ID'], LOG_INFO, 'Marketing');
+        }
         else
         {
             if (in_array($Call['HTTP']['URL'], $Call['Analytics']['Google']['URLs']['Disabled']))
-                ;
+                F::Log('GA Suppressed by URLs: '.$Call['ID'], LOG_INFO, 'Marketing');
             else
             {
                 if (defined('Overlay'))
                     $Dimension.= "ga('set', 'dimension1', '".Overlay."');";
 
                 if (F::Environment() == 'Production')
+                {
                     $Code = "<script>
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -37,6 +41,10 @@
           ".$Dimension."
           ga('send', 'pageview');
         </script>";
+                    F::Log('GA Registered: '.$Call['ID'], LOG_INFO, 'Marketing');
+                }
+                else
+                    F::Log('GA Suppressed by Environment: '.$Call['ID'], LOG_INFO, 'Marketing');
             }
         }
 
