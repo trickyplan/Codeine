c522483b6564cca79b5caec8368c7db88458c0d7
Dmitry Bergstein <bergstein@trickyplan.com>
2015-08-16 03:30:38 +0300

    Fix.

--- src/Drivers/Text/Morphology/Case/MorpherSo.php
+++ src/Drivers/Text/Morphology/Case/MorpherSo.php
@@ -12,7 +12,16 @@
         $Value = morpher_inflect($Call['Value'], $Call['Morpher']['Cases mapping'][$Call['Case']]);
 
         if (empty($Value) or $Value{0} == '#')
+        {
+            switch ($Call['Case'])
+            {
+                case 'Praepositionalis':
+                   $Call['Value'] = 'о '.$Call['Value'];
+                break;
+            }
+
             return $Call['Value'];
+        }
         else
             return $Value;
     });
