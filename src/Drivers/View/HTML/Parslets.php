dbe9b40a3da169649125321559ac21ac666a639b
8bit <dimon130589@gmail.com>
2015-11-24 20:30:17 +0300

    change Combine patterns
    1. all Parslets in One patterns

--- src/Drivers/View/HTML/Parslets.php
+++ src/Drivers/View/HTML/Parslets.php
@@ -7,64 +7,69 @@
      * @version 8.x
      */
 
-     setFn('Process', function ($Call)
-     {
-         if (isset($Call['Output']))
-         {
-             F::Log('Start parslets processing', LOG_INFO);
-             foreach ($Call['View']['HTML']['Parslets']['Queue'] as $Parslet)
-             {
-                 $Tag = strtolower($Parslet);
+    setFn('Process', function ($Call)
+    {
+        if (isset($Call['Output']))
+        {
+            F::Log('Start parslets processing', LOG_INFO);
+            // Собираем паттерны
+            $Pass = 1;
+            $Patterns = [];
 
-                 $Pass = 1;
+            while($Pass <= $Call['View']['HTML']['Parslets']['Max Passes']) {
 
-                 $cTag = $Tag;
+                $tags = [];
+                foreach ($Call['View']['HTML']['Parslets']['Queue'] as $Parser) {
+                    $tags[strtolower($Parser)] = strtolower($Parser).($Pass > 1? $Pass : '');
+                }
 
-                 while($Call['Parsed'] = F::Run('Text.Regex', 'All',
-                     [
-                         'Pattern' => '<'.$cTag.' (.*?)>(.*?)</'.$cTag.'>',
-                         'Value'   => $Call['Output']
-                     ]))
-                 {
-                     $Call = F::Apply('View.HTML.Parslets.'.$Parslet, 'Parse', $Call);
-                     $Pass++;
+                $tags = implode('|', $tags);
+                $Patterns[$Pass][] = '<(' . $tags . ') (.*?)>(.*?)</(\1)>';
+                $Patterns[$Pass][] = '<(' . $tags . ')()>(.*?)</(\1)>';
+                $Pass++;
+            }
+            // Парсим в глубину
+            foreach($Patterns as $Order => $Variants)
+            {
+                while ( true )
+                {
+                    $Assoc = [];
 
-                     if ($Pass > 1)
-                         $cTag = $Tag.$Pass;
+                    foreach ($Variants as $Pattern)
+                    {
+                        $Parsed = F::Run('Text.Regex', 'All',
+                            [
+                                'Pattern' => $Pattern,
+                                'Value' => $Call['Output']
+                            ]);
+                        $Parsed = is_array($Parsed) ? $Parsed : [0,[]];
 
-                     if ($Pass > $Call['View']['HTML']['Parslets']['Max Passes'])
-                     {
-                         F::Log($Parslet.' Parslet raised max passes limit.', LOG_ERR);
-                         break;
-                     }
-                 }
+                        foreach ($Parsed[1] as $key => $Parser)
+                        {
+                            $ind = strtolower(
+                                $Order>1 ? substr($Parser,0,-strlen($Order)): $Parser
+                            );
+                            $Assoc[$ind][0][] = $Parsed[0][$key];
+                            $Assoc[$ind][1][] = $Parsed[2][$key];
+                            $Assoc[$ind][2][] = $Parsed[3][$key];
+                        }
+                    }
 
-                 $Pass = 1;
+                    if($Assoc === [])
+                        break;
 
-                 $cTag = $Tag;
-
-                 while($Call['Parsed'] = F::Run('Text.Regex', 'All',
-                     [
-                         'Pattern' => '<'.$cTag.'()>(.*?)</'.$cTag.'>',
-                         'Value'   => $Call['Output']
-                     ]))
-                 {
-                     $Call = F::Apply('View.HTML.Parslets.'.$Parslet, 'Parse', $Call);
-                     $Pass++;
-
-                     if ($Pass > 1)
-                         $cTag = $Tag.$Pass;
-
-                     if ($Pass > $Call['View']['HTML']['Parslets']['Max Passes'])
-                     {
-                         F::Log($Parslet.' Parslet raised max passes limit.', LOG_ERR);
-                         break;
-                     }
-                 }
-
-                 F::Log('Parslet *'.$Parslet.'* processed', LOG_DEBUG);
-             }
-         }
-         F::Log('End parslets processing', LOG_INFO);
-         return $Call;
-     });
+                    foreach ($Call['View']['HTML']['Parslets']['Queue'] as $Parser)
+                    {
+                        $ind = strtolower($Parser);
+                        if (!isset($Assoc[$ind]))
+                            continue;
+                        $Call['Parsed'] = $Assoc[$ind];
+                        $Call = F::Apply('View.HTML.Parslets.' . $Parser, 'Parse', $Call);
+                        F::Log('Parslet *' . $Parser . '* processed', LOG_DEBUG);
+                    }
+                }
+            }
+        }
+        F::Log('End parslets processing', LOG_INFO);
+        return $Call;
+    });
