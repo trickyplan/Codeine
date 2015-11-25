59a1f97b8c401960550932806a4aa4dac390c2bd
8bit <dimon130589@gmail.com>
2015-11-25 12:57:55 +0300

    fix Combine patterns
    1. Incorrect calculate sub-template

--- src/Drivers/View/HTML/Parslets.php
+++ src/Drivers/View/HTML/Parslets.php
@@ -15,11 +15,14 @@
             // Собираем паттерны
             $Pass = 1;
             $Patterns = [];
+            $MaxPass = $Call['View']['HTML']['Parslets']['Max Passes'];
+            $Queue = $Call['View']['HTML']['Parslets']['Queue'];
 
-            while($Pass <= $Call['View']['HTML']['Parslets']['Max Passes']) {
-
+            while($Pass <= $MaxPass)
+            {
                 $tags = [];
-                foreach ($Call['View']['HTML']['Parslets']['Queue'] as $Parser) {
+                foreach ($Queue as $Parser)
+                {
                     $tags[strtolower($Parser)] = strtolower($Parser).($Pass > 1? $Pass : '');
                 }
 
@@ -29,45 +32,46 @@
                 $Pass++;
             }
             // Парсим в глубину
-            foreach($Patterns as $Order => $Variants)
+            $Pass = 1;
+            while( true )
             {
-                while ( true )
-                {
-                    $Assoc = [];
+                $Variants = $Patterns[$Pass];
+                $Assoc = [];
 
-                    foreach ($Variants as $Pattern)
-                    {
-                        $Parsed = F::Run('Text.Regex', 'All',
-                            [
-                                'Pattern' => $Pattern,
-                                'Value' => $Call['Output']
-                            ]);
-                        $Parsed = is_array($Parsed) ? $Parsed : [0,[]];
+                foreach ($Variants as $Pattern) {
+                    $Parsed = F::Run('Text.Regex', 'All',
+                        [
+                            'Pattern' => $Pattern,
+                            'Value' => $Call['Output']
+                        ]);
+                    $Parsed = is_array($Parsed) ? $Parsed : [0, []];
 
-                        foreach ($Parsed[1] as $key => $Parser)
-                        {
-                            $ind = strtolower(
-                                $Order>1 ? substr($Parser,0,-strlen($Order)): $Parser
-                            );
-                            $Assoc[$ind][0][] = $Parsed[0][$key];
-                            $Assoc[$ind][1][] = $Parsed[2][$key];
-                            $Assoc[$ind][2][] = $Parsed[3][$key];
-                        }
-                    }
-
-                    if($Assoc === [])
-                        break;
-
-                    foreach ($Call['View']['HTML']['Parslets']['Queue'] as $Parser)
-                    {
-                        $ind = strtolower($Parser);
-                        if (!isset($Assoc[$ind]))
-                            continue;
-                        $Call['Parsed'] = $Assoc[$ind];
-                        $Call = F::Apply('View.HTML.Parslets.' . $Parser, 'Parse', $Call);
-                        F::Log('Parslet *' . $Parser . '* processed', LOG_DEBUG);
+                    foreach ($Parsed[1] as $key => $Parser) {
+                        $ind = strtolower(
+                            $Pass > 1 ? substr($Parser, 0, -strlen($Pass)) : $Parser
+                        );
+                        $Assoc[$ind][0][] = $Parsed[0][$key];
+                        $Assoc[$ind][1][] = $Parsed[2][$key];
+                        $Assoc[$ind][2][] = $Parsed[3][$key];
                     }
                 }
+
+                if ($Assoc === [] && $Pass == $MaxPass)
+                    break;
+
+                foreach ($Queue as $Parser) {
+                    $ind = strtolower($Parser);
+                    if (!isset($Assoc[$ind]))
+                        continue;
+                    $Call['Parsed'] = $Assoc[$ind];
+                    $Call = F::Apply('View.HTML.Parslets.' . $Parser, 'Parse', $Call);
+                    F::Log('Parslet *' . $Parser . '* processed', LOG_DEBUG);
+                }
+
+                $Pass++;
+
+                if ($Assoc !== [] && $Pass > 1)
+                    $Pass = 1;
             }
         }
         F::Log('End parslets processing', LOG_INFO);
