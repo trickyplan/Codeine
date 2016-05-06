7bcb1ff898c1677ff84e46bbdbd0d1523eca250e
Dmitry Bergstein <bergstein@trickyplan.com>
2016-05-06 18:42:31 +0300

    Small Time Fix.

---
+++ src/Drivers/System/Time/Day.php
@@ -0,0 +1,15 @@
+<?php
+
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description: time() wrapper
+     * @package Codeine
+     * @version 8.x
+     * @date 09.03.11
+     * @time 16:34
+     */
+
+    setFn('Get', function ($Call)
+    {
+        return date('Y.m.d');
+    });
