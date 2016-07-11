b8723d0a7a2a8ba5ad1b1d9fce462b1b6ac4954a
Dmitry Bergstein <bergstein@trickyplan.com>
2016-07-11 08:54:50 +0300

    Fix.

--- src/Assets/Default/js/activelink.js
+++ src/Assets/Default/js/activelink.js
@@ -1,15 +1,10 @@
 // show current menu object highlighted
 var url = decodeURI(window.location.pathname+window.location.search);
-
-console.info(url);
 // now grab every link from the navigation
 $('.nav a').each(function () {
         // and test its href against the url pathname
-        console.info(url);
-        console.info($(this).attr('href'));
         if (($(this).attr('href') != '/') && (url == $(this).attr('href')))
         {
-            console.log('Active link');
             $(this).addClass('active');
             $(this).parent().addClass('active');
             return false;
