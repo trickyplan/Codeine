d1e909b9e481c544c43c7cf6c17cffcd74ca5c92
Dmitry Bergstein <bergstein@trickyplan.com>
2016-09-14 11:00:06 +0300

    Another small fix for great good and stop shitting logs.

--- src/Drivers/Gamification/Achievement/List.php
+++ src/Drivers/Gamification/Achievement/List.php
@@ -10,26 +10,29 @@
      setFn('Do', function ($Call)
      {
          $Achievements = F::loadOptions('Gamification.Achievement')['Achievements'];
-
+         
          foreach ($Achievements as $Achievement)
-             if ($Check = F::Run('Gamification.Achievement.'.$Achievement, 'Check', $Call))
+         {
+             $Award = F::Run('Entity', 'Read',
+             [
+                 'Entity' => 'Gamification.Achievement',
+                 'Where'  =>
+                 [
+                     'Award'    => $Achievement,
+                     'User'     => $Call['User']
+                 ],
+                 'One'  => true
+             ]);
+             
+             if ($Check = F::Run('Gamification.Achievement.'.$Achievement, 'Check', $Call) and !empty($Award))
              {
-                 $Call['Output']['Content'][] =
-                     [
-                         'Type'     => 'Template',
-                         'Scope'    => 'Gamification/Achievement/Show',
-                         'ID'       => 'Short',
-                         'Data'     => F::Run('Entity', 'Read',
+                     $Call['Output']['Content'][] =
                          [
-                             'Entity' => 'Gamification.Achievement',
-                             'Where'  =>
-                             [
-                                 'Award'    => $Achievement,
-                                 'User'     => $Call['User']
-                             ],
-                             'One'  => true
-                         ])
-                     ];
+                             'Type'     => 'Template',
+                             'Scope'    => 'Gamification/Achievement/Show',
+                             'ID'       => 'Short',
+                             'Data'     => $Award
+                         ];
              }
              else
              {
@@ -37,9 +40,14 @@
                      [
                          'Type'     => 'Template',
                          'Scope'    => 'Gamification/Achievement/Show',
-                         'ID'       => 'Default'
+                         'ID'       => 'Default',
+                         'Data'     =>
+                         [
+                             'Award' => $Achievement
+                         ]
                      ];
              }
+         }
 
          return $Call;
      });
