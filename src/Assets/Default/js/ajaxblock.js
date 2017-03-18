fda6a232aac3ccee84161816177bf85b757b03ac
Dmitry Bergstein <bergstein@trickyplan.com>
2017-03-18 18:48:05 +0300

    Delayed Ajax Divs.

--- src/Assets/Default/js/ajaxblock.js
+++ src/Assets/Default/js/ajaxblock.js
@@ -31,5 +31,25 @@ $(document).ready(function ()
                 });
             }
         );
+
+        $('.ajax-delayed').on('ajax-load', function ()
+            {
+                var el = $(this);
+                if (el.attr('loaded') == true)
+                    ;
+                else
+                {
+                    $.ajax({
+                        type: 'GET',
+                        url: el.attr('data-url'),
+                        success: function(data)
+                        {
+                            el.html(data)
+                            el.attr('loaded', true);
+                        }
+                    });
+                }
+            });
+        return true;
     }
 );
