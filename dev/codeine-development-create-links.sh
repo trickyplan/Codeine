9d1975ee5158d722943f648b81e3dcb88e5180de
Dmitry Bergstein <bergstein@trickyplan.com>
2017-03-24 15:34:21 +0300

    ln -sfn for dev links.

--- dev/codeine-development-create-links.sh
+++ dev/codeine-development-create-links.sh
@@ -4,14 +4,14 @@ cd "`dirname "$0"`"
 cd ..
 Codeine=`pwd`
 echo "Codeine Work Directory: $Codeine"
-sudo ln -sf "$Codeine/etc/nginx/conf.d/codeine-development.nginx" /etc/nginx/conf.d/codeine-development.nginx
-sudo ln -sf "$Codeine/etc/nginx/conf.d/codeine-production.nginx" /etc/nginx/conf.d/codeine-production.nginx
-sudo ln -sf "$Codeine/etc/nginx/conf.d/access.conf" /etc/nginx/conf.d/access.conf
+sudo ln -sfn "$Codeine/etc/nginx/conf.d/codeine-development.nginx" /etc/nginx/conf.d/codeine-development.nginx
+sudo ln -sfn "$Codeine/etc/nginx/conf.d/codeine-production.nginx" /etc/nginx/conf.d/codeine-production.nginx
+sudo ln -sfn "$Codeine/etc/nginx/conf.d/access.conf" /etc/nginx/conf.d/access.conf
 ls -la /etc/nginx/conf.d/
-sudo ln -sf "$Codeine/src" /usr/share/php/Codeine
+sudo ln -sfn "$Codeine/src" /usr/share/php/Codeine
 ls -la /usr/share/php/Codeine
-sudo ln -sf "$Codeine/etc/init.d/codeined" /etc/init.d/codeined
+sudo ln -sfn "$Codeine/etc/init.d/codeined" /etc/init.d/codeined
 ls -la /etc/init.d/codeined
-sudo ln -sf "$Codeine/usr/bin/codeine" /usr/bin/codeine
-sudo ln -sf "$Codeine/usr/bin/codeined" /usr/bin/codeined
+sudo ln -sfn "$Codeine/usr/bin/codeine" /usr/bin/codeine
+sudo ln -sfn "$Codeine/usr/bin/codeined" /usr/bin/codeined
 ls -la /usr/bin/codeine /usr/bin/codeined
