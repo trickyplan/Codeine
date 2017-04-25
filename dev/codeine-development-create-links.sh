dedbefa23e20f061ecd1b79fa8b5b5403d7f3e40
Dmitry Bergstein <bergstein@trickyplan.com>
2017-04-25 15:18:23 +0300

    Locale Parslet. New configurations. External updates.

--- dev/codeine-development-create-links.sh
+++ dev/codeine-development-create-links.sh
@@ -4,14 +4,11 @@ cd "`dirname "$0"`"
 cd ..
 Codeine=`pwd`
 echo "Codeine Work Directory: $Codeine"
-sudo ln -sfn "$Codeine/etc/nginx/conf.d/codeine-development.nginx" /etc/nginx/conf.d/codeine-development.nginx
-sudo ln -sfn "$Codeine/etc/nginx/conf.d/codeine-production.nginx" /etc/nginx/conf.d/codeine-production.nginx
-sudo ln -sfn "$Codeine/etc/nginx/conf.d/access.conf" /etc/nginx/conf.d/access.conf
+sudo ln -sfn $Codeine/etc/nginx/conf.d/* -t /etc/nginx/conf.d/
 ls -la /etc/nginx/conf.d/
 sudo ln -sfn "$Codeine/src" /usr/share/php/Codeine
 ls -la /usr/share/php/Codeine
 sudo ln -sfn "$Codeine/etc/init.d/codeined" /etc/init.d/codeined
 ls -la /etc/init.d/codeined
-sudo ln -sfn "$Codeine/usr/bin/codeine" /usr/bin/codeine
-sudo ln -sfn "$Codeine/usr/bin/codeined" /usr/bin/codeined
-ls -la /usr/bin/codeine /usr/bin/codeined
+sudo ln -sfn "$Codeine/usr/bin/*" -t /usr/bin/
+ls -la /usr/bin/codeine*
--- src/Drivers/View/HTML/Parslets/Locale.php
+++ src/Drivers/View/HTML/Parslets/Locale.php
@@ -12,7 +12,30 @@
         $Replaces = [];
         
         foreach ($Call['Parsed']['Value'] as $IX => $Match)
-            $Replaces[$IX] = F::Run ('Locale', 'Get', $Call, ['Message' => $Match]);
+        {
+            if (strpos($Match, ',') !== false)
+                $Matches = explode(',', $Match);
+            else
+                $Matches = [$Match];
+                
+             $Translation = F::Run('Locale', 'Get', $Call, ['Message' => $Matches]);
+
+            if ($Translation === null)
+            {
+                if (F::Environment() === 'Development')
+                {
+                    $Replaces[$IX] = '<span class="nl" lang="'.$Call['Locale'].'">' . array_shift($Matches) . '</span>';
+                    echo $Replaces[$IX].PHP_EOL;
+                }
+                else
+                    $Replaces[$IX] = '';
+            }
+            else
+                if (is_scalar($Translation))
+                    $Replaces[$IX] = $Translation;
+                else
+                    $Replaces[$IX] = '{}';
+        }
 
         return $Replaces;
     });
