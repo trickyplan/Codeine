c91fc2abd132da77a46442c210e857e80398556f
Dmitry Bergstein <bergstein@trickyplan.com>
2017-05-05 18:23:27 +0300

    Fixes.

--- src/Drivers/View/HTML/Widget/Form/Password/One.php
+++ src/Drivers/View/HTML/Widget/Form/Password/One.php
@@ -9,8 +9,6 @@
 
      setFn('Make', function ($Call)
      {
-         $Call['Value'] = '';
-         $Call['HTML'] = F::Run('View.HTML.Widget.Base', 'Make', $Call, ['Tag' => 'input', 'Type' => 'password']);
          
          return $Call;
      });
