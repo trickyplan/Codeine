9133854fc394f333b71978c0ca93e67e5dccc859
Dmitry Bergstein <bergstein@trickyplan.com>
2017-05-21 12:20:12 +0300

    Trumbowyg Integration

---
+++ src/Drivers/View/HTML/Widget/Form/WYSIWYG/Trumbowyg.php
@@ -0,0 +1,13 @@
+<?php
+
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description HTML Textfield Driver 
+     * @package Codeine
+     * @version 8.x
+     */
+
+     setFn('Make', function ($Call)
+     {
+         return $Call;
+     });
