191c92fad8a6aa66a427f685270de19d6cbd7a03
Alex <alex@captchabot.com>
2017-09-14 18:24:13 +0300

    Remove debug info

--- src/Drivers/IO/Service/Mattermost.php
+++ src/Drivers/IO/Service/Mattermost.php
@@ -8,8 +8,6 @@
 
     setFn('Write', function ($Call)
     {
-        file_put_contents("/home/user/work/test.log", 'Channel'.PHP_EOL.print_r($Call['Channel'], true) . PHP_EOL, FILE_APPEND);
-        file_put_contents("/home/user/work/test.log", 'Data'.PHP_EOL.print_r($Call['Data'], true) . PHP_EOL, FILE_APPEND);
         $Data =
             [
                 'channel'  => empty($Call['MattermostChannel'])?$Call['Default']['MattermostChannel']:$Call['MattermostChannel'],
@@ -18,15 +16,13 @@
             ];
         $Data = ['payload' => j($Data)];
 
-//        return
-            $Result = F::Run('IO', 'Write',
+        return $Result = F::Run('IO', 'Write',
             [
                 'Storage' => 'Web',
                 'Where'   => $Call['Webhook URL'],
                 'Data'    => $Data
             ]);
-        file_put_contents("/home/user/work/test.log", 'Result'.PHP_EOL.print_r($Result, true) . PHP_EOL, FILE_APPEND);
-            return $Result;
+
     });
 
     setFn('Read', function ($Call)
