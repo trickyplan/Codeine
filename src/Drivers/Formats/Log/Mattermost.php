40dc1cee7e38a6204f0db224634d08f680de66fd
Alex <alex@captchabot.com>
2017-09-15 10:47:43 +0300

    Mattermost format

--- src/Drivers/Formats/Log/Mattermost.php
+++ src/Drivers/Formats/Log/Mattermost.php
@@ -4,26 +4,36 @@
     setFn('Write', function ($Call)
     {
         $Output =
-            [$Call['Channel'].' Channel ('.count($Call['Value']).')   '.$Call['Where']['ID'].PHP_EOL.
-            date(DATE_RSS, round(Started)).' *'.$Call['HTTP']['Agent'].'* from *'.$Call['HTTP']['IP'].'*'];
+            $Call['Channel'].' Channel ('.count($Call['Value']).')   '.$Call['Where']['ID'].PHP_EOL.
+            date(DATE_RSS, round(Started)).' *'.$Call['HTTP']['Agent'].'* from *'.$Call['HTTP']['IP'].'*'.PHP_EOL;
 
         if (isset($Call['Session']['User']['ID']) && !empty($Call['Session']['User']['ID']))
-            $Output[] = PHP_EOL.'User: '.$Call['Session']['User']['ID'].(isset($Call['Session']['User']['Title'])? '('.$Call['Session']['User']['Title'].')': '');
+            $Output .= PHP_EOL.'User: '.$Call['Session']['User']['ID'].
+                        (isset($Call['Session']['User']['Title'])? '('.$Call['Session']['User']['Title'].')': '').PHP_EOL;
 
-        $Output[] = '***';
-        $Output[] = '|Level|Time :watch:|Path|Message|';
-        $Output[] = '|:---:|:---:|:---|:---|';
+        $Output .= '***'.PHP_EOL;
+        $TableHeader = PHP_EOL .'|Level|Time :watch:|Path|Message|'. PHP_EOL .'|:---:|:---:|:---|:---|'. PHP_EOL;
+        $Output .= $TableHeader;
+        $TotalLen = mb_strlen($Output);
+
         foreach ($Call['Data'] as $IX => $Row)
         {
             if (is_scalar($Row[2]))
                 ;
             else
                 $Row[2] = j($Row[2]);
-            
-            $Output[] = "|".$Call['Mattermosst']['Levels'][$Call['Levels'][$Row[0]]] .' '. $Call['Levels'][$Row[0]]."|".$Row[1]."|".$Row[3]."|".stripslashes($Row[2]);
-        }
 
-        $Output = implode(PHP_EOL, $Output);
+            $Str = "|".$Call['Mattermosst']['Levels'][$Call['Levels'][$Row[0]]] .' '. $Call['Levels'][$Row[0]]."|".$Row[1]."|".$Row[3]."|".stripslashes($Row[2]).PHP_EOL;
+            $TempLen = mb_strlen($Str);
+            if (($TotalLen + $TempLen) > $Call['Mattermosst']['Max Lenthgs']) {
+                $Diff = $Call['Mattermosst']['Max Lenthgs'] - $TotalLen;
+                $Output .= str_repeat(' ', $Diff);
+                $Output .= $TableHeader;
+                $TotalLen = mb_strlen($TableHeader);
+            }
+            $TotalLen += $TempLen;
+            $Output .= $Str;
+         }
 
         return $Output;
     });
