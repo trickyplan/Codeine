45c4e3f5dc35713510dbbb6475a450b3516a0731
Dmitry Bergstein <bergstein@trickyplan.com>
2017-12-05 15:15:48 +0300

    Google Analytics template and fixes.

--- src/Drivers/View/HTML/Widget/Analytics/Google.php
+++ src/Drivers/View/HTML/Widget/Analytics/Google.php
@@ -9,38 +9,28 @@
 
     setFn('Universal', function ($Call)
     {
-        // FIXME Templatize
-        // GA FIXME Options
+        // FIXME GA Options
         $Code = '';
-        $Dimension = '';
 
-        if (isset($Call['Analytics']['Google']['DNT Support']) && F::Run('System.Interface.HTTP.DNT', 'Detect', $Call))
+        if (F::Dot($Call, 'Analytics.Google.DoNotTrack') && F::Run('System.Interface.HTTP.DNT', 'Detect', $Call))
         {
             $Code = '<!-- Do Not Track enabled. Google Analytics supressed. -->';
             F::Log('GA Suppressed by DNT: '.$Call['ID'], LOG_INFO, 'Marketing');
         }
         else
         {
-            if (in_array($Call['HTTP']['URL'], $Call['Analytics']['Google']['URLs']['Disabled']))
+            if (in_array($Call['HTTP']['URL'], F::Dot($Call, 'Analytics.Google.URLs.Disabled')))
                 F::Log('GA Suppressed by URLs: '.$Call['ID'], LOG_INFO, 'Marketing');
             else
             {
-                if (defined('Overlay'))
-                    $Dimension.= "ga('set', 'dimension1', '".Overlay."');";
-
-                if (F::Environment() == 'Production')
+                if (F::Dot($Call, 'Analytics.Google.Environment.'.F::Environment()) === true)
                 {
-                    $Code = "<script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-          ga('create', 'UA-".$Call['ID']."', '".$Call['HTTP']['Host']."');
-          ga('require', 'linkid', 'linkid.js');
-          ga('require', 'displayfeatures');
-          ".$Dimension."
-          ga('send', 'pageview');
-        </script>";
+                    $Code = F::Live(F::Run('View', 'Load', $Call,
+                        [
+                            'Scope'     => 'View.HTML.Widget.Analytics',
+                            'ID'        => 'Google'
+                        ]), $Call);
+                    
                     F::Log('GA Registered: '.$Call['ID'], LOG_INFO, 'Marketing');
                 }
                 else
