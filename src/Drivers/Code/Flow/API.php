72c995209a43c5ee97d184af7f5dc528c24bda2e
Dmitry Bergstein <bergstein@trickyplan.com>
2018-01-12 11:28:09 +0300

    Additional checks in API

--- src/Drivers/Code/Flow/API.php
+++ src/Drivers/Code/Flow/API.php
@@ -20,10 +20,10 @@
         if (isset($Call['API']['Request']['Service']))
         {
             $Call = F::loadOptions($Call['API']['Request']['Service'], 'API', $Call);
-        
+    
             F::startColor('aed581');
             // Если передан нормальный вызов, совершаем его
-        
+    
             if (F::Dot($Call, 'Skip API'))
                 F::Log('API Skipped', LOG_NOTICE);
             else
@@ -32,40 +32,40 @@
                 {
                     if (!isset($Call['API']['Request']['Method']) or empty($Call['API']['Request']['Method']))
                         $Call['API']['Request']['Method'] = 'Do';
-
+            
                     $Enabled = $Call['API'][$Call['API']['Request']['Service']][$Call['API']['Request']['Method']]['Enabled'] ?? false;
                     if ($Enabled)
                     {
                         if ($Call['API']['Response']['Access'])
                         {
-                            F::Log('API *'.$Call['API']['Request']['Service'].':'.$Call['API']['Request']['Method'].'* started', LOG_NOTICE, 'All');
-                           
+                            F::Log('API *' . $Call['API']['Request']['Service'] . ':' . $Call['API']['Request']['Method'] . '* started', LOG_NOTICE, 'All');
+                    
                             $Parameters = $Call['API'][$Call['API']['Request']['Service']][$Call['API']['Request']['Method']]['Parameters'] ?? null;
-
+                    
                             if (empty($Parameters))
                                 ;
                             else
                                 foreach ($Parameters as $Parameter)
                                     $Request = F::Dot($Request, $Parameter, F::Dot($Call['Request'], $Parameter));
-
+                    
                             $Call['Output']['Content']['Parameters'] = $Parameters;
                             $Call['Output']['Content']['Request'] = $Request;
-
+                    
                             $Call['API']['Response']['Data'] =
                                 F::Run($Call['API']['Request']['Service'], $Call['API']['Request']['Method'], $Call, $Request);
-                            
+                    
                         }
-                    }
-                    else
+                    } else
                         $Call['API']['Response']['Data'] = 'Unknown API Service or Method';
                 }
             }
-           
-            F::Log('API *'.$Call['API']['Request']['Service'].':'.$Call['API']['Request']['Method'].'* finished', LOG_NOTICE, 'All');
+    
+            F::Log('API *' . $Call['API']['Request']['Service'] . ':' . $Call['API']['Request']['Method'] . '* finished', LOG_NOTICE, 'All');
             F::stopColor();
-            
+    
             $Call = F::Merge($Call, $Call['API']['Formats'][$Call['API']['Request']['Format']]);
-            
+        }
+        
         $Call['API']['Response']['Generated'] = microtime(true);
         $Call['API']['Response']['Time'] = $Call['API']['Response']['Generated'] - $Request['Started'];
         $Call['Output']['Content']['Response'] = $Call['API']['Response'];
