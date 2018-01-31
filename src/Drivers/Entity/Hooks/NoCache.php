8c2f30b7492be308ee669f44194c0daba9e2f903
Dmitry Bergstein <bergstein@trickyplan.com>
2018-01-31 13:57:39 +0300

    Fix No Cache Check ambiguity. Disable it by default.

--- src/Drivers/Entity/Hooks/NoCache.php
+++ src/Drivers/Entity/Hooks/NoCache.php
@@ -9,12 +9,20 @@
 
     setFn('Check', function ($Call)
     {
-        if (isset($Call['HTTP']['Request']['Headers']['Pragma']) && $Call['HTTP']['Request']['Headers']['Pragma'] == 'no-cache')
-        {
-            F::Log('No cache header detected, force update initiated', LOG_INFO);
-            
-            F::Run('Entity', 'Update', $Call);
-        }
+        if (F::Dot($Call, 'No-Cache Check'))
+            if (F::Dot($Call, 'HTTP.Request.Headers.Pragma') == 'no-cache')
+            {
+                F::Log('No-cache header detected, force update initiated', LOG_INFO);
+    
+                $Data  = F::Run('Entity', 'Read', $Call, ['One' => true]);
+                
+                if (isset($Data['ID']))
+                    F::Run('Entity', 'Update', $Call,
+                        [
+                            'Where!' => $Data['ID'],
+                            'Data'  => $Data
+                        ]);
+            }
 
         return $Call;
     });
