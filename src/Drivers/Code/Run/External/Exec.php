d1e38de839f005e239c868b36245c58d41d2cbbf
Dmitry Bergstein <bergstein@trickyplan.com>
2018-02-14 22:00:44 +0300

    Exec Contract Example

--- src/Drivers/Code/Run/External/Exec.php
+++ src/Drivers/Code/Run/External/Exec.php
@@ -9,23 +9,22 @@
     
     setFn('Run', function ($Call)
     {
-        exec($Call['Command'], $Call['Run']['Result'], $Call['Run']['Code']);
+        F::Log('Will execute command: '.$Call['Command'], LOG_INFO);
+        
+            $Call = F::Hook('beforeExecRun', $Call);
+        
+                exec($Call['Command'], $Call['Run']['Result'], $Call['Run']['Code']);
+        
+            $Call = F::Hook('afterExecRun', $Call);
         
         $Call['Run']['Result'] = implode(PHP_EOL, $Call['Run']['Result']);
         
-        F::Log('Command: '.$Call['Command'], LOG_INFO);
-        
         if (empty($Call['Run']['Result']))
-            F::Log('Zero output', LOG_INFO);
+            F::Log('No output', LOG_INFO);
         else
-            F::Log('Output: '.$Call['Run']['Result'], LOG_INFO);
+            F::Log('Output: *'.mb_substr($Call['Run']['Result'], 0, $Call['Exec']['Cut']).'*', LOG_INFO);
         
-        if ($Call['Run']['Code'] > 0)
-            $Verbose = LOG_WARNING;
-        else
-            $Verbose = LOG_INFO;
-        
-        F::Log('Return Code:'.$Call['Run']['Code'], $Verbose);
+        F::Log('Return code: *'.$Call['Run']['Code'].'*', ($Call['Run']['Code'] > 0)? LOG_NOTICE: LOG_INFO);
         
         return $Call['Run'];
     });
