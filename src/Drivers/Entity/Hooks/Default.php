915413366935d8131eb043df3a551871e7f7ec35
Dmitry Bergstein <bergstein@trickyplan.com>
2018-02-20 14:21:45 +0300

    Empty as Default.

--- src/Drivers/Entity/Hooks/Default.php
+++ src/Drivers/Entity/Hooks/Default.php
@@ -10,14 +10,21 @@
     setFn('Process', function ($Call)
     {
         foreach ($Call['Nodes'] as $Name => $Node)
-        {
             if (isset($Node['Default']))
             {
                 $Node['Default'] = F::Live($Node['Default']);
-
-                if (F::Dot($Call['Data'], $Name) === null)
-                    $Call['Data'] = F::Dot($Call['Data'], $Name, $Node['Default']); // FIXME Add flag
+                
+                if (F::Dot($Node, 'Empty as Default'))
+                {
+                    if (F::Dot($Call['Data'], $Name) == null)
+                        $Call['Data'] = F::Dot($Call['Data'], $Name, $Node['Default']);
+                }
+                else
+                {
+                    if (F::Dot($Call['Data'], $Name) === null)
+                        $Call['Data'] = F::Dot($Call['Data'], $Name, $Node['Default']);
+                }
             }
-        }
+
         return $Call;
     });
