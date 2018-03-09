1fb3ac46d5e760c3869512a4af34aad3a6f8a4c8
Dmitry Bergstein <bergstein@trickyplan.com>
2018-03-09 15:41:14 +0300

    Restore arrow-navigation

--- src/Assets/Default/js/arrow-navigation.js
+++ src/Assets/Default/js/arrow-navigation.js
@@ -1,3 +1,11 @@
-/*
-$(document).bind('keydown', 'Ctrl+left', function() { window.location.replace($('a[rel="prev"]').attr('href'));} );
-$(document).bind('keydown', 'Ctrl+right', function() { window.location.replace($('a[rel="next"]').attr('href'));} );*/
+key('ctrl + left', function ()
+{
+    window.location.replace($('a[rel="prev"]').attr('href'));
+    return false;
+});
+
+key('ctrl + right', function ()
+{
+    window.location.replace($('a[rel="next"]').attr('href'));
+    return false;
+});
