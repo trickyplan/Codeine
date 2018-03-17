6f3af11a1eb4dd730cc110bb03eac136fc50caed
Dmitry Bergstein <bergstein@trickyplan.com>
2018-03-17 14:37:36 +0300

    Dot the code

--- src/Drivers/View/HTML/LocalizedURLs.php
+++ src/Drivers/View/HTML/LocalizedURLs.php
@@ -9,7 +9,7 @@
 
     setFn('Process', function ($Call)
     {
-        if ($Call['Locale'] !== $Call['Default']['Locale'])
+        if ($Call['Locale'] !== F::Dot($Call, 'Default.Locale'))
             if (F::Dot($Call, 'LocalizedURLs.Enabled') && preg_match_all('@a href="(/.*)"@SsUu',$Call['Output'], $Links))
             {
                 $Pair = [];
