b1259ab8ed15d593741c7c54c2d399e8eb11859b
Dmitry Bergstein <bergstein@trickyplan.com>
2018-04-09 12:43:22 +0300

    Fix little mess.

--- src/Drivers/Advertisement/RTB/SSP/Hooks/DataToJSON.php
+++ src/Drivers/Advertisement/RTB/SSP/Hooks/DataToJSON.php
@@ -1,8 +1,9 @@
 <?php
 
-setFn('Do', function ($Call) {
-    return F::Dot($Call, 'RTB.DSP.Items', array_map(function ($DSP) {
-        $DSP['Request'] = j($DSP['Request']);
-        return $DSP;
-    }, F::Dot($Call, 'RTB.DSP.Items')));
-});
+    setFn('Do', function ($Call)
+    {
+        return F::Dot($Call, 'RTB.DSP.Items', array_map ( function ($DSP) {
+            $DSP['Request'] = j($DSP['Request']);
+            return $DSP;
+        }, F::Dot($Call, 'RTB.DSP.Items')));
+    });
