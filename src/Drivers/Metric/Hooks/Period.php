9f5310f1a7d1feef78f2126d0f5432b11814cbe8
Dmitry Bergstein <bergstein@trickyplan.com>
2018-04-19 20:08:43 +0300

    Cleanup.

--- src/Drivers/Metric/Hooks/Period.php
+++ src/Drivers/Metric/Hooks/Period.php
@@ -1,27 +1,32 @@
 <?php
 
-setFn('Do', function ($Call) {
-    if (isset($Call['Since']) || isset($Call['Till'])) {
-        $Call['Since'] = $Call['Since'] ?? 0;
-        $Call['Till'] = $Call['Till'] ?? time();
-        $Period = $Call['Till'] - $Call['Since'];
-
-        if (!isset($Call['Where']['Resolution'])) {
-            switch (true) {
-                case $Period > 86400:
-                    $Call['Where']['Resolution'] = 86400;
-                    break;
-                case $Period > 3600;
-                    $Call['Where']['Resolution'] = 3600;
-                    break;
-                default: 
-                    $Call['Where']['Resolution'] = 60;
+    
+    setFn('Do', function ($Call)
+    {
+        if (isset($Call['Since']) || isset($Call['Till']))
+        {
+            $Call['Since'] = $Call['Since'] ?? 0;
+            $Call['Till'] = $Call['Till'] ?? time();
+            
+            $Period = $Call['Till'] - $Call['Since'];
+    
+            if (!isset($Call['Where']['Resolution'])) {
+                switch (true) {
+                    case $Period > 86400:
+                        $Call['Where']['Resolution'] = 86400;
+                        break;
+                    case $Period > 3600;
+                        $Call['Where']['Resolution'] = 3600;
+                        break;
+                    default:
+                        $Call['Where']['Resolution'] = 60;
+                }
             }
+            $Resolution = array_pop($Call['Where']['Resolution']);
+            
+            $Call['Where']['Time']['$gte'] = floor($Call['Since'] / $Resolution);
+            $Call['Where']['Time']['$lte'] = floor($Call['Till'] / $Resolution);
         }
-
-        $Call['Where']['Time']['$gte'] = $Call['Since'] / $Call['Where']['Resolution'];
-        $Call['Where']['Time']['$lte'] = $Call['Till'] / $Call['Where']['Resolution'];
-    }
-
-    return $Call;
-});
+    
+        return $Call;
+    });
