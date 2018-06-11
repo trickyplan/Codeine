07852efbabc5da618787e63c6405747b3da152ca
Dmitry Bergstein <bergstein@trickyplan.com>
2018-06-12 00:43:56 +0300

    Dry Run fixes.

--- src/Drivers/Advertisement/RTB/SSP/Hooks/WinNotice.php
+++ src/Drivers/Advertisement/RTB/SSP/Hooks/WinNotice.php
@@ -2,15 +2,18 @@
 
     setFn('RTB.SSP.Winner.Exists', function ($Call)
     {
-        F::Run('IO', 'Read',
-        [
-            'Storage' => 'Web',
-            'Time' => uniqid(true),
-            'Where' =>
+        if (F::Dot($Call, 'RTB.DryRun'))
+            ;
+        else
+            F::Run('IO', 'Read',
             [
-                'ID' => F::Dot($Call, 'RTB.Winner.Bid.nurl')
-            ]
-        ]);
+                'Storage' => 'Web',
+                'Time' => uniqid(true),
+                'Where' =>
+                [
+                    'ID' => F::Dot($Call, 'RTB.Winner.Bid.nurl')
+                ]
+            ]);
         
         return $Call;
     });
