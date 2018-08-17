5cf0a3485d82eca84b7efd5892788430cb4bfa68
vladimir <hate.squids@gmail.com>
2018-08-17 10:04:56 +0400

    Check requested domain is proxy

--- src/Drivers/SEO/Robots.php
+++ src/Drivers/SEO/Robots.php
@@ -9,29 +9,40 @@
 
     setFn('Do', function ($Call)
     {
+        $Call = F::Hook('beforeSEORobotsGenerate', $Call);
+
         $Call['Layouts'][] =
         [
             'Scope' => 'SEO',
             'ID' => 'Robots'
         ];
 
-        $Sitemaps = F::Run('SEO.Sitemap', 'List Sitemap Indexes', $Call);
+        if (F::Dot($Call, 'SEO.Robots.Domain Is Valid')) {
+            $Sitemaps = F::Run('SEO.Sitemap', 'List Sitemap Indexes', $Call);
 
-        foreach ($Sitemaps['Sitemap Indexes'] as $Sitemap)
-            $Call['Output']['Content'][] = 'Sitemap: '.$Sitemap;
+            foreach ($Sitemaps['Sitemap Indexes'] as $Sitemap)
+                $Call['Output']['Content'][] = 'Sitemap: '.$Sitemap;
 
-        if (isset($Call['Robots']['Crawl-delay']) && $Call['Robots']['Crawl-delay']>0)
-            $Call['Output']['Content'][] = 'Crawl-delay: '.$Call['Robots']['Crawl-delay'];
+            if (isset($Call['Robots']['Crawl-delay']) && $Call['Robots']['Crawl-delay']>0)
+                $Call['Output']['Content'][] = 'Crawl-delay: '.$Call['Robots']['Crawl-delay'];
 
-        foreach ($Call['Robots']['Crawlers'] as $Crawler => $URL)
-        {
-            $Call['Output']['Content'][] = 'User-agent: '.$Crawler;
-            $Call['Output']['Content'][] = 'Disallow: '.$URL.PHP_EOL;
+            foreach ($Call['Robots']['Crawlers'] as $Crawler => $URL)
+            {
+                $Call['Output']['Content'][] = 'User-agent: '.$Crawler;
+                $Call['Output']['Content'][] = 'Disallow: '.$URL.PHP_EOL;
+            }
+
+            $Call['Output']['Content'][] = 'User-agent: Yandex';
+            $Call['Output']['Content'][] = 'Host: '.$Call['HTTP']['Proto'].$Call['HTTP']['Host'];
+
+        } else {
+            $Call['Output']['Content'][] = 'User-agent: *';
+            $Call['Output']['Content'][] = 'Disallow: /';
         }
 
-        $Call['Output']['Content'][] = 'User-agent: Yandex';
-        $Call['Output']['Content'][] = 'Host: '.$Call['HTTP']['Proto'].$Call['HTTP']['Host'];
-
         $Call['Output']['Content'] = [implode(PHP_EOL, $Call['Output']['Content'])];
+
+        $Call = F::Hook('afterSEORobotsGenerate', $Call);
+
         return $Call;
     });
