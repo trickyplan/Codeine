feabbbdde37d386e0d52d17f3ec59ad8a80d7f6c
vladimir <hate.squids@gmail.com>
2018-11-22 19:10:06 +0400

    convert winner price to native DSP currency

--- src/Drivers/Advertisement/RTB/SSP/Hooks/SubstituteMacroses.php
+++ src/Drivers/Advertisement/RTB/SSP/Hooks/SubstituteMacroses.php
@@ -6,9 +6,27 @@
      * @package Codeine
      * @version 8.x
      */
+
+    setFn('Convert price', function ($Call) {
+        if (F::Dot($Call, 'RTB.Winner.NativeCurrency') != F::Dot($Call, 'RTB.Winner.Currency')) {
+            $Price = F::Run('Finance.Currency', 'Rate.Convert', 
+            [
+                'Value' => F::Dot($Call, 'RTB.Winner.Price'),
+                'From' => F::Dot($Call, 'RTB.Winner.Currency'),
+                'To' => F::Dot($Call, 'RTB.Winner.NativeCurrency')
+            ]);
+
+            $Call = F::CopyDot($Call, 'RTB.Winner.NativeCurrency', 'RTB.Winner.Currency');
+            $Call = F::Dot($Call, 'RTB.Winner.Bid.price', $Price);
+        }
+
+        return $Call;
+    });
     
     setFn('RTB.SSP.Request.Executed', function ($Call)
     {
+        $Call = F::Run(null, 'Convert price', $Call);
+
         $Map =
         [
             '${AUCTION_BID_ID}' => F::Dot($Call, 'RTB.Winner.Bid.id'),
