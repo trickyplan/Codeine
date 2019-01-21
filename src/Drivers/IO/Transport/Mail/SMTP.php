2d9dd2e3cfbcfb833c46dc5815c3190a74c8f8e1
vladimir <hate.squids@gmail.com>
2019-01-21 11:58:23 +0400

    email dkim sign

--- src/Drivers/IO/Transport/Mail/SMTP.php
+++ src/Drivers/IO/Transport/Mail/SMTP.php
@@ -82,6 +82,8 @@
         $Call['Data'] = $mime->get(['text_charset' => 'utf-8']);
         $Call['Headers'] = $mime->headers($Call['Headers']);
 
+        $Call = F::Run('Security.Mail.DKIM', 'Do', $Call);
+
         F::Log('Sending mail "'.$Call['Where']['ID'].'" to '.$Call['Scope'].' with '.$Call['Server'], LOG_INFO, 'Administrator');
 
         $Result = $Call['Link']->send($Call['Scope'], $Call['Headers'], $Call['Data']);
