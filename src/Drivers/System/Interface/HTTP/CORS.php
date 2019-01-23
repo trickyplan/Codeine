2f43213c03181700632d92232b0380ad36a5042b
vladimir <hate.squids@gmail.com>
2019-01-23 17:55:17 +0400

    master CORS fixes

---
+++ src/Drivers/System/Interface/HTTP/CORS.php
@@ -0,0 +1,23 @@
+<?php
+
+    setFn('Do', function ($Call) {
+
+        $Call['HTTP']['Headers']['Access-Control-Allow-Credentials:'] = F::Dot($Call, 'CORS.Credentials');
+
+        if (F::Dot($Call, 'HTTP.Method') == 'OPTIONS') 
+            ;
+        else {
+            $Call['HTTP']['Headers']['Access-Control-Allow-Headers: '] = 'X-Requested-With,Content-Type';
+
+            $Origin = F::Dot($Call, 'HTTP.Request.Headers.Origin');
+
+            if (empty($Origin)) 
+            {
+                $Origin = (isset($_SERVER['HTTP_ORIGIN'])) ? $_SERVER['HTTP_ORIGIN'] : '*';
+            }
+
+            $Call['HTTP']['Headers']['Access-Control-Allow-Origin:'] = $Origin;
+        }
+
+        return $Call;
+    });
