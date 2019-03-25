8d56625bd1924359f66db2c4860f749c4c7b227c
Dmitry Bergstein <bergstein@trickyplan.com>
2019-03-25 18:58:26 +0300

    Refactor Literal.String

--- src/Drivers/Data/Type/Literal/String.php
+++ src/Drivers/Data/Type/Literal/String.php
@@ -36,14 +36,14 @@
 
     setFn('Read', function ($Call)
     {
-//        if (is_string($Call['Value']))
         if (is_scalar($Call['Value']))
         {
             if (mb_strlen($Call['Value']) > $Call['Literal']['String']['Read Size'])
-                return mb_substr((string) $Call['Value'], 0, $Call['Literal']['String']['Read Size']);
+                $Result = mb_substr((string) $Call['Value'], 0, $Call['Literal']['String']['Read Size']);
             else
-                return (string) $Call['Value'];
+                $Result = (string) $Call['Value'];
         }
         else
-             return null;
+             $Result = null;
+        return $Result;
     });
