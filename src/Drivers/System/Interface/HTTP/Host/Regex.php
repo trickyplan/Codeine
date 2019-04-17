0a24c2c907807db5848ce3f92a9b5e670fac9115
Dmitry Bergstein <bergstein@trickyplan.com>
2019-04-17 17:33:26 +0300

    Regex logging fixes. Status variable.

--- src/Drivers/System/Interface/HTTP/Host/Regex.php
+++ src/Drivers/System/Interface/HTTP/Host/Regex.php
@@ -11,15 +11,17 @@
     {
         if (isset($Call['Project']['Regex Hosts']))
         {
-            F::Log('Regex Hosts activated', LOG_INFO);
-           
+            F::Log('Regex Hosts is *active*', LOG_INFO);
+            $Call = F::Dot($Call, 'Code.Routing.Regex.Override.Active', false);
+            
             foreach ($Call['Project']['Regex Hosts'] as $Name => $Rule)
             {
                 if (preg_match_all('@'.$Rule['Regex'].'@Ssu', $_SERVER['HTTP_HOST'], $Pockets))
                 {
                     F::Log('Regex Hosts Rule *'.$Name.'* matched ', LOG_INFO);
                     $Call['HTTP']['Override']['Matches'] = $Pockets;
-                    
+                    $Call = F::Dot($Call, 'Code.Routing.Regex.Override.Active', true);
+
                     if (isset($Rule['Override']['Host']))
                     {
                         $Call['HTTP']['Override']['Host'] = $_SERVER['HTTP_HOST'];
