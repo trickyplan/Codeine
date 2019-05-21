6b08092f1f2c7e3d34168f40fb3b135be2702a83
Dmitry Bergstein <bergstein@trickyplan.com>
2019-05-21 19:18:56 +0300

    Test minor fixes.

--- src/Drivers/Test.php
+++ src/Drivers/Test.php
@@ -24,20 +24,17 @@
             if (isset($Call['Test']['Mixins']) && is_array($Call['Test']['Mixins']))
             {
                 foreach($Call['Test']['Mixins'] as $Mixin)
-                {
                     $Call['Test'] = F::Merge(F::loadOptions($Mixin), $Call['Test']);
-                }
                 unset($Call['Test']['Mixins']);
             }
 
             if (isset($Call['Test']['Suites']))
-                foreach($Call['Test']['Suites'] as
-                        $Call['Test']['Suite']['Name'] => $Call['Test']['Suite']['Cases'])
+                foreach($Call['Test']['Suites'] as $Call['Test']['Suite']['Name'] => $Call['Test']['Suite']['Cases'])
                     $Call = F::Apply(null, 'Run.Suite', $Call);
-            
+
             if (isset($Call['Test']['Results']))
                 foreach ($Call['Test']['Results'] as $TestResult)
-                    $Call['Output']['Content'][$TestResult['Name']] =
+                    $Call['Output']['Content'][] =
                         [
                             'Type'  => 'Template',
                             'Scope' => 'Test',
@@ -56,10 +53,9 @@
         
             $Call['Virtual'] = $Call;
 
-            foreach ($Call['Test']['Suite']['Cases'] as
-                     $Call['Test']['Case']['Name'] => $Call['Test']['Case'])
+            foreach ($Call['Test']['Suite']['Cases'] as $Call['Test']['Case']['Name'] => $Call['Test']['Case'])
                 $Call = F::Apply(null, 'Run.Case', $Call);
-        
+
         $Call = F::Hook('Test.Suite.Run.After', $Call);
         return $Call;
     });
@@ -67,17 +63,23 @@
     setFn('Run.Case', function ($Call)
     {
         $Call = F::Hook('Test.Case.Run.Before', $Call);
-            
+
+            $SCID = $Call['Test']['Suite']['Name'].':'.$Call['Test']['Case']['Name'];
+
             if (mb_substr($Call['Test']['Case']['Name'], 0, 1) == '-')
                 return $Call;
-        
+
+            if ($BaseEncodedParameters = F::Dot($Call, 'Test.Case.Preprocess.Base64'))
+                foreach ($BaseEncodedParameters as $Parameter)
+                    $Call['Test']['Case'] = F::Dot($Call['Test']['Case'], $Parameter, base64_decode(F::Dot($Call['Test']['Case'], $Parameter)));
+
             if (isset($Call['Test']['Case']['Apply']))
                 $Call['Virtual'] = F::Live($Call['Test']['Case']['Apply'], $Call['Virtual']);
             
             // Run
             $Call = F::Hook('Test.Case.Run.Execute.Before', $Call);
-            
-                F::Start($Call['Test']['Case']['Name']);
+
+                F::Start($SCID);
 
                 $Result = F::Live(F::Dot($Call, 'Test.Case.Run'), $Call['Virtual']);
 
@@ -86,12 +88,15 @@
                 else
                     $Call = F::Dot($Call, 'Test.Case.Result.Actual', $Result);
             
-                F::Stop($Call['Test']['Case']['Name']);
-                $Call = F::Dot($Call, 'Test.Case.Time.Run',
-                    F::Time($Call['Test']['Case']['Name']));
+                F::Stop($SCID);
+                $Call = F::Dot($Call, 'Test.Case.Time.Run', F::Time($SCID));
                 
             $Call = F::Hook('Test.Case.Run.Execute.After', $Call);
-            
+
+            if (F::Dot($Call, 'Test.Case.Result.Base64'))
+                $Call = F::Dot($Call, 'Test.Case.Result.Actual',
+                    base64_encode(F::Dot($Call, 'Test.Case.Result.Actual')));
+
             // Assert
             $Status = 'Passed';
             
@@ -99,18 +104,18 @@
                 foreach ($Call['Test']['Case']['Assert'] as $Assert => $Expected)
                 {
                     $Call = F::Dot($Call, 'Test.Case.Result.'.$Assert.'.Expected', $Expected);
-                    F::Start($Call['Test']['Case']['Name'].$Assert);
+                    F::Start($SCID.'.'.$Assert);
                     
                     $Decision = F::Run('Test.Assert.'.$Assert, 'Do', $Call);
-                    $Call = F::Dot ($Call, 'Test.Case.Assert.'.$Assert.'.Decision',$Decision);
+                    $Call = F::Dot ($Call, 'Test.Case.Assert.'.$Assert.'.Decision', $Decision);
                     
                     if ($Decision == false)
                         $Status = 'Failed';
                     
-                    F::Stop($Call['Test']['Case']['Name'].$Assert);
+                    F::Stop($SCID.'.'.$Assert);
                     
                     $Call = F::Dot($Call, 'Test.Case.Time.'.$Assert,
-                        F::Time($Call['Test']['Case']['Name'].$Assert));
+                        F::Time($SCID.'.'.$Assert));
                 }
         
         $Call['Test']['Case']['Status'] = $Status;
@@ -122,14 +127,11 @@
             $Call = F::Dot($Call, 'Test.Case.Result.Actual',
                 j(F::Dot($Call, 'Test.Case.Result.Actual')));
 
-        if (F::Dot($Call, 'Test.Case.Result.Base64'))
-            $Call = F::Dot($Call, 'Test.Case.Result.Actual',
-                base64_encode(F::Dot($Call, 'Test.Case.Result.Actual')));
-
         $Call['Test']['Case']['ID'] = $Call['ID'];
         $Call['Test']['Case']['CID'] = uniqid();
         $Call['Test']['Case']['Suite'] = $Call['Test']['Suite']['Name'];
-        $Call['Test']['Results'][$Call['Test']['Case']['Name']] = $Call['Test']['Case'];
+        $Call['Test']['Results'][] = $Call['Test']['Case'];
+
         return $Call;
     });
     
