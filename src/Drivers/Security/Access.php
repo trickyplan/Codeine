f9fd8509eb81537c05b203daa0f90d2c0376a271
Dmitry Bergstein <bergstein@trickyplan.com>
2019-05-26 20:18:05 +0300

    Feature-Policy

--- src/Drivers/Security/Access.php
+++ src/Drivers/Security/Access.php
@@ -9,13 +9,9 @@
 
     setFn('Check', function ($Call)
     {
-        if (isset($Call['System']))
-        {
-            $Call['System'] = (array) $Call['System'];
-
-            foreach ($Call['System'] as $System)
+        if ($Systems = (array) F::Dot($Call, 'Security.Access.System'))
+            foreach ($Systems as $System)
                 $Call = F::Apply('Security.Access.'.$System, 'Check', $Call);
-        }
 
         return $Call['Decision'];
     });
---
+++ src/Drivers/System/Interface/HTTP/Headers/Feature-Policy.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Codeine
+ * @author bergstein@trickyplan.com
+ * @description  
+ * @package Codeine
+ * @version 2019.x
+ */
+
+    setFn('Do', function ($Call)
+    {
+        $Header = [];
+
+        $Policies = F::Dot($Call, 'System.Interface.HTTP.Headers.Feature-Policy');
+        foreach ($Policies as $Policy => $Decision)
+            $Header[] = $Policy."'".$Decision."'";
+
+        $Call['HTTP']['Headers']['Feature-Policy:'] = implode('; ', $Header);
+
+        return $Call;
+    });
