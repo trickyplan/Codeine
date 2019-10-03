91be3bfb5cb2a199c526bad1332e786a81a13243
Dmitry Bergstein <bergstein@trickyplan.com>
2019-10-03 18:21:01 +0300

    Cache Control Initial Commit

---
+++ src/Drivers/System/Interface/HTTP/Caching.php
@@ -0,0 +1,27 @@
+<?php
+
+/* Codeine
+ * @author bergstein@trickyplan.com
+ * @description  
+ * @package Codeine
+ * @version 2019.x
+ */
+
+    setFn('Do', function ($Call)
+    {
+        if (F::Dot($Call, 'HTTP.Caching.Enabled'))
+        {
+            if (F::Dot($Call, 'HTTP.Caching.Private'))
+                $Directives[] = 'private';
+            else
+                $Directives[] = 'public';
+
+            if (null === ($TTL = F::Dot($Call, 'HTTP.Caching.TTL')))
+                ;
+            else
+                $Directives[] = 'max-age='.$TTL;
+
+            $Call = F::Dot($Call, 'HTTP.Headers.Cache-Control:', implode(', ', $Directives));
+        }
+        return $Call;
+    });
