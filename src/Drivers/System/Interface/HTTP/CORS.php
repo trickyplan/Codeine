42cbed3c74497223bf922338b2eb95ba78989eca
Dmitry Bergstein <bergstein@trickyplan.com>
2019-10-11 00:20:51 +0300

    Caching for CORS preflight.

--- src/Drivers/System/Interface/HTTP/CORS.php
+++ src/Drivers/System/Interface/HTTP/CORS.php
@@ -4,9 +4,16 @@
     {
         if (F::Dot($Call, 'HTTP.CORS.Enabled'))
         {
-            $Call['HTTP']['Headers']['Access-Control-Allow-Credentials:'] = F::Dot($Call, 'CORS.Credentials');
+            $Call['HTTP']['Headers']['Access-Control-Allow-Credentials:'] = F::Dot($Call, 'HTTP.CORS.Credentials');
             $Call['HTTP']['Headers']['Access-Control-Allow-Headers: '] = 'X-Requested-With,Content-Type';
             $Call['HTTP']['Headers']['Access-Control-Allow-Methods: '] = implode(',', F::Dot($Call, 'HTTP.Methods.Allowed') ?? []);
+
+            if ($Call['HTTP']['Method'] == 'OPTIONS')
+            {
+                $Call = F::Dot($Call, 'HTTP.Caching.Enabled', F::Dot($Call, 'HTTP.CORS.Caching.Enabled'));
+                $Call = F::Dot($Call, 'HTTP.Caching.Private', F::Dot($Call, 'HTTP.CORS.Caching.Private'));
+                $Call = F::Dot($Call, 'HTTP.Caching.TTL', F::Dot($Call, 'HTTP.CORS.Caching.TTL'));
+            }
 
             $Origin = F::Dot($Call, 'HTTP.Request.Headers.Origin');
 
