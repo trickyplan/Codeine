81b09418858875aaaef7d07613837454c349578e
Dmitry Bergstein <bergstein@trickyplan.com>
2019-11-05 18:25:23 +0300

    Fix for missed Accept-Encoding Composer

--- src/Drivers/System/Interface/HTTP/Compression.php
+++ src/Drivers/System/Interface/HTTP/Compression.php
@@ -11,36 +11,41 @@
     {
         if (F::Dot($Call, 'System.Interface.HTTP.Compression.Enabled'))
         {
-            $Call['Output'] = ob_get_clean().$Call['Output'];
-            $ContentType = F::Dot($Call, 'HTTP.Headers.Content-Type:');
-            if (($Pos = mb_strpos($ContentType, ';')) == false)
-                $ContentType = F::Dot($Call, 'HTTP.Headers.Content-Type:');
-            else
-                $ContentType = mb_substr($ContentType, 0, $Pos);
-
-            if (in_array($ContentType, F::Dot($Call, 'System.Interface.HTTP.Compression.Content-Types.Allowed')))
+            if (isset($Call['HTTP']['Request']['Headers']['Accept-Encoding']))
             {
-                $Algorithm = null;
-                $AcceptEncoding = $Call['HTTP']['Request']['Headers']['Accept-Encoding'];
-                $AcceptEncodings = explode(',', $AcceptEncoding);
-
-                foreach ($AcceptEncodings as &$Encoding)
-                    $Encoding = trim($Encoding);
-
-                $AllowedEncodings = F::Dot($Call, 'System.Interface.HTTP.Compression.Algorithms.Allowed');
-
-                if (F::Dot($Call, 'System.Interface.HTTP.Compression.Algorithms.PreferClientOrder'))
-                    $Algorithms = array_intersect($AcceptEncodings, $AllowedEncodings);
+                $Call['Output'] = ob_get_clean().$Call['Output'];
+                $ContentType = F::Dot($Call, 'HTTP.Headers.Content-Type:');
+                if (($Pos = mb_strpos($ContentType, ';')) == false)
+                    $ContentType = F::Dot($Call, 'HTTP.Headers.Content-Type:');
                 else
-                    $Algorithms = array_intersect($AllowedEncodings, $AcceptEncodings);
+                    $ContentType = mb_substr($ContentType, 0, $Pos);
 
-                if (empty($Algorithms))
-                    F::Log('No compatible compression algorithms', LOG_INFO);
+                if (in_array($ContentType, F::Dot($Call, 'System.Interface.HTTP.Compression.Content-Types.Allowed')))
+                {
+                    $Algorithm = null;
+                    $AcceptEncoding = $Call['HTTP']['Request']['Headers']['Accept-Encoding'];
+                    $AcceptEncodings = explode(',', $AcceptEncoding);
+
+                    foreach ($AcceptEncodings as &$Encoding)
+                        $Encoding = trim($Encoding);
+
+                    $AllowedEncodings = F::Dot($Call, 'System.Interface.HTTP.Compression.Algorithms.Allowed');
+
+                    if (F::Dot($Call, 'System.Interface.HTTP.Compression.Algorithms.PreferClientOrder'))
+                        $Algorithms = array_intersect($AcceptEncodings, $AllowedEncodings);
+                    else
+                        $Algorithms = array_intersect($AllowedEncodings, $AcceptEncodings);
+
+                    if (empty($Algorithms))
+                        F::Log('No compatible compression algorithms', LOG_INFO);
+                    else
+                        $Call = F::Run('System.Interface.HTTP.Compression.'.array_shift($Algorithms), 'Do', $Call);
+                }
                 else
-                    $Call = F::Run('System.Interface.HTTP.Compression.'.array_shift($Algorithms), 'Do', $Call);
+                    F::Log('Compression is not allowed by Content-Type', LOG_INFO);
             }
             else
-                F::Log('Compression is not allowed by Content-Type', LOG_INFO);
+                F::Log('Compression is not allowed, Accept-Encoding is not defined', LOG_INFO);
         }
         else
             F::Log('Compression is *disabled*', LOG_INFO);
