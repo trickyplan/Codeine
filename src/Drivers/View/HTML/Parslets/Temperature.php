2f882eda03956f588e4b8f8dcfaf926397cdcbd3
Dmitry Bergstein <bergstein@trickyplan.com>
2020-07-26 15:26:06 +0300

    + Temperature parslet

---
+++ src/Drivers/View/HTML/Parslets/Temperature.php
@@ -0,0 +1,36 @@
+<?php
+    
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description Exec Parslet 
+     * @package Codeine
+     * @version 6.0
+     */
+    
+    setFn('Parse', function ($Call)
+    {
+        $Replaces = [];
+
+        foreach ($Call['Parsed']['Value'] as $IX => $Match)
+        {
+            $From = F::Dot($Call['Parsed'], 'Options.'.$IX.'.from') ? F::Dot($Call['Parsed'], 'Options.'.$IX.'.from'): 'Kelvin';
+            $To = F::Dot($Call['Parsed'], 'Options.'.$IX.'.to') ? F::Dot($Call['Parsed'], 'Options.'.$IX.'.to'): 'Celsius';
+
+            $Match = trim($Match);
+
+            if (is_scalar($Match) && isset($From) && isset($To) && $Match !== 'null')
+            {
+                $Replaces[$IX] =
+                    round(F::Run('Science.Math.Conversion.Temperature', 'Do',
+                        [
+                            'From'  => $From,
+                            'To'    => $To,
+                            'Value' => $Match
+                        ]
+                    )).$Call['View']['HTML']['Parslets']['Temperature']['Unit'][$To];
+            } else
+                $Replaces[$IX] = '';
+        }
+        
+        return $Replaces;
+    });
