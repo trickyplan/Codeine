b4dbd159f4e6b705ad5a3ab1b811f339a0fac717
Dmitry Bergstein <bergstein@trickyplan.com>
2020-07-26 15:29:16 +0300

    + OpenWeatherMap One Call Wrapper

---
+++ src/Drivers/Providers/Weather/OpenWeatherMap/OneCall.php
@@ -0,0 +1,70 @@
+<?php
+
+/* Codeine
+ * @author bergstein@trickyplan.com
+ * @description
+ * @package Codeine
+ * @version 2020.x.x
+ */
+
+    setFn('Do', function ($Call)
+    {
+        $Output = [];
+
+        $Response = F::Run('IO', 'Read',
+            [
+                'Storage'   => 'Web',
+                'Where'     =>
+                [
+                    'ID'    => F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Endpoint')
+                ],
+                'Data'      => [
+                    'lat'   => F::Dot($Call, 'Latitude'),
+                    'lon'   => F::Dot($Call, 'Longitude'),
+                    'appid' => F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.APIKey')
+                ],
+                'Behaviours'=>
+                [
+                    'Cached'    =>
+                    [
+                        'Enabled'   => true,
+                        'TTL'       => 3600,
+                        'Keys'      => ['Where.ID', 'Data', 'IO One']
+                    ]
+                ],
+                'Format' => 'Formats.JSON',
+                'IO One'           => true
+            ]);
+
+        $Map = F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Map');
+        $Output['Now'] = [
+            'Time' => ['Time' => date(DATE_W3C, $Response['current']['dt'])]
+        ];
+
+        foreach ($Map['Now'] as $From => $To)
+            $Output['Now'] = F::Dot($Output['Now'], $To, F::Dot($Response['current'], $From));
+
+        foreach ($Response['hourly'] as $Hourly)
+        {
+            $Time = date('Hi', $Hourly['dt']);
+            $Output['Hourly'][$Time] = ['Time' => $Time];
+            foreach ($Map['Hourly'] as $From => $To)
+            {
+                if ($Value = F::Dot($Hourly, $From))
+                    $Output['Hourly'][$Time] = F::Dot($Output['Hourly'][$Time], $To, $Value);
+            }
+        }
+
+        foreach ($Response['daily'] as $Daily)
+        {
+            $Date = date('Ymd', $Daily['dt']);
+            $Output['Daily'][$Date] = ['Date' => $Date];
+            foreach ($Map['Daily'] as $From => $To)
+            {
+                if ($Value = F::Dot($Daily, $From))
+                    $Output['Daily'][$Date] = F::Dot($Output['Daily'][$Date], $To, $Value);
+            }
+        }
+
+        return $Output;
+    });
