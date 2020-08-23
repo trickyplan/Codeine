d48dbb385efdf919032ee4abcf56d224c8cc4286
Dmitry Bergstein <bergstein@trickyplan.com>
2020-08-24 02:59:30 +0300

    + Except option for Foreach

--- src/Drivers/View/HTML/Parslets/Foreach.php
+++ src/Drivers/View/HTML/Parslets/Foreach.php
@@ -10,14 +10,24 @@
     setFn('Parse', function ($Call)
     {
         $Replaces = [];
-       
+
         foreach ($Call['Parsed']['Value'] as $IX => $Cludge)
         {
+            if (($Except = F::Dot($Call, 'Parsed.Options.'.$IX.'.except')) === null)
+                $Except = [];
+            else
+            {
+                if (mb_strpos($Except, ',') !== false)
+                    $Except = explode(',', $Except);
+                else
+                    $Except = [$Except];
+            }
+
             $Output = [];
             if (isset($Call['Parsed']['Options'][$IX]['key']))
             {
                 $Key = $Call['Parsed']['Options'][$IX]['key'];
-            
+
                 if (mb_strpos($Key, ',') !== false)
                     $Key = explode(',', $Key);
                 else
@@ -26,18 +36,18 @@
                 foreach ($Key as $CMatch)
                 {
                     $Value = F::Live(F::Dot($Call['Data'], $CMatch));
-                    
+
                     if ($Value === null)
                         ;
                     else
                         break;
                 }
             }
-            
+
             if (isset($Call['Parsed']['Options'][$IX]['call']))
             {
                 $Key = $Call['Parsed']['Options'][$IX]['call'];
-            
+
                 if (mb_strpos($Key, ',') !== false)
                     $Key = explode(',', $Key);
                 else
@@ -46,36 +56,41 @@
                 foreach ($Key as $CMatch)
                 {
                     $Value = F::Live(F::Dot($Call, $CMatch)); // FIXME?
-                    
+
                     if ($Value === null)
                         ;
                     else
                         break;
                 }
             }
-            
+
             if (is_array($Value))
             {
                 foreach ($Value as $CKey => $CValue)
                 {
-                    if ($CValue === null)
-                        $CValue = 'null';
+                    if (in_array($CKey, $Except))
+                        ;
                     else
                     {
-                        if (is_array($CValue))
-                            $CValue = array_shift($CValue);
-    
-                        if ($CValue === 0)
-                            $CValue = '0';
-                        
-                        if ($CValue === false)
-                            $CValue = 'false';
-                        
-                        if ($CValue === true)
-                            $CValue = 'true';
+                        if ($CValue === null)
+                            $CValue = 'null';
+                        else
+                        {
+                            if (is_array($CValue))
+                                $CValue = array_shift($CValue);
+
+                            if ($CValue === 0)
+                                $CValue = '0';
+
+                            if ($CValue === false)
+                                $CValue = 'false';
+
+                            if ($CValue === true)
+                                $CValue = 'true';
+                        }
+
+                        $Output[$CKey] = str_replace(['<fe-key/>', '<fe-value/>'], [$CKey, $CValue], $Cludge);
                     }
-                   
-                    $Output[$CKey] = str_replace(['<fe-key/>', '<fe-value/>'], [$CKey, $CValue], $Cludge);
                 }
             }
             else
