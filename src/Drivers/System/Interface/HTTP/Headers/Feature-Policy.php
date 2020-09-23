3ed0589cd22f4bff4df3ab838e276ad3adb92868
Dmitry Bergstein <bergstein@trickyplan.com>
2020-09-23 23:25:00 +0300

    + 43.5.2 + fix Feature Policy headers

--- src/Drivers/System/Interface/HTTP/Headers/Feature-Policy.php
+++ src/Drivers/System/Interface/HTTP/Headers/Feature-Policy.php
@@ -13,7 +13,7 @@
 
         $Policies = F::Dot($Call, 'System.Interface.HTTP.Headers.Feature-Policy');
         foreach ($Policies as $Policy => $Decision)
-            $Header[] = $Policy." '".$Decision."'";
+            $Header[] = $Policy." ".F::Variable($Decision, $Call);
 
         $Call['HTTP']['Headers']['Feature-Policy:'] = implode('; ', $Header);
 
