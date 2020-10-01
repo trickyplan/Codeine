302d4e238aa92bc924cee700912bb24c078b033f
Dmitry Bergstein <bergstein@trickyplan.com>
2020-10-02 02:27:43 +0300

    + 43.7.3 - codeine-cli logs

--- src/CLI.php
+++ src/CLI.php
@@ -7,69 +7,68 @@
 
     require 'Core.php';
 
+    fwrite(STDERR, "\033[1;31mCodeine CLI started \033[0m".PHP_EOL);
+
     $Opts = [];
 
-    if (isset($argv[1]) && file_exists($argv[1]))
+    if (isset($argv[1]))
     {
-        if ($Opts = F::Merge(jd(file_get_contents($argv[1]), true), $Opts))
-            ;//F::Log('JSON CLI parameters loaded from '.$argv[1], LOG_INFO);
-    }
-    else
-    {
+        if (file_exists($argv[1]))
+            if ($Opts = F::Merge(jd(file_get_contents($argv[1]), true), $Opts))
+                F::Log('JSON CLI parameters loaded from ' . $argv[1], LOG_INFO);
+
         if (preg_match_all('/--([\w.]+)\=([\S]+)/Ssu', $argv[1], $Pockets))
         {
             foreach ($Pockets[1] as $IX => $Key)
             {
                 $Key = strtr($Key, '_', ' ');
                 $Opts = F::Dot($Opts, $Key, $Pockets[2][$IX]);
-                ;//F::Log('Get Opt Style CLI parameter loaded from *'.$Key.'* = *'.$Pockets[2][$IX].'*', LOG_INFO);
+                F::Log('Get Opt Style CLI parameter loaded from *' . $Key . '* = *' . $Pockets[2][$IX] . '*', LOG_INFO);
             }
         }
         else
             $Opts[] = $argv;
-    }
 
-    if (file_exists('/etc/default/codeine'))
-        define('Root', file_get_contents('/etc/default/codeine'));
+        !defined('Root') ? define('Root', getcwd()) : false;
 
-    !defined('Root')? define('Root', getcwd()): false;
+        fwrite(STDERR, "\033[1;31mRoot: \033[0m".Root.PHP_EOL);
+        include Root . '/vendor/autoload.php';
 
-    //F::Log('Root folder: '.Root, LOG_INFO);
-    include Root.'/vendor/autoload.php';
-    
-    if (empty($Opts))
-        ;//F::Log('Empty CLI parameters', LOG_INFO);
-    else
-    {
-        if (isset($Opts['Service']))
+        if (empty($Opts))
+            ;//F::Log('Empty CLI parameters', LOG_INFO);
+        else
         {
-            if (isset($Opts['Method']))
-                ;
-            else
-                $Opts['Method'] = 'Do';
-
-            try
+            if (isset($Opts['Service']))
             {
-                $Call = F::Bootstrap
+                fwrite(STDERR, "\033[1;31mService: \033[0m".$Opts['Service'].PHP_EOL);
+                if (isset($Opts['Method']))
+                    ;
+                else
+                    $Opts['Method'] = 'Do';
+
+                fwrite(STDERR, "\033[1;31mMethod: \033[0m".$Opts['Method'].PHP_EOL);
+
+                try
+                {
+                    $Call = F::Bootstrap
                     ([
                         'Paths' => [Root],
-                        'Environment' => isset($Opts['Environment'])? $Opts['Environment']: null,
+                        'Environment' => isset($Opts['Environment']) ? $Opts['Environment'] : null,
                         'Service' => 'System.Interface.CLI',
                         'Method' => 'Do',
                         'Call' => $Opts
                     ]);
 
-                F::Shutdown($Call);
+                    F::Shutdown($Call);
+                } catch (Exception $e)
+                {
+                    F::Log('CLI Exception: ' . $e->getMessage(), LOG_CRIT, 'Developer');
+                }
             }
-            catch (Exception $e)
-            {
-               F::Log('CLI Exception: '.$e->getMessage(), LOG_CRIT, 'Developer');
-            }
+
+            if (isset($Call['Return Code']))
+                exit($Call['Return Code']);
+            else
+                exit(0);
         }
-
-        if (isset($Call['Return Code']))
-            exit($Call['Return Code']);
-        else
-            exit(0);
     }
-
