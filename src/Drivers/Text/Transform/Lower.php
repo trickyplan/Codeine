6094bd9517c85ff0d57508560e4bbe12391c9c85
Dmitry Bergstein <bergstein@trickyplan.com>
2020-10-13 05:44:18 +0300

    + 43.7.10 + force lowercase for email + refactor Text.Transform.Lower (shall refactor all Text.* ns)

--- src/Drivers/Text/Transform/Lower.php
+++ src/Drivers/Text/Transform/Lower.php
@@ -7,10 +7,18 @@
      * @version 8.x
      */
 
-    setFn('Do', function ($Call)
+    setFn('Apply', function ($Call)
     {
         if (is_string($Call['Value']))
             $Call['Value'] = mb_strtolower($Call['Value']);
 
         return $Call;
+    });
+
+    setFn('Do', function ($Call)
+    {
+        if (is_string($Call['Value']))
+            $Call['Value'] = mb_strtolower($Call['Value']);
+
+        return $Call['Value'];
     });
