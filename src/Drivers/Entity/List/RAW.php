9a1e33e5b2bb572091bce8b17f130d1895b65769
Dmitry Bergstein <bergstein@trickyplan.com>
2020-11-05 01:22:40 +0300

    + 43.7.17 + Permission-Policy + Logs to Browser (temp) + RAW.Call

--- src/Drivers/Entity/List/RAW.php
+++ src/Drivers/Entity/List/RAW.php
@@ -26,3 +26,23 @@
 
         return $Output;
     });
+
+    setFn('Call', function ($Call)
+    {
+        $Call['Output'] = [];
+
+        $Call = F::Merge($Call, F::loadOptions($Call['Entity'].'.Entity'));
+
+        $Call = F::Hook('beforeRAWList', $Call);
+
+            $Elements = F::Run('Entity', 'Read', $Call, ['Skip Enum Live' => true]);
+
+            if ($Elements !== null)
+                foreach ($Elements as $Element)
+                    if (isset($Element[$Call['Primary']]))
+                        $Call['Output'][$Element[$Call['Primary']]] = F::Dot($Element, $Call['Key']);
+
+        $Call = F::Hook('afterRAWList', $Call);
+
+        return $Call;
+    });
--- src/Drivers/System/Interface/HTTP/Headers/Permissions-Policy.php
+++ src/Drivers/System/Interface/HTTP/Headers/Permissions-Policy.php
@@ -11,11 +11,11 @@
     {
         $Header = [];
 
-        $Policies = F::Dot($Call, 'System.Interface.HTTP.Headers.Feature-Policy');
+        $Policies = F::Dot($Call, 'System.Interface.HTTP.Headers.Permissions-Policy');
         foreach ($Policies as $Policy => $Decision)
             $Header[] = $Policy." ".F::Variable($Decision, $Call);
 
-        $Call['HTTP']['Headers']['Feature-Policy:'] = implode('; ', $Header);
+        $Call['HTTP']['Headers']['Permissions-Policy:'] = implode('; ', $Header);
 
         return $Call;
     });
