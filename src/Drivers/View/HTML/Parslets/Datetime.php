1585ee6a30bd2943eedebb97b984fbf5392a1360
Dmitry Bergstein <bergstein@trickyplan.com>
2020-11-13 10:16:45 +0300

    + 43.8.0 + Timezone support in <datetime>

--- src/Drivers/View/HTML/Parslets/Datetime.php
+++ src/Drivers/View/HTML/Parslets/Datetime.php
@@ -10,19 +10,31 @@
     setFn('Parse', function ($Call)
     {
         $Replaces = [];
+
         foreach ($Call['Parsed']['Value'] as $IX => $Match)
         {
             $Engine = F::Dot($Call['Parsed'],'Options.'.$IX.'.engine') ? F::Dot($Call['Parsed'],'Options.'.$IX.'.'.'engine'): 'Date';
-            
+
             // TODO Due bug 13744 at w3c validator, time tag temporary diabled.
             // $Outer = '<time datetime="'.date(DATE_ISO8601, $Match).'">'.date($Format, $Inner).'</time>';
-            
+
             $Date = ['Value' => $Match];
-            
+
             if (F::Dot($Call['Parsed'],'Options.'.$IX.'.format'))
                 $Date['Format'] = F::Dot($Call['Parsed'],'Options.'.$IX.'.format');
-            
-            $Date = F::Run('Formats.Date.' . $Engine, 'Format', $Date);
+
+            if ($Timezone = F::Dot($Call['Parsed'],'Options.'.$IX.'.timezone'))
+            {
+                $LastZone = date_default_timezone_get();
+                date_default_timezone_set($Timezone);
+            }
+
+                $Date = F::Run('Formats.Date.' . $Engine, 'Format', $Date);
+
+            if (empty($Timezone))
+                ;
+            else
+                date_default_timezone_set($LastZone);
             
             $Replaces[$IX] = $Date;
         }
