5f9afdac70cfbd6c7e10c009fa5231fd7f1e035b
Dmitry Bergstein <bergstein@trickyplan.com>
2020-11-19 07:04:54 +0300

    + 43.9.4 + CLI cosmetics

--- src/CLI.php
+++ src/CLI.php
@@ -1,37 +1,39 @@
 <?php
 
-    /**
-     * @author bergstein@trickyplan.com
-     * @time 5:17
-     */
-
     require 'Core.php';
 
-    fwrite(STDERR, "\033[1;31mCodeine CLI started \033[0m".PHP_EOL);
+    // log is not available before bootstrap
+    fwrite(STDERR, "Codeine CLI started".PHP_EOL);
 
     $Opts = [];
 
     if (isset($argv[1]))
     {
+        // /usr/bin/codeine Run.json
         if (file_exists($argv[1]))
-            if ($Opts = F::Merge(jd(file_get_contents($argv[1]), true), $Opts))
-                F::Log('JSON CLI parameters loaded from ' . $argv[1], LOG_INFO);
+            if ($Opts = F::Merge(jd(file_get_contents($argv[1])), $Opts))
+                // log is not available before bootstrap
+                fwrite(STDERR, "JSON CLI parameters loaded from \033[0;32m".$argv[1]."\033[0m".PHP_EOL);
 
-        if (preg_match_all('/--([\w.]+)\=([\S]+)/Ssu', $argv[1], $Pockets))
+        if (preg_match_all('/--([\w.]+)\=([\S]+)/Ssu', implode(' ', $argv), $Pockets))
         {
             foreach ($Pockets[1] as $IX => $Key)
             {
                 $Key = strtr($Key, '_', ' ');
                 $Opts = F::Dot($Opts, $Key, $Pockets[2][$IX]);
-                F::Log('Get Opt Style CLI parameter loaded from *' . $Key . '* = *' . $Pockets[2][$IX] . '*', LOG_INFO);
+                // log is not available before bootstrap
+                fwrite(STDERR, "Get Opt Style CLI parameter loaded from \033[1;32m" . $Key . "\033[0m = \033[1;34m".$Pockets[2][$IX]."\033[0m".PHP_EOL);
             }
         }
         else
+        {
             $Opts[] = $argv;
+        }
 
         !defined('Root') ? define('Root', getcwd()) : false;
 
-        fwrite(STDERR, "\033[1;31mRoot: \033[0m".Root.PHP_EOL);
+        // log is not available before bootstrap
+        fwrite(STDERR, "Root: \033[1;32m".Root."\033[0m".PHP_EOL);
         include Root . '/vendor/autoload.php';
 
         if (empty($Opts))
@@ -40,26 +42,27 @@
         {
             if (isset($Opts['Service']))
             {
-                fwrite(STDERR, "\033[1;31mService: \033[0m".$Opts['Service'].PHP_EOL);
+                // log is not available before bootstrap
+                fwrite(STDERR, "Service: \033[1;32m".$Opts['Service']."\033[0m".PHP_EOL);
                 if (isset($Opts['Method']))
                     ;
                 else
                     $Opts['Method'] = 'Do';
 
-                fwrite(STDERR, "\033[1;31mMethod: \033[0m".$Opts['Method'].PHP_EOL);
+                fwrite(STDERR, "Method: \033[1;32m".$Opts['Method']."\033[0m".PHP_EOL);
 
                 try
                 {
                     $Call = F::Bootstrap
                     ([
                         'Paths' => [Root],
-                        'Environment' => isset($Opts['Environment']) ? $Opts['Environment'] : null,
+                        'Environment' => $Opts['Environment'] ?? null,
                         'Service' => 'System.Interface.CLI',
                         'Method' => 'Do',
                         'Call' => $Opts
                     ]);
 
-                    F::Shutdown($Call);
+                    F::Shutdown();
                 } catch (Exception $e)
                 {
                     F::Log('CLI Exception: ' . $e->getMessage(), LOG_CRIT, 'Developer');
