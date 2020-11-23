25630353c9752ea1f86b8cbb534173090a62be51
Dmitry Bergstein <bergstein@trickyplan.com>
2020-11-23 10:46:46 +0300

    + 43.10.2 + Live attributes for HTML Widgets

--- src/Drivers/View/HTML/Widget/Base.php
+++ src/Drivers/View/HTML/Widget/Base.php
@@ -2,46 +2,57 @@
 
     /* Codeine
      * @author bergstein@trickyplan.com
-     * @description  
+     * @description
      * @package Codeine
-     * @version 8.x
+     * @version 43.10.2
      */
+
     setFn('Make', function ($Call)
     {
-        if (isset($Call['Attributes']['String']))
+        if (isset($Call['Attributes']['String']) && !empty($Call['Attributes']['String']))
             foreach ($Call['Attributes']['String'] as $Attribute => $Value)
             {
-                 if (isset($Call[$Attribute]) && $Call[$Attribute] !== null)
-                 {
-                     if (is_array($Call[$Attribute]))
-                         $Call[$Attribute] = implode(' ', F::Merge($Value, $Call[$Attribute]));
-                     
-                     $Attributes[] = strtolower($Attribute).'="'.$Call[$Attribute].'"';
-                 }
-                 else
-                 {
-                     if (empty($Value))
-                         ;
-                     else
-                     {
-                         if (is_array($Value))
-                            $Value = implode(' ', $Value);
-                         
-                         $Attributes[] = strtolower($Attribute).'="'.$Value.'"';
-                     }
-                 }
+                if (isset($Call[$Attribute]))
+                {
+                    $Call[$Attribute] = F::Live($Call[$Attribute], $Call);
+
+                    if ($Call[$Attribute] !== null)
+                    {
+                        if (is_array($Call[$Attribute]))
+                            $Call[$Attribute] = implode(' ', F::Merge($Value, $Call[$Attribute]));
+
+                        $Attributes[] = strtolower($Attribute).'="'.$Call[$Attribute].'"';
+                    }
+                    else
+                    {
+                        if (empty($Value))
+                            ;
+                        else
+                        {
+                            if (is_array($Value))
+                                $Value = implode(' ', $Value);
+
+                            $Attributes[] = strtolower($Attribute).'="'.$Value.'"';
+                        }
+                    }
+                }
             }
 
-        if (isset($Call['Attributes']['Boolean']))
+        if (isset($Call['Attributes']['Boolean']) && !empty($Call['Attributes']['Boolean']))
             foreach ($Call['Attributes']['Boolean'] as $Attribute => $Value)
             {
-                 if (isset($Call[$Attribute]) && $Call[$Attribute])
-                     $Attributes[] = strtolower($Attribute);
-                 else
-                     if (!empty($Value) && $Value)
-                         $Attributes[] = strtolower($Attribute);
+                if (isset($Call[$Attribute]))
+                {
+                    $Call[$Attribute] = F::Live($Call[$Attribute], $Call);
+
+                    if (F::Dot($Call, $Attribute) !== null)
+                        $Attributes[] = strtolower($Attribute);
+                    else
+                        if (!empty($Value) && $Value)
+                            $Attributes[] = strtolower($Attribute);
+                }
             }
-         
+
         if (isset($Call['Block']) && $Call['Block'])
             $Call['HTML'] = '<'.$Call['Tag'].' '.implode(' ', $Attributes).'>'.$Call['Value'].'</'.$Call['Tag'].'>';
         else
