c63b7d1e086992ff717cfac834555b6c15263351
Dmitry Bergstein <bergstein@trickyplan.com>
2021-01-15 18:19:04 +0300

    + 45.3.10 + New Parslet «Replace»

---
+++ src/Drivers/View/HTML/Parslets/Replace.php
@@ -0,0 +1,57 @@
+<?php
+
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description
+     * @package Codeine
+     * @version 7.0
+     */
+
+    setFn('Parse', function ($Call)
+    {
+        $Replaces = [];
+        foreach ($Call['Parsed']['Value'] as $IX => $Data)
+        {
+            if (isset($Call['Parsed']['Options'][$IX]['scope']))
+            {
+                $Call['Scope'] = $Call['Parsed']['Options'][$IX]['scope'];
+
+                if (isset($Call['Parsed']['Options'][$IX]['id']))
+                {
+                    $Call['ID'] = $Call['Parsed']['Options'][$IX]['id'];
+                    $IDs = [$Call['ID']];
+
+                    if (isset($Call['Context']) && !empty($Call['Context']))
+                        $IDs[] =  $Call['ID'].'.'.$Call['Context'];
+
+                    $IDs = array_reverse($IDs);
+
+                    if (isset($Call['Scope']))
+                        $Call['Scope'] = strtr($Call['Scope'], '.', '/');
+
+                    $Data = jd($Data);
+
+                    $Template = F::Run('IO', 'Read', $Call,
+                    [
+                        'Storage' => 'Layout',
+                        'Where'   =>
+                        [
+                            'ID'    => $IDs
+                        ],
+                        'IO One'    => true
+                    ]);
+
+                    $Replaces[$IX] = preg_replace_callback('@<codeine-replace-variable>(.*?)</codeine-replace-variable>@Ssu', function ($Matches) use ($Data) {
+                        return F::Dot($Data, $Matches[1]);
+                        }, $Template);
+                }
+                else
+                    F::Log('ID attribute is required for template parslet', LOG_ERR);
+            }
+            else
+                F::Log('Scope attribute is required for template parslet', LOG_ERR);
+
+        }
+
+        return $Replaces;
+     });
