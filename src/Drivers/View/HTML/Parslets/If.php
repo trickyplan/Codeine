8d54795573b445ca327ab395c45f974fff69e897
Dmitry Bergstein <bergstein@trickyplan.com>
2021-09-17 13:34:14 +0300

    + 46.0.4 + If fixes

--- src/Drivers/View/HTML/Parslets/If.php
+++ src/Drivers/View/HTML/Parslets/If.php
@@ -1,132 +1,135 @@
 <?php
 
-    /* Codeine
-     * @author bergstein@trickyplan.com
-     * @description Layout Parslet 
-     * @package Codeine
-     * @version 8.x
-     */
+/* Codeine
+ * @author bergstein@trickyplan.com
+ * @description Layout Parslet
+ * @package Codeine
+ * @version 8.x
+ */
 
-    setFn ('Parse', function ($Call)
+setFn ('Parse', function ($Call)
+{
+    $Replaces = [];
+
+    foreach ($Call['Parsed']['Value'] as $IX => $Match)
     {
-        $Replaces = [];
+        $Decision = false;
 
-        foreach ($Call['Parsed']['Value'] as $IX => $Match)
+        if (empty($Call['Parsed']['Options'][$IX]))
+            ;
+        else
         {
-            $Decision = false;
+            $Value = null;
 
-            if (empty($Call['Parsed']['Options'][$IX]))
+            $Variable = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.variable');
+
+            if (empty($Variable))
                 ;
             else
-            {
-                $Variable = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.variable');
+                $Value = F::Dot($Call, $Variable);
 
-                if (empty($Variable))
+            if (empty($Value))
+            {
+                $Key = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.key');
+                if (empty($Key))
                     ;
                 else
-                    $Value = F::Dot($Call, $Variable);
+                    $Value = F::Dot($Call['Data'], $Key);
+            }
 
-                if (empty($Value))
-                {
-                    $Key = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.key');
-                    if (empty($Key))
-                        ;
-                    else
-                        $Value = F::Dot($Call['Data'], $Key);
-                }
+            if (empty($Value))
+                $Value = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.value');
 
-                if (empty($Value))
-                    $Value = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.value');
+            if ($Value === 0)
+                $Value = '0';
 
-                if ($Value === 0)
-                    $Value = '0';
+            if ($Value === false)
+                $Value = 'false';
 
-                if ($Value === false)
-                    $Value = 'false';
+            if ($Value === true)
+                $Value = 'true';
 
-                if ($Value === true)
-                    $Value = 'true';
+            if (empty($Value))
+            {
+                if ($Null = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.null'))
+                    $Value = $Null;
+                else
+                    $Value = 'null'; // TODO Externalize
+            }
 
-                if (empty($Value))
-                {
-                    if ($Null = (string) F::Dot($Call['Parsed'], 'Options.'.$IX.'.null'))
-                        $Value = $Null;
-                    else
-                        $Value = 'null'; // TODO Externalize
-                }
-                
-                if (isset($Call['Parsed']['Options'][$IX]['null']))
-                {
-                    if ($Call['Parsed']['Options'][$IX]['null'] == 1)
-                        $Decision = (null == $Value);
-                    else
-                        $Decision = !(null == $Value);
-                }
+            if (isset($Call['Parsed']['Options'][$IX]['null']))
+            {
+                if ($Call['Parsed']['Options'][$IX]['null'] == 1)
+                    $Decision = (null == $Value);
+                else
+                    $Decision = !(null == $Value);
+            }
 
-                if (isset($Call['Parsed']['Options'][$IX]['eq']))
-                    $Decision = ($Value == (string) $Call['Parsed']['Options'][$IX]['eq']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['neq']))
-                    $Decision = ($Value != (string) $Call['Parsed']['Options'][$IX]['neq']);
+            if (isset($Call['Parsed']['Options'][$IX]['eq']))
+                $Decision = ($Value == (string) $Call['Parsed']['Options'][$IX]['eq']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['lt']))
-                    $Decision = ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lt']);
+            if (isset($Call['Parsed']['Options'][$IX]['neq']))
+                $Decision = ($Value != (string) $Call['Parsed']['Options'][$IX]['neq']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['gt']))
-                    $Decision = ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gt']);
+            if (isset($Call['Parsed']['Options'][$IX]['lt']))
+                $Decision = ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lt']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['lte']))
-                    $Decision = ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lte']);
+            if (isset($Call['Parsed']['Options'][$IX]['gt']))
+                $Decision = ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gt']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['gte']))
-                    $Decision = ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gte']);
-                
-                if (isset($Call['Parsed']['Options'][$IX]['gt']) && isset($Call['Parsed']['Options'][$IX]['lt']))
-                {
-                    $Decision =
-                        ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lt'])
-                        &&
-                        ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gt']);
-                }
-                
-                if (isset($Call['Parsed']['Options'][$IX]['gte']) && isset($Call['Parsed']['Options'][$IX]['lte']))
-                {
-                    $Decision =
-                        ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lte'])
-                        &&
-                        ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gte']);
-                }
+            if (isset($Call['Parsed']['Options'][$IX]['lte']))
+                $Decision = ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lte']);
 
-                if (isset($Call['Parsed']['Options'][$IX]['gt']) && isset($Call['Parsed']['Options'][$IX]['lte']))
-                {
-                    $Decision =
-                        ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lte'])
-                        &&
-                        ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gt']);
-                }
-                
-                if (isset($Call['Parsed']['Options'][$IX]['gte']) && isset($Call['Parsed']['Options'][$IX]['lt']))
-                {
-                    $Decision =
-                        ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lt'])
-                        &&
-                        ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gte']);
-                }
-                
-                if (isset($Call['Parsed']['Options'][$IX]['in']))
-                {
-                    $Data = F::Dot($Call, $Call['Parsed']['Options'][$IX]['in']);
-                    if (is_array($Data)) {
-                        $Decision = in_array($Value, $Data);
-                    }
+            if (isset($Call['Parsed']['Options'][$IX]['gte']))
+                $Decision = ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gte']);
+
+            if (isset($Call['Parsed']['Options'][$IX]['gt']) && isset($Call['Parsed']['Options'][$IX]['lt']))
+            {
+                $Decision =
+                    ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lt'])
+                    &&
+                    ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gt']);
+            }
+
+            if (isset($Call['Parsed']['Options'][$IX]['gte']) && isset($Call['Parsed']['Options'][$IX]['lte']))
+            {
+                $Decision =
+                    ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lte'])
+                    &&
+                    ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gte']);
+            }
+
+            if (isset($Call['Parsed']['Options'][$IX]['gt']) && isset($Call['Parsed']['Options'][$IX]['lte']))
+            {
+                $Decision =
+                    ((float) preg_replace('/,/','.',$Value) < (float) $Call['Parsed']['Options'][$IX]['lte'])
+                    &&
+                    ((float) preg_replace('/,/','.',$Value) >= (float) $Call['Parsed']['Options'][$IX]['gt']);
+            }
+
+            if (isset($Call['Parsed']['Options'][$IX]['gte']) && isset($Call['Parsed']['Options'][$IX]['lt']))
+            {
+                $Decision =
+                    ((float) preg_replace('/,/','.',$Value) <= (float) $Call['Parsed']['Options'][$IX]['lt'])
+                    &&
+                    ((float) preg_replace('/,/','.',$Value) > (float) $Call['Parsed']['Options'][$IX]['gte']);
+            }
+
+            if (isset($Call['Parsed']['Options'][$IX]['in']))
+            {
+                $Data = F::Dot($Call, $Call['Parsed']['Options'][$IX]['in']);
+                if (is_array($Data)) {
+                    $Decision = in_array($Value, $Data);
                 }
             }
-            
-            if ($Decision)
-                $Replaces[$Call['Parsed']['Match'][$IX]] = $Match;
-            else
-                $Replaces[$Call['Parsed']['Match'][$IX]] = '';
         }
 
-        return $Replaces;
-    });
+        if ($Decision)
+            $Replaces[$Call['Parsed']['Match'][$IX]] = $Match;
+        else
+            $Replaces[$Call['Parsed']['Match'][$IX]] = '';
+    }
+
+    return $Replaces;
+});
