b96000eef5fb062512169dad6feb82411871f39a
Dmitry Bergstein <bergstein@trickyplan.com>
2021-11-15 20:10:41 +0300

    + 46.3.1 + Permissions-Policy fix

--- src/Drivers/System/Interface/HTTP/Headers/Permissions-Policy.php
+++ src/Drivers/System/Interface/HTTP/Headers/Permissions-Policy.php
@@ -13,9 +13,9 @@
 
         $Policies = F::Dot($Call, 'System.Interface.HTTP.Headers.Permissions-Policy');
         foreach ($Policies as $Policy => $Decision)
-            $Header[] = $Policy." ".F::Variable($Decision, $Call);
+            $Header[] = $Policy."=".F::Variable($Decision, $Call);
 
-        $Call['HTTP']['Headers']['Permissions-Policy:'] = implode('; ', $Header);
+        $Call['HTTP']['Headers']['Permissions-Policy:'] = implode(', ', $Header);
 
         return $Call;
     });
