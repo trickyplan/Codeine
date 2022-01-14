99148f2ddd93f45943fdc089f122fc0b8beca294
Dmitry Bergstein <bergstein@trickyplan.com>
2022-01-14 12:51:40 +0300

    # 47.3.1 + OpenWeatherMap minor fixes.

--- src/Drivers/Providers/Weather/OpenWeatherMap/OneCall.php
+++ src/Drivers/Providers/Weather/OpenWeatherMap/OneCall.php
@@ -1,70 +1,83 @@
 <?php
 
-/* Codeine
- * @author bergstein@trickyplan.com
- * @description
- * @package Codeine
- * @version 2020.x.x
- */
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description
+     * @package Codeine
+     * @version 2020.x.x
+     */
 
     setFn('Do', function ($Call)
     {
-        $Output = [];
+        $Weather = [];
 
         $Response = F::Run('IO', 'Read', $Call,
             [
                 'Storage'   => 'Web',
                 'Where'     =>
-                [
-                    'ID'    => F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Endpoint')
-                ],
+                    [
+                        'ID'    => F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Endpoint')
+                    ],
                 'Data'      => [
                     'lat'   => F::Dot($Call, 'Latitude'),
                     'lon'   => F::Dot($Call, 'Longitude'),
                     'appid' => F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.APIKey')
                 ],
                 'Behaviours'=>
-                [
-                    'Cached'    =>
                     [
-                        'Enabled'   => true,
-                        'TTL'       => 3600,
-                        'Keys'      => ['Where.ID', 'Data', 'IO One']
-                    ]
-                ],
+                        'Cached'    =>
+                            [
+                                'Enabled'   => true,
+                                'TTL'       => 3600,
+                                'Keys'      => ['Storage', 'Where.ID', 'Data', 'IO One'],
+                                'Result'    =>
+                                    [
+                                        'Allow Storing Empty' => false
+                                    ]
+                            ]
+                    ],
                 'Format' => 'Formats.JSON',
                 'IO One'           => true
             ]);
 
-        $Map = F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Map');
-        $Output['Now'] = [
-            'Time' => $Response['current']['dt']
-        ];
-
-        foreach ($Map['Now'] as $From => $To)
-            $Output['Now'] = F::Dot($Output['Now'], $To, F::Dot($Response['current'], $From));
-
-        foreach ($Response['hourly'] as $Hourly)
+        if (empty($Response))
         {
-            $Time = date('Hi', $Hourly['dt']);
-            $Output['Hourly'][$Time] = ['Time' => $Time];
-            foreach ($Map['Hourly'] as $From => $To)
+            $Weather = null;
+            F::Log('Empty response from OpenWeatherMap', LOG_WARNING);
+        }
+        else
+        {
+            $Map = F::Dot($Call, 'Providers.Weather.OpenWeatherMap.OneCall.Map');
+            $Weather['Now'] = [
+                'Time' => $Response['current']['dt']
+            ];
+
+            foreach ($Map['Now'] as $From => $To)
+                $Weather['Now'] = F::Dot($Weather['Now'], $To, F::Dot($Response['current'], $From));
+
+            foreach ($Response['hourly'] as $Hourly)
             {
-                if ($Value = F::Dot($Hourly, $From))
-                    $Output['Hourly'][$Time] = F::Dot($Output['Hourly'][$Time], $To, $Value);
+                $Time = date('Hi', $Hourly['dt']);
+                $Weather['Hourly'][$Time] = ['Time' => $Time];
+                foreach ($Map['Hourly'] as $From => $To)
+                {
+                    if ($Value = F::Dot($Hourly, $From))
+                        $Weather['Hourly'][$Time] = F::Dot($Weather['Hourly'][$Time], $To, $Value);
+                }
+            }
+
+            foreach ($Response['daily'] as $Daily)
+            {
+                $Date = date('Ymd', $Daily['dt']);
+                $Weather['Daily'][$Date] = ['Date' => $Date];
+                foreach ($Map['Daily'] as $From => $To)
+                {
+                    if ($Value = F::Dot($Daily, $From))
+                        $Weather['Daily'][$Date] = F::Dot($Weather['Daily'][$Date], $To, $Value);
+                }
             }
         }
 
-        foreach ($Response['daily'] as $Daily)
-        {
-            $Date = date('Ymd', $Daily['dt']);
-            $Output['Daily'][$Date] = ['Date' => $Date];
-            foreach ($Map['Daily'] as $From => $To)
-            {
-                if ($Value = F::Dot($Daily, $From))
-                    $Output['Daily'][$Date] = F::Dot($Output['Daily'][$Date], $To, $Value);
-            }
-        }
 
-        return $Output;
+        return $Weather;
     });
