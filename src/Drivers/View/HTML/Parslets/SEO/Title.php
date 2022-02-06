1db7468c17c055b891baaf4db1d580d6414e9402
Dmitry Bergstein <bergstein@trickyplan.com>
2022-02-06 15:47:43 +0300

    @feat: Add SEO Parslets

---
+++ src/Drivers/View/HTML/Parslets/SEO/Title.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* Codeine
+     * @author bergstein@trickyplan.com
+     * @description  
+     * @package Codeine
+     * @version 7.0
+     */
+
+    setFn('Parse', function ($Call)
+    {
+        foreach ($Call['Parsed']['Value'] as $IX => $Match)
+        {
+            $Link = $Call['Parsed']['Value'][$IX];
+            $Call['SEO']['Titles'][] = $Link;
+            $Call['Replace'][$Call['Parsed']['Match'][$IX]] = '';
+        }
+
+        return $Call;
+    });
