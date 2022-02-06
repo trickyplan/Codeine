5102b0b6c9604e051c4c68270c39c4544111f00b
Dmitry Bergstein <bergstein@trickyplan.com>
2022-02-06 15:47:01 +0300

    @deprecated: View.HTML.Meta is now View.HTML.SEO

--- src/Drivers/View/HTML/Meta/Scan.php
+++ src/Drivers/View/HTML/Meta/Scan.php
@@ -11,6 +11,7 @@
     {
         if (preg_match_all('@<subtitle>(.*)<\/subtitle>@SsUu', $Call['Output'], $Pockets))
         {
+            F::Log('*title* tag is deprecated. Use *codeine-seo-title* instead', LOG_WARNING, ['Developer', 'Deprecated']);
             $Call['View']['HTML']['Title'] = F::Merge($Call['View']['HTML']['Title'], $Pockets[1]);
             $Call['Output'] = str_replace($Pockets[0], '', $Call['Output']);
         }
@@ -22,6 +23,7 @@
     {
         if (preg_match_all('@<keyword>(.*)<\/keyword>@SsUu', $Call['Output'], $Pockets))
         {
+            F::Log('*keyword* tag is deprecated. Use *codeine-seo-keyword* instead', LOG_WARNING, ['Developer', 'Deprecated']);
             $Call['View']['HTML']['Keywords'] = F::Merge($Call['View']['HTML']['Keywords'], $Pockets[1]);
             $Call['Output'] = str_replace($Pockets[0], '', $Call['Output']);
         }
@@ -33,6 +35,7 @@
     {
         if (preg_match_all('@<description>(.*)<\/description>@SsUu', $Call['Output'], $Pockets))
             {
+                F::Log('*description* tag is deprecated. Use *codeine-seo-description* instead', LOG_WARNING, ['Developer', 'Deprecated']);
                 $Call['View']['HTML']['Description'] = F::Merge($Call['View']['HTML']['Description'], $Pockets[1]);
                 $Call['Output'] = str_replace($Pockets[0], '', $Call['Output']);
             }
