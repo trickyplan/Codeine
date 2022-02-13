7abb078d0d28fa40ff61e7de1807e6c6da43bb3f
Dmitry Bergstein <bergstein@trickyplan.com>
2022-02-13 13:41:18 +0300

    @fix: fix log channels sorting

--- src/Drivers/IO/Log.php
+++ src/Drivers/IO/Log.php
@@ -22,36 +22,44 @@
             if (isset($Call['HTTP']))
                 $App = $Call['HTTP']['Proto'] . $Call['HTTP']['Host'] . $Call['HTTP']['URI'];
             else
-                $App = $Call['Service'].':'.$Call['Method'];
+                $App = $Call['Service'] . ':' . $Call['Method'];
 
-                foreach ($Call['All Logs'] as $Call['Channel'] => $Call['Channel Logs'])
+            $Codeine = F::loadOptions('Codeine');
+            $Channels = F::Dot($Codeine, 'Verbose');
+
+            foreach ($Channels as $Call['Channel'] => $Verbose)
+            {
+                if (isset($Call['All Logs'][$Call['Channel']]))
                 {
+                    $Call['Channel Logs'] = $Call['All Logs'][$Call['Channel']];
+
                     if (empty($Call['Channel Logs']))
                         ;
                     else
                     {
                         $Call = F::Hook('Log.Spit.Channel.Before', $Call);
 
-                            F::Run('IO', 'Write', $Call,
-                                [
-                                    'Storage' => $Call['Channel'],
-                                    'Where!'  => '[' . $Call['Channel'] . '] ' . $App,
-                                    'Data!'   => $Call['Channel Logs']
-                                ]);
+                        F::Run('IO', 'Write', $Call,
+                            [
+                                'Storage' => $Call['Channel'],
+                                'Where!' => '[' . $Call['Channel'] . '] ' . $App,
+                                'Data!' => $Call['Channel Logs']
+                            ]);
 
-                            F::Run('IO', 'Close', ['Storage' => $Call['Channel']]);
+                        F::Run('IO', 'Close', ['Storage' => $Call['Channel']]);
 
                         $Call = F::Hook('Log.Spit.Channel.After', $Call);
                     }
                 }
+            }
 
             $Call = F::Hook('Log.Spit.After', $Call);
         }
-                
+
         return $Call;
     });
-    
-    
+
+
     setFn('Autotest', function ($Call)
     {
         F::Log('Autotest. I\'m alive.', LOG_WARNING, 'All');
