bf05c077e16309330a90b13bfa464f3e2595fcd7
Dmitry Bergstein <bergstein@trickyplan.com>
2022-04-05 16:50:29 +0300

    @fix format/type attribute for fn

--- src/Drivers/View/HTML/Parslets/Fn.php
+++ src/Drivers/View/HTML/Parslets/Fn.php
@@ -9,13 +9,24 @@
 
     setFn('Parse', function ($Call) {
         foreach ($Call['Parsed']['Value'] as $IX => $Match) {
-            if (F::Dot($Call['Parsed'], 'Options.' . $IX . '.type') === null) {
-                $Type = 'XML';
+            if (F::Dot($Call['Parsed'], 'Options.' . $IX . '.format') === null) {
+                if (F::Dot($Call['Parsed'], 'Options.' . $IX . '.type') === null) {
+                    $Format = $Call['Parslet']['Exec']['Type'];
+                } else {
+                    $Format = F::Dot($Call['Parsed'], 'Options.' . $IX . '.type');
+
+                    F::Log(
+                        'Please, replace "type" with "format" as more semantically correct',
+                        LOG_WARNING,
+                        ['Developer', 'Deprecated']
+                    );
+                    F::Log($Match, LOG_WARNING, ['Developer', 'Deprecated']);
+                }
             } else {
-                $Type = F::Dot($Call['Parsed'], 'Options.' . $IX . '.type');
+                $Format = F::Dot($Call['Parsed'], 'Options.' . $IX . '.format');
             }
 
-            $Match = F::Run('Formats.' . $Type, 'Read', ['Value' => trim($Call['Parsed']['Value'][$IX])]);
+            $Match = F::Run('Formats.' . $Format, 'Read', ['Value' => trim($Call['Parsed']['Value'][$IX])]);
 
             foreach ($Call['Inherited'] as $Key) {
                 if (isset($Call[$Key])) {
