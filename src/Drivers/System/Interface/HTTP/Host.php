85b0cbc3ac2fee509b1eb679e8ab74b5cf102746
Dmitry Bergstein <bergstein@trickyplan.com>
2022-04-10 00:22:20 +0300

    @feat refined proto/host/port logic @bc-break

--- src/Drivers/System/Interface/HTTP/Host.php
+++ src/Drivers/System/Interface/HTTP/Host.php
@@ -7,28 +7,94 @@
      * @version 8.x
      */
 
-    setFn('Do', function ($Call) {
-        $Call = F::Hook('beforeHostDetect', $Call);
+    setFn('Do', function ($Call)
+    {
+        $Call = F::Hook('beforeHostDetermine', $Call);
 
-        F::Log('Host Strategy *' . $Call['HTTP']['Host Strategy'] . '* selected', LOG_INFO);
-        $Call = F::Apply($Call['HTTP']['Host Strategy'], 'Do', $Call);
+            $Call = F::Apply(null, 'Determine.Proto', $Call);
+            $Call = F::Apply(null, 'Determine.Port', $Call);
+            $Call = F::Apply(null, 'Determine.Host', $Call);
+            $Call = F::Apply(null, 'Generate.FQDN', $Call);
 
-        if ($Host = F::Dot($Call, 'HTTP.Host')) {
-            if (isset($Call['HTTP']['Domain'])) {
-                ;
-            } else {
-                if (str_contains($Host, ':')) {
-                    list ($Call['HTTP']['Domain'], $Call['HTTP']['Port']) = explode(':', $Host);
-                } else {
-                    $Call['HTTP']['Domain'] = $Host;
-                }
+        return F::Hook('afterHostDetermine', $Call);
+    });
+
+    setFn('Determine.Proto', function ($Call) {
+        $Secure = false;
+
+        $Keys = ['HTTPS', 'HTTP_X_FORWARDED_PROTO', 'HTTP_X_HTTPS'];
+
+        foreach ($Keys as $Key) {
+            if (F::Dot($_SERVER, $Key)) {
+                $Secure = true;
+                F::Log('HTTPS is *On* (' . $Key . ')', LOG_INFO);
+                break;
             }
-
-            F::Log('Host is *' . $Host . '*', LOG_INFO);
-            $Call = F::loadOptions($Host, null, $Call);
         }
 
-        $Call = F::Hook('afterHostDetect', $Call);
+        $Call['HTTP']['Proto'] = $Secure ? 'https://' : 'http://';
+
+        return $Call;
+    });
+
+    setFn('Determine.Port', function ($Call)
+    {
+        $Port = 0;
+
+        if (isset($_SERVER['CODEINE_HTTP_PORT']))
+        {
+            $Port = $_SERVER['CODEINE_HTTP_PORT'];
+            F::Log('Port: *'.$Port.'* (via CODEINE_HTTP_PORT)', LOG_INFO);
+        }
+        elseif (str_contains($_SERVER['HTTP_HOST'], ':'))
+        {
+            list(,$Port) = explode(':', $_SERVER['HTTP_HOST']);
+            F::Log('Port: *'.$Port.'* (via HTTP_HOST)', LOG_INFO);
+        }
+        elseif (isset($_SERVER['SERVER_PORT']))
+        {
+            $Port = $_SERVER['SERVER_PORT'];
+            F::Log('Port: *'.$Port.'* (via SERVER_PORT)', LOG_INFO);
+        }
+
+        $Call['HTTP']['Port'] = $Port;
+        return $Call;
+    });
+
+    setFn('Determine.Host', function ($Call)
+    {
+        $Host = '';
+
+        if (isset($_SERVER['CODEINE_HTTP_HOST']))
+        {
+            $Host = $_SERVER['CODEINE_HTTP_HOST'];
+            F::Log('Host: *'.$Host.'* (via CODEINE_HTTP_HOST)', LOG_INFO);
+        }
+        elseif (isset($_SERVER['HTTP_HOST']))
+        {
+            if (str_contains($_SERVER['HTTP_HOST'], ':'))
+                list($Host,) = explode(':', $_SERVER['HTTP_HOST']);
+            else
+                $Host = $_SERVER['HTTP_HOST'];
+
+            F::Log('Host: *'.$Host.'* (via HTTP_HOST)', LOG_INFO);
+        }
+
+        $Call['HTTP']['Host'] = $Host;
+
+        return $Call;
+    });
+
+    setFn('Generate.FQDN', function ($Call) {
+        $Call['HTTP']['FQDN'] = $Call['HTTP']['Proto'] . $Call['HTTP']['Host'];
+        if ($Call['HTTP']['Proto'] === 'http' && $Call['HTTP']['Port'] != 80) {
+            $Call['HTTP']['FQDN'] .= ':' . $Call['HTTP']['Port'];
+        }
+
+        if ($Call['HTTP']['Proto'] === 'https' && $Call['HTTP']['Port'] != 443) {
+            $Call['HTTP']['FQDN'] .= ':' . $Call['HTTP']['Port'];
+        }
+        F::Log('FQDN: *'.$Call['HTTP']['FQDN'].'*', LOG_INFO);
 
         return $Call;
     });
