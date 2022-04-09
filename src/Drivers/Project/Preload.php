83967385c34e211ab9fc657d01e0d91c7b86216c
Dmitry Bergstein <bergstein@trickyplan.com>
2022-04-10 00:21:23 +0300

    @fix codeine version preload

--- src/Drivers/Project/Preload.php
+++ src/Drivers/Project/Preload.php
@@ -19,13 +19,6 @@
     {
         $Call['Project'] = F::loadOptions('Project');
 
-        $CodeineComposerFile = jd(file_get_contents(Root.'/vendor/trickyplan/codeine/composer.json'));
-
-        if (isset($CodeineComposerFile['version']))
-            $Call['Version']['Codeine'] = $CodeineComposerFile['version'];
-        else
-            $Call['Version']['Codeine'] = 'Live';
-
         $ProjectComposerFile = jd(file_get_contents(Root.'/composer.json'));
 
         if (isset($ProjectComposerFile['version']))
@@ -34,5 +27,18 @@
             $Call['Version']['Project'] = 'Live';
 
         F::Log('Project Version: *' . F::Dot($Call, 'Version.Project') . '*', LOG_INFO);
+
+        if (file_exists(Root.'/vendor/trickyplan/codeine/composer.json'))
+        {
+            $CodeineComposerFile = jd(file_get_contents(Root.'/vendor/trickyplan/codeine/composer.json'));
+
+            if (isset($CodeineComposerFile['version']))
+                $Call['Version']['Codeine'] = $CodeineComposerFile['version'];
+            else
+                $Call['Version']['Codeine'] = 'Live';
+        }
+        else // we are codeine
+            $Call['Version']['Codeine'] = $Call['Version']['Project'];
+
         return $Call;
     });
