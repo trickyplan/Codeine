1cd3fece0b39e253d768e73b90cfaee16e0c7818
Dmitry Bergstein <bergstein@trickyplan.com>
2022-04-10 18:59:14 +0300

    @feat split Project/Version and Project/Preload

--- src/Drivers/Project/Preload.php
+++ src/Drivers/Project/Preload.php
@@ -1,44 +1,9 @@
 <?php
 
-    /* Codeine
-     * @author bergstein@trickyplan.com
-     * @description
-     * @package Codeine
-     * @version 8.x
-     */
-
     setFn('beforeRequestRun', function ($Call) {
-        return F::Apply(null, 'Load.ProjectAndVersion', $Call);
+        return $Call;
     });
 
     setFn('beforeCLIRequestRun', function ($Call) {
-        return F::Apply(null, 'Load.ProjectAndVersion', $Call);
-    });
-
-    setFn('Load.ProjectAndVersion', function ($Call)
-    {
-        $Call['Project'] = F::loadOptions('Project');
-
-        $ProjectComposerFile = jd(file_get_contents(Root.'/composer.json'));
-
-        if (isset($ProjectComposerFile['version']))
-            $Call['Version']['Project'] = $ProjectComposerFile['version'];
-        else
-            $Call['Version']['Project'] = 'Live';
-
-        F::Log('Project Version: *' . F::Dot($Call, 'Version.Project') . '*', LOG_INFO);
-
-        if (file_exists(Root.'/vendor/trickyplan/codeine/composer.json'))
-        {
-            $CodeineComposerFile = jd(file_get_contents(Root.'/vendor/trickyplan/codeine/composer.json'));
-
-            if (isset($CodeineComposerFile['version']))
-                $Call['Version']['Codeine'] = $CodeineComposerFile['version'];
-            else
-                $Call['Version']['Codeine'] = 'Live';
-        }
-        else // we are codeine
-            $Call['Version']['Codeine'] = $Call['Version']['Project'];
-
         return $Call;
     });
--- src/Drivers/Project/Version.php
+++ src/Drivers/Project/Version.php
@@ -7,6 +7,30 @@
      * @version 8.x
      */
 
-    setFn('Do', function ($Call) {
+    setFn('Load', function ($Call)
+    {
+        $Call['Project'] = F::loadOptions('Project');
+
+        $ProjectComposerFile = jd(file_get_contents(Root.'/composer.json'));
+
+        if (isset($ProjectComposerFile['version']))
+            $Call['Version']['Project'] = $ProjectComposerFile['version'];
+        else
+            $Call['Version']['Project'] = 'Live';
+
+        F::Log('Project Version: *' . F::Dot($Call, 'Version.Project') . '*', LOG_INFO);
+
+        if (file_exists(Root.'/vendor/trickyplan/codeine/composer.json'))
+        {
+            $CodeineComposerFile = jd(file_get_contents(Root.'/vendor/trickyplan/codeine/composer.json'));
+
+            if (isset($CodeineComposerFile['version']))
+                $Call['Version']['Codeine'] = $CodeineComposerFile['version'];
+            else
+                $Call['Version']['Codeine'] = 'Live';
+        }
+        else // we are codeine
+            $Call['Version']['Codeine'] = $Call['Version']['Project'];
+
         return $Call;
     });
