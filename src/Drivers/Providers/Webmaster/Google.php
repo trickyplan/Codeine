f66cb1b8f24a8f7ff090dd2d4afb5540ec44a6be
Dmitry Bergstein <bergstein@trickyplan.com>
2022-04-19 00:49:25 +0300

    @feat envs support for webmaster consoles

--- src/Drivers/Providers/Webmaster/Google.php
+++ src/Drivers/Providers/Webmaster/Google.php
@@ -8,6 +8,9 @@
      */
 
     setFn('Auth', function ($Call) {
+        $ID = F::Dot($Call, 'Webmaster.Google.ID');
+        $Call = F::Dot($Call, 'Webmaster.Google.ID', F::Variable($ID, $Call));
+
         if ($Call['ID'] == F::Dot($Call, 'Webmaster.Google.ID')) {
             $Call['Output']['Content'][] =
                 F::Dot($Call, 'Webmaster.Google.Prefix')
--- src/Drivers/Providers/Webmaster/Yandex.php
+++ src/Drivers/Providers/Webmaster/Yandex.php
@@ -8,6 +8,9 @@
      */
 
     setFn('Auth', function ($Call) {
+        $ID = F::Dot($Call, 'Webmaster.Yandex.ID');
+        $Call = F::Dot($Call, 'Webmaster.Yandex.ID', F::Variable($ID, $Call));
+
         if ($Call['ID'] == F::Dot($Call, 'Webmaster.Yandex.ID')) {
             $Call['Output']['Content'][] = F::Variable(F::Dot($Call, 'Webmaster.Yandex.Template'), $Call);
         } else {
